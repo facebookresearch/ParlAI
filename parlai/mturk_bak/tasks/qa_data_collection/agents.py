fe5c5738fa996fb0749e761cb9c3c30ed1fc7a72
Jason Weston <jase@fb.com>
2017-04-27 16:41:35 -0400

    bla

--- parlai/mturk_bak/tasks/qa_data_collection/agents.py
+++ parlai/mturk_bak/tasks/qa_data_collection/agents.py
@@ -17,11 +17,17 @@ class QADataCollectionAgent(Agent):
         self.conversation_id = None
         self.turn_index = -1
         # The task that we will be collecting QA pairs for.
+        module_name = 'parlai.tasks.squad.agents'
+        class_name = 'DefaultTeacher'
+        my_module = importlib.import_module(module_name)
+        task_class = getattr(my_module, class_name)
         task_opt = {}
-        task_opt['task'] = 'squad'
         task_opt['datatype'] = 'train'
         task_opt['datapath'] = opt['datapath']
-        self.task = create_task_agent_from_taskname(task_opt)[0]
+        self.task = task_class(task_opt)
+        # Alternatively, one could create the task like this also:
+        #  task_opt['task'] = 'squad'
+        #  self.task = create_task_agent_from_taskname(task_opt)[0]
 
     def act(self):
         self.turn_index = (self.turn_index + 1) % 3;
