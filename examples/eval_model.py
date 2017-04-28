6bbb4bec61c1b2ab675f4cc66ccf1666b0b076b9
jaseweston <jaseweston@gmail.com>
2017-04-28 10:48:45 -0400

    Examples cleanup (#4)
    * example cleanup
    
    * fix
    
    * teacher and bot can be any agents
    
    * fix:

--- examples/eval_model.py
+++ examples/eval_model.py
@@ -3,9 +3,16 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
+"""Basic example which iterates through the tasks specified and
+evaluates the given model on them.
 
+For example:
+`python examples/eval_model.py -t "babi:Task1k:2" -m "repeat_label"`
+or
+`python examples/eval_model.py -t "#CornellMovie" -m "ir_baseline" -mp "-lp 0.5"`
+"""
 from parlai.core.params import ParlaiParser
-from parlai.agents.repeat_label.agents import RepeatLabelAgent
+from parlai.core.agents import create_agent
 from parlai.core.worlds import create_task
 
 import random
@@ -14,21 +21,21 @@ def main():
     random.seed(42)
 
     # Get command line arguments
-    opt = ParlaiParser().parse_args()
-    agent = RepeatLabelAgent(opt)
-    agent.cantAnswerPercent = 0.35
+    parser = ParlaiParser(True, True)
+    parser.add_argument('-n', '--num_examples', default=1000)
+    opt = parser.parse_args()
+    # Create model and assign it to the specified task
+    agent = create_agent(opt)
     world = create_task(opt, agent)
 
     # Show some example dialogs:
-    for k in range(1000):
+    for k in range(int(opt['num_examples'])):
         world.parley()
-
         print("---")
         print(world.report())
-
-        if k > 100 and world.episode_done():
+        if world.epoch_done():
+            print("EPOCH DONE")
             break
-
     world.shutdown()
 
 if __name__ == '__main__':
--- parlai/tasks/babi/agents.py
+++ parlai/tasks/babi/agents.py
@@ -33,7 +33,7 @@ class Task1kTeacher(FbDialogTeacher):
 # Single bAbI task (10k training).
 class Task10kTeacher(FbDialogTeacher):
     def __init__(self, opt, shared=None):
-        task = opt.get('task', 'babi:Task-10k:1')
+        task = opt.get('task', 'babi:Task10k:1')
         opt['datafile'] = _path('-10k', task.split(':')[2], opt)
         opt['cands_datafile'] = _path('', task.split(':')[2], opt, 'train')
         super().__init__(opt, shared)
