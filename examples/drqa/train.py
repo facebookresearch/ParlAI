0655a72072779623471589e7eab5cc65dbe6fc43
Alexander Miller <ahm@fb.com>
2017-05-02 15:04:38 -0400

    header fixes (#23)

--- examples/drqa/train.py
+++ examples/drqa/train.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env python3
-# Copyright 2004-present Facebook. All Rights Reserved.
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
 """Trains (a partial) implementation of the DrQa Document Reader from:
 
 Danqi Chen, Adam Fisch, Jason Weston, Antoine Bordes. 2017.
