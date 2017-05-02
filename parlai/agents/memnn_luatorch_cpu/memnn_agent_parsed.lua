c444b6d40b49d00916bb92d65270a2cf944a7d4c
Alexander Miller <ahm@fb.com>
2017-05-02 15:03:45 -0400

    header fixes

--- parlai/agents/memnn_luatorch_cpu/memnn_agent_parsed.lua
+++ parlai/agents/memnn_luatorch_cpu/memnn_agent_parsed.lua
@@ -1,4 +1,8 @@
--- Copyright 2004-present Facebook. All Rights Reserved.
+-- Copyright (c) 2017-present, Facebook, Inc.
+-- All rights reserved.
+-- This source code is licensed under the BSD-style license found in the
+-- LICENSE file in the root directory of this source tree. An additional grant
+-- of patent rights can be found in the PATENTS file in the same directory.
 --[[
 This file has the same functionality as memnn_agent, except that it expects
 (and returns) a zero-indexed vector (as a table) of word indices into the
