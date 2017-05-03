e69cc58c1b1b8dc062f85812cdf26d1036dc2982
Alexander Miller <ahm@fb.com>
2017-05-03 19:06:28 -0400

    better check_examples (exits squad after 60s) (#41)

--- tests/check_examples.sh
+++ tests/check_examples.sh
@@ -12,4 +12,5 @@ python build_dict.py -t babi:task1k:1 --dict-savepath /tmp/dict.tsv
 # TODO: this one breaks when done in scripts due to some environment variable issues
 #python memnn_luatorch_cpu/full_task_train.py -t babi:task10k:1 -n 8 --num_examples 100 --num_its 1
 
-python drqa/train.py -t squad -b 32
+# if this returns without an error code, you're good!
+python drqa/train.py -t squad -b 32 & sleep 60 ; kill $!
