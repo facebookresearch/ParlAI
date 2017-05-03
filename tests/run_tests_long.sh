3ec854e3084b60468326900b59f3dc0d3c61abba
jaseweston <jaseweston@gmail.com>
2017-05-02 23:11:46 -0400

    mv test mains (#28)

--- tests/run_tests_long.sh
+++ tests/run_tests_long.sh
@@ -6,4 +6,4 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
-python tests/test_data.py
+python test_data.py
