3023f0c05a426ae75280dd467a1a7cee3cd48b2f
jaseweston <jaseweston@gmail.com>
2017-05-30 18:17:10 -0400

    local human agent (#110)
    * local human
    
    * Space

---
+++ parlai/agents/local_human/agents.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+"""Agent does gets the local keyboard input in the act() function.
+   Example: python examples/eval_model.py -m local_human -t babi:Task1k:1 -dt valid
+"""
+
+from parlai.core.agents import Agent
+from parlai.core.worlds import display_messages
+
+class LocalHumanAgent(Agent):
+
+    def __init__(self, opt, shared=None):
+        super().__init__(opt)
+        self.id = 'localHuman'
+
+    def observe(self, msg):
+        print(display_messages([msg]))
+
+    def act(self):
+        obs = self.observation
+        reply = {}
+        reply['id'] = self.getID()
+        reply['text'] = input("Enter Your Reply: ")
+        return reply
