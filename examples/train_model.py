0c6f27564103ac0c8bc2ef6bc9e6371538828ebd
Jason Weston <jase@fb.com>
2017-05-30 23:00:32 -0400

    main train script

---
+++ examples/train_model.py
@@ -0,0 +1,77 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+"""Train a model.
+   After training, computes validation and test error.
+   Run with, e.g.:
+   python examples/train_model.py -m ir_baseline -t dialog_babi:Task:1 -mf "/tmp/model"
+
+   TODO List:
+   - Add model specific params
+   - Validate & Log while training
+   - Keep best model from validation error, if desired
+"""
+
+from parlai.core.agents import create_agent
+from parlai.core.worlds import create_task
+from parlai.core.params import ParlaiParser
+from parlai.core.utils import Timer
+
+
+def run_eval(agent, opt, datatype):
+    ''' Eval on validation/test data. '''
+    print("[running eval: " + datatype + "]")
+    opt['datatype'] = datatype
+    valid_world = create_task(opt, agent)
+    for _ in range(len(valid_world)):
+        valid_world.parley()
+        if opt['display_examples']:
+            print(valid_world.display() + "\n~~")
+            print(valid_world.report())
+        if valid_world.epoch_done():
+            break
+    valid_world.shutdown()
+    metrics = datatype + ":" + str(valid_world.report())
+    print(metrics)
+    # Write out metrics
+    if opt['model_file']:
+        f = open(opt['model_file'] + '.' + datatype, "a+")
+        f.write(metrics)
+        f.close()
+
+def main():
+    # Get command line arguments
+    parser = ParlaiParser(True, True)
+    parser.add_argument('-d', '--display-examples',
+                        type='bool', default=False)
+    parser.add_argument('-e', '--num-epochs', default=1)
+    parser.add_argument('-mtt', '--max-train-time',
+                        type=float, default=float('inf'))
+    opt = parser.parse_args()
+    # Create model and assign it to the specified task
+    agent = create_agent(opt)
+    world = create_task(opt, agent)
+
+    train_time = Timer()
+    print("[training...]")
+    for _ in range(len(world) * opt['num_epochs']):
+        world.parley()
+        if opt['display_examples']:
+            print(world.display() + "\n~~")
+        if train_time.time() > opt['max_train_time']:
+            print("[max_train_time elapsed: " + str(train_time.time()) + "]")
+            break
+    world.shutdown()
+
+    if opt['model_file']:
+        agent.save(opt['model_file'])
+    run_eval(agent, opt, 'valid')
+    run_eval(agent, opt, 'test')
+
+
+if __name__ == '__main__':
+    main()
+
+
--- parlai/agents/drqa/utils.py
+++ parlai/agents/drqa/utils.py
@@ -183,34 +183,3 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
-
-
-class Timer(object):
-    """Computes elapsed time."""
-    def __init__(self):
-        self.running = True
-        self.total = 0
-        self.start = time.time()
-
-    def reset(self):
-        self.running = True
-        self.total = 0
-        self.start = time.time()
-        return self
-
-    def resume(self):
-        if not self.running:
-            self.running = True
-            self.start = time.time()
-        return self
-
-    def stop(self):
-        if self.running:
-            self.running = False
-            self.total += time.time() - self.start
-        return self
-
-    def time(self):
-        if self.running:
-            return self.total + time.time() - self.start
-        return self.total
