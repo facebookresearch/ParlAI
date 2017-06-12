9dc6d54bd0fdc38be1764be8356d25d46919b35a
Alexander Miller <ahm@fb.com>
2017-06-12 17:30:22 -0400

    Update agents.py

--- parlai/tasks/mnist_qa/agents.py
+++ parlai/tasks/mnist_qa/agents.py
@@ -4,8 +4,7 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
-"""
-This is a simple question answering task on the MNIST dataset.
+"""This is a simple question answering task on the MNIST dataset.
 In each episode, agents are presented with a number, which they are asked to
 identify.
 
