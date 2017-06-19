c9c86c6e619827c1e8e9b3a5515a0a78e7ed0908
Alexander Miller <ahm@fb.com>
2017-06-19 18:06:03 -0400

    move predictor to utils, more documentation for predictor module

--- examples/predictor.py
+++ examples/predictor.py
@@ -3,9 +3,9 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
-"""Basic example which allows local human keyboard input to talk to a trained model.
-
-For example:
+"""Parallel of `interactive.py`, a basic example which allows local human
+keyboard input to talk to a trained model. The command-line functionality is
+essentially the same, for example:
 `wget https://s3.amazonaws.com/fair-data/parlai/_models/drqa/squad.mdl`
 `python examples/predictor.py -m drqa -mf squad.mdl`
 
@@ -13,6 +13,14 @@ Then enter something like:
 "Bob is Blue.\nWhat is Bob?"
 as the user input (or in general for the drqa model, enter
 a context followed by '\n' followed by a question all as a single input.)
+
+However, this example demonstrates how to use the predictor module for this
+example, which can use a set of arguments other than the command line to provide
+model predictions from within a running program.
+
+As shown in this example, just create a predictor with arguments which specify
+the model (e.g. p = Predictor(model='drqa')), and the Predictor module will set
+up the model and handle communication with it.
 """
 
 from parlai.core.predictor import Predictor
--- parlai/core/utils.py
+++ parlai/core/utils.py
@@ -4,7 +4,46 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
+from parlai.core.params import ParlaiParser
+
+from parlai.core.params import ParlaiParser
+from parlai.core.agents import create_agent
+from parlai.agents.local_human.local_human import LocalHumanAgent
+
+import sys
 import time
+
+
+class Predictor(object):
+    """Provides functionality for setting up a running version of a model and
+    requesting predictions from that model on live data.
+    Note that this maintains no World state (does not use a World), merely
+    providing the observation directly to the model and getting a response.
+    """
+
+    def __init__(self, args=None, **kwargs):
+        """Initializes the predictor, setting up opt automatically if necessary.
+        Args is expected to be in the same format as sys.argv.
+        """
+        if args is None:
+            args = []
+        for k, v in kwargs.items():
+            args.append('--' + str(k).replace('_', '-'))
+            args.append(str(v))
+        parser = ParlaiParser(True, True, model_argv=args)
+        self.opt = parser.parse_args(args)
+        self.agent = create_agent(self.opt)
+
+    def predict(self, observation):
+        """From a ParlAI-standard observation dict, returns a prediction from
+        the model.
+        """
+        if 'episode_done' not in observation:
+            observation['episode_done'] = True
+        self.agent.observe(observation)
+        reply = self.agent.act()
+        return reply
+
 
 class Timer(object):
     """Computes elapsed time."""
@@ -35,4 +74,3 @@ class Timer(object):
         if self.running:
             return self.total + time.time() - self.start
         return self.total
-
