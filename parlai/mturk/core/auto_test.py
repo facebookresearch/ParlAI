4621e6a1aae08cd7792bd81124d2481744e45976
Will Feng <willfeng@fb.com>
2017-07-10 15:36:41 -0400

    fixed comment

--- parlai/mturk/core/auto_test.py
+++ parlai/mturk/core/auto_test.py
@@ -3,9 +3,9 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
-'''
+"""
 Script for auto-completing HITs. Please change the test flow according to your task.
-'''
+"""
 try:
     from selenium import webdriver
     import chromedriver_installer
