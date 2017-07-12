b7faaaa575186e9736de359ce690c5a8edc1dfec
Alexander Miller <ahm@fb.com>
2017-07-12 17:00:26 -0400

    fix test for init files to exclude mturk html dir (#203)

--- tests/test_init.py
+++ tests/test_init.py
@@ -9,13 +9,16 @@ import unittest
 
 
 class TestInit(unittest.TestCase):
-    """Make sure the package is alive."""
+    """Make sure all python packages have init files."""
 
     def test_init_everywhere(self):
         from parlai.core.params import ParlaiParser
         opt = ParlaiParser().parse_args()
         for root, subfolder, files in os.walk(os.path.join(opt['parlai_home'], 'parlai')):
             if not root.endswith('__pycache__'):
+                if os.path.basename(root) == 'html':
+                    # skip mturk core's html folder--not a python module
+                    continue
                 assert '__init__.py' in files, 'Dir {} is missing __init__.py'.format(root)
 
 
