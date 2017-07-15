6b472ff6fe1c3056f702768899c277a3cacf86ec
Sanyam Satia <ssatia@fb.com>
2017-07-15 12:41:05 -0700

    Change evidence handling and add verified teachers

--- parlai/tasks/triviaqa/agents.py
+++ parlai/tasks/triviaqa/agents.py
@@ -19,62 +19,114 @@ def _path(opt):
     return (os.path.join(opt['datapath'], 'TriviaQA', 'qa'),
             os.path.join(opt['datapath'], 'TriviaQA', 'evidence'))
 
-def setup_data_common(dataset, path, suffix, evidence_dir):
-    dataset_path = os.path.join(path,
-                                dataset + '-' + suffix + '.json')
-    print('loading: ' + dataset_path)
-    with open(dataset_path) as data_file:
-        data = json.load(data_file)['Data']
-    for datapoint in data:
-        question = datapoint['Question']
-        answers = datapoint['Answer']['Aliases']
-        if dataset == 'web':
-            evidence_list = datapoint['SearchResults']
-        else:
-            evidence_list = datapoint['EntityPages']
-
-        if len(evidence_list) == 0:
-            continue
-
-        evidence_num = random.randrange(len(evidence_list))
-        evidence_filename = evidence_list[evidence_num]['Filename']
-        evidence_file_path = os.path.join(evidence_dir, dataset,
-                                          evidence_filename)
-        with open(evidence_file_path) as evidence_file:
-            evidence = evidence_file.read()
-            yield (evidence + '\n' + question, answers), True
-
 
 class WebTeacher(DialogTeacher):
     def __init__(self, opt, shared=None):
-        if opt['datatype'].startswith('train'):
-            self.suffix = 'train'
-        else:
-            self.suffix = 'dev'
+        if not hasattr(self, 'prefix'):
+            self.prefix = ''
+            if opt['datatype'].startswith('train'):
+                self.suffix = 'train'
+            else:
+                self.suffix = 'dev'
 
         opt['datafile'], self.evidence_dir = _path(opt)
         self.id = 'triviaqa'
         super().__init__(opt, shared)
 
     def setup_data(self, path):
-        return setup_data_common('web', path, self.suffix, self.evidence_dir)
+        dataset = 'web'
+        dataset_path = os.path.join(path,
+                                    self.prefix + dataset + '-' + self.suffix +
+                                    '.json')
+        print('loading: ' + dataset_path)
+        with open(dataset_path) as data_file:
+            data = json.load(data_file)['Data']
+        for datapoint in data:
+            question = datapoint['Question']
+            answers = datapoint['Answer']['Aliases']
+            evidence_list = datapoint['SearchResults']
+
+            if len(evidence_list) == 0:
+                continue
+
+            for evidence_item in evidence_list:
+                evidence_file_path = os.path.join(self.evidence_dir, dataset,
+                                                  evidence_item['Filename'])
+                with open(evidence_file_path) as evidence_file:
+                    evidence = 'Title: %s\n' % evidence_item['Title']
+                    evidence += evidence_file.read()
+                    yield (evidence + '\n' + question, answers), True
+
+
+class VerifiedWebTeacher(WebTeacher):
+    def __init__(self, opt, shared=None):
+        self.prefix = 'verified-'
+        self.suffix = 'dev'
+        if opt['datatype'] != 'valid':
+            print('WARNING: Verified teacher only provides dev data')
+
+        opt['datafile'], self.evidence_dir = _path(opt)
+        self.id = 'triviaqa'
+        super().__init__(opt, shared)
 
 
 class WikipediaTeacher(DialogTeacher):
     def __init__(self, opt, shared=None):
-        if opt['datatype'].startswith('train'):
-            self.suffix = 'train'
-        else:
-            self.suffix = 'dev'
+        if not hasattr(self, 'prefix'):
+            self.prefix = ''
+            if opt['datatype'].startswith('train'):
+                self.suffix = 'train'
+            else:
+                self.suffix = 'dev'
 
         opt['datafile'], self.evidence_dir = _path(opt)
         self.id = 'triviaqa'
         super().__init__(opt, shared)
 
     def setup_data(self, path):
-        return setup_data_common('wikipedia', path, self.suffix,
-                                 self.evidence_dir)
+        dataset = 'wikipedia'
+        dataset_path = os.path.join(path,
+                                    self.prefix + dataset + '-' + self.suffix +
+                                    '.json')
+        print('loading: ' + dataset_path)
+        with open(dataset_path) as data_file:
+            data = json.load(data_file)['Data']
+        for datapoint in data:
+            question = datapoint['Question']
+            answers = datapoint['Answer']['Aliases']
+            evidence_list = datapoint['EntityPages']
 
+            if len(evidence_list) == 0:
+                continue
+
+            evidence = ''
+            for evidence_item in evidence_list:
+                evidence_file_path = os.path.join(self.evidence_dir, dataset,
+                                                  evidence_item['Filename'])
+                with open(evidence_file_path) as evidence_file:
+                    evidence += 'Title: %s\n' % evidence_item['Title']
+                    evidence += evidence_file.read() + '\n\n'
+
+            yield (evidence + question, answers), True
+
+
+class VerifiedWikipediaTeacher(WikipediaTeacher):
+    def __init__(self, opt, shared=None):
+        self.prefix = 'verified-'
+        self.suffix = 'dev'
+        if opt['datatype'] != 'valid':
+            print('WARNING: Verified teacher only provides dev data')
+
+        opt['datafile'], self.evidence_dir = _path(opt)
+        self.id = 'triviaqa'
+        super().__init__(opt, shared)
+
+
+class VerifiedTeacher(MultiTaskTeacher):
+    def __init__(self, opt, shared=None):
+        opt = copy.deepcopy(opt)
+        opt['task'] = 'triviaqa:VerifiedWikipedia,triviaqa:VerifiedWeb'
+        super().__init__(opt, shared)
 
 class DefaultTeacher(MultiTaskTeacher):
     def __init__(self, opt, shared=None):
