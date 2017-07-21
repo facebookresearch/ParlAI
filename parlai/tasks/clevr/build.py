a7a9875222a4217c9435937c359859503f93be50
Filipe de Avila Belbute Peres <filipeabperes@gmail.com>
2017-07-21 18:50:42 -0400

    Added CLEVR task

---
+++ parlai/tasks/clevr/build.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+# Download and build the data if it does not exist.
+
+import parlai.core.build_data as build_data
+import os
+
+from parlai.tasks.vqa_v1.build import buildImage
+
+
+def build(opt):
+    dpath = os.path.join(opt['datapath'], 'CLEVR')
+    version = 'v1.0'
+
+    if not build_data.built(dpath, version_string=version):
+        print('[building data: ' + dpath + ']')
+        # An older version exists, so remove these outdated files.
+        if build_data.built(dpath):
+            build_data.remove_dir(dpath)
+        build_data.make_dir(dpath)
+
+        # Download the data.
+        fname = 'CLEVR_v1.0.zip'
+        url = 'https://s3-us-west-1.amazonaws.com/clevr/'
+
+        build_data.download(url + fname, dpath, fname)
+        build_data.untar(dpath, fname)
+
+        # Mark the data as built.
+        build_data.mark_done(dpath, version_string=version)
