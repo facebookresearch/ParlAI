eb7ee3d84210cf37b9a405b93a72b27a04576262
Alexander Miller <ahm@fb.com>
2017-08-04 14:41:44 -0400

    update msmarco description

--- parlai/tasks/task_list.py
+++ parlai/tasks/task_list.py
@@ -232,6 +232,6 @@ task_list = [
         "display_name": "MS_MARCO",
         "task": "ms_marco",
         "tags": [ "All",  "QA" ],
-        "description": "A Reading Comprehension Dataset for the Artificial Intelligence research community. From Nguyen et al. '16. Link: https://arxiv.org/abs/1611.09268"
+        "description": "A large scale Machine Reading Comprehension Dataset with questions sampled from real anonymized user queries and contexts from web documents. From Nguyen et al. '16. Link: https://arxiv.org/abs/1611.09268"
     }
 ]
