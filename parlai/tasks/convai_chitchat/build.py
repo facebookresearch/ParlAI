6918cffb2acc27309df321421478a6300fa71873
bavadim@gmail.com <bavadim@gmail.com>
2017-10-20 17:58:53 +0300

    fix comments and clenup PR

--- parlai/tasks/convai_chitchat/build.py
+++ parlai/tasks/convai_chitchat/build.py
@@ -1,9 +1,8 @@
-# Copyright (c) 2017-present, Facebook, Inc.
+# Copyright (c) 2017-present, Moscow Institute of Physics and Technology.
 # All rights reserved.
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
-# Download and build the data if it does not exist.
 
 import parlai.core.build_data as build_data
 import os
