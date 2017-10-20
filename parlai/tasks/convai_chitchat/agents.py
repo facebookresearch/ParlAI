caef3c6c3909c1dce6c66f488735d6db33a0c853
bavadim@gmail.com <bavadim@gmail.com>
2017-10-20 16:12:34 +0300

    initial commit

---
+++ parlai/tasks/convai_chitchat/agents.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2017-present, Moscow Institute of Physics and Technology.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+from parlai.core.dialog_teacher import DialogTeacher
+
+class ConvaiChitChatTeacher(DialogTeacher):
+    def __init__(self, opt, shared=None):
+        self.datatype = opt['datatype']
+        self.data_path = ConvaiChitChatTeacher._path(opt)
+        opt['datafile'] = self.data_path
+        self.id = 'ConvAIChitChat'
+        super().__init__(opt, shared)
+
+    @classmethod
+    def _path(cls, opt):
+        import os
+        from parlai.tasks.convai_chitchat.build import build
+        build(opt)
+        dt = opt['datatype'].split(':')[0]
+
+        if dt == 'train':
+            path = os.path.join(opt['datapath'], 'ConvAIChitChat', 'train.json')
+        elif dt == 'valid':
+            path = os.path.join(opt['datapath'], 'ConvAIChitChat', 'valid.json')
+        elif dt == 'test':
+            path = os.path.join(opt['datapath'], 'ConvAIChitChat', 'test.json')
+        else:
+            raise RuntimeError('Not valid datatype.')
+
+        return path
+
+    def setup_data(self, path):
+        import json
+        print('loading: ' + path)
+
+        with open(path) as data_file:
+            self.dialogs = json.load(data_file)
+
+        for dialog in self.dialogs:
+            prev_utterance = None
+            for i, utterance in enumerate(dialog["thread"]):
+                episode_done = False
+                if i == len(dialog["thread"]) - 1:
+                    episode_done = True
+                res = ((prev_utterance['text'], prev_utterance['userId']), (utterance['text'], utterance['userId']))
+                prev_utterance = utterance
+
+                yield res, episode_done
---
+++ parlai/tasks/convai_chitchat/build.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+# Download and build the data if it does not exist.
+
+import parlai.core.build_data as build_data
+import os
+
+
+def _train_test_valid_split(dialogs):
+    import math
+    import random
+    dialogs = random.shuffle(dialogs)
+    l = len(dialogs)
+    border_valid = math.floor(l * 0.1)
+    valid = dialogs[:border_valid]
+    border_test = border_valid + math.floor(l * 0.1)
+    test = dialogs[border_valid:border_test]
+    train = dialogs[border_test:]
+    return train, test, valid
+
+
+def build(opt):
+    import json
+    dpath = os.path.join(opt['datapath'], 'ConvAIChitChat')
+    version = None
+
+    if not build_data.built(dpath, version_string=version):
+        print('[building data: ' + dpath + ']')
+
+        if build_data.built(dpath):
+            build_data.remove_dir(dpath)
+        build_data.make_dir(dpath)
+
+        fname = 'train_full.json'
+        url = 'https://raw.githubusercontent.com/deepmipt/turing-data/master/' + fname
+        build_data.download(url, dpath, fname)
+
+        with open(os.path.join(dpath, fname)) as dataset:
+            dialogs = json.load(dataset)
+            train, test, valid = _train_test_valid_split(dialogs)
+            with open(os.path.join(dpath, "valid.json")) as outfile:
+                json.dump(valid, outfile)
+            with open(os.path.join(dpath, "test.json")) as outfile:
+                json.dump(test, outfile)
+            with open(os.path.join(dpath, "train.json")) as outfile:
+                json.dump(train, outfile)
+
+        build_data.mark_done(dpath, version_string=version)
