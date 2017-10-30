adc33d0f9d7649bd53d12a4b38a6c30100b7899f
ahm <ahm@fb.com>
2017-10-30 15:24:13 -0400

    move seq2seq to use nn.Module

--- parlai/tasks/opensubtitles/agents.py
+++ parlai/tasks/opensubtitles/agents.py
@@ -9,6 +9,15 @@ from .build import build
 
 import copy
 import os
+import re
+
+
+def _regularize(sent):
+    sent = sent.replace('i&gt;', '').replace('&lt;', '').replace('&gt;', '')
+    sent = re.sub(r'x[0-9|a-z][0-9]', ' ', sent)
+    sent = sent.replace('\\', '')
+    sent = ' '.join(re.findall(r"[\w']+|[.,!?:;]", sent))
+    return sent
 
 
 def _path(opt, filtered):
@@ -25,3 +34,23 @@ class DefaultTeacher(FbDialogTeacher):
         opt['datafile'] = _path(opt, '')
         opt['cands_datafile'] = opt['datafile']
         super().__init__(opt, shared)
+
+    def setup_data(self, path):
+        alternate = []
+        for entry, new in super().setup_data(path):
+            entry[0] = _regularize(entry[0])
+            if len(entry) > 1 and entry[1]:  # labels
+                entry[1] = [_regularize(y) for y in entry[1]]
+            if new:
+                for i, e in enumerate(alternate):
+                    yield e, i == 0
+                alternate.clear()
+            else:
+                alternate.append(entry)
+            yield entry, new
+        if alternate:
+            for i, e in enumerate(alternate):
+                yield e, i == 0
+
+    def load_cands(self, path):
+        return [r for r in (_regularize(c) for c in super().load_cands(path)) if r]
