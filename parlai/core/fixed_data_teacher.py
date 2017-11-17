1831bc2751ba83be832485ab171681a861394af9
Kurt <kshuster@fb.com>
2017-11-17 10:55:40 -0800

    move threadpool loading to fixed data teacher

--- parlai/core/fixed_data_teacher.py
+++ parlai/core/fixed_data_teacher.py
@@ -6,7 +6,41 @@
 
 from .agents import Teacher
 
+import concurrent.futures
+from threading import Thread
+import queue
 import random
+
+class DataLoader(Thread):
+    """A worker thread that provides a threadpool for data loading.
+
+    A teacher may submit a request to the loader, which will return the
+    appropriate data.
+
+    To submit a request, a teacher must provide the following:
+        - a receive function (for receiving the data)
+        - a load function (for loading the data), e.g. ImageLoader.load
+        - args for the load function, e.g. an image path for an image loader
+            -> args can be either a dictionary of arguments for a function, or
+               a list of positional arguments
+    """
+    def __init__(self, opt):
+        Thread.__init__(self, daemon=True)
+        self.num_workers = opt.get('numthreads', 1)
+        self.request_queue = queue.Queue()
+
+    def __len__(self):
+        return len(self.ques['questions'])
+
+    def run(self):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.num_workers) as executor:
+            while True:
+                receive_fn, load_fn, args = self.request_queue.get()
+                if type(args) == dict:
+                    future = executor.submit(load_fn, **args)
+                else:
+                    future = executor.submit(load_fn, *args)
+                receive_fn(future)
 
 
 class FixedDataTeacher(Teacher):
@@ -17,8 +51,8 @@ class FixedDataTeacher(Teacher):
     - Resets a teacher
     - Provides an observe method
     - Computes and retrieves the next episode index for a teacher
-    - TODO Provides a threadpool option for loading data (especially useful for
-      large examples, e.g. images)
+    - Provides a threadpool option for loading data (especially useful for
+      large data, e.g. images)
 
     """
     def __init__(self, opt, shared=None):
@@ -35,6 +69,14 @@ class FixedDataTeacher(Teacher):
         # size so they all process disparate sets of the data
         self.step_size = opt.get('batchsize', 1)
         self.data_offset = opt.get('batchindex', 0)
+
+        self.data_queue = queue.Queue()
+        if shared:
+            self.data_loader = shared['data_loader']
+        else:
+            self.data_loader = DataLoader(opt)
+            self.data_loader.start()
+
         self.reset()
 
     def __len__(self):
@@ -50,6 +92,7 @@ class FixedDataTeacher(Teacher):
         self.episode_idx = self.data_offset - self.step_size
         self.episode_done = True
         self.epochDone = False
+        self.data_queue = queue.Queue()
         try:
             if (self.episode_idx + self.step_size >= len(self) and not self.random):
                 self.epochDone = True
@@ -75,3 +118,15 @@ class FixedDataTeacher(Teacher):
             if self.episode_idx + self.step_size >= num_eps:
                 epoch_done = True
         return self.episode_idx, epoch_done
+
+    def submit_data_request(self, receive_fn, load_fn, args):
+        self.data_loader.request_queue.put((receive_fn, load_fn, args))
+
+    def receive_data(self, future):
+        data = future.result()
+        self.data_queue.put(data)
+
+    def share(self):
+        shared = super().share()
+        shared['data_loader'] = self.data_loader
+        return shared
