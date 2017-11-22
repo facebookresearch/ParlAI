a4b51dc27a25b994752db2e5dc4e984d023fb9b1
Kurt Shuster <kshuster@fb.com>
2017-11-22 17:10:00 -0500

    display num epochs if sampling_data as well

--- examples/train_model.py
+++ examples/train_model.py
@@ -179,8 +179,9 @@ def main(parser):
                         time_left = other_time_left
                 if time_left is not None and max_exs != 0:
                     logs.append('time_left:{}s'.format(math.floor(time_left)))
-                if opt['num_epochs'] > 0 and not sampling_data:
-                    logs.append('num_epochs:{}'.format(num_epochs))
+                if opt['num_epochs'] > 0:
+                    display_epochs = int(total_exs / len(world)) if sampling_data else num_epochs
+                    logs.append('num_epochs:{}'.format(display_epochs))
                 # join log string and add full metrics report to end of log
                 log = '[ {} ] {}'.format(' '.join(logs), train_report)
 
