5020dae15be3f8b1b5eb029855ca7e3d85b74473
kimiyoung <kimiyoung@yeah.net>
2017-11-29 16:32:33 -0500

    add copyright, remove gitignore

--- projects/mastering_the_dungeon/tasks/graph_world2/worlds.py
+++ projects/mastering_the_dungeon/tasks/graph_world2/worlds.py
@@ -1,3 +1,10 @@
+##
+## Copyright (c) 2017-present, Facebook, Inc.
+## All rights reserved.
+## This source code is licensed under the BSD-style license found in the
+## LICENSE file in the root directory of this source tree. An additional grant
+## of patent rights can be found in the PATENTS file in the same directory.
+##
 from parlai.core.worlds import World, ExecutableWorld, validate
 from projects.mastering_the_dungeon..tasks.graph_world2.graph import construct_graph
 
