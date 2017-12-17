ef2046dc42bfe49754c9dadae9805545dc49ecfe
Eve-B <e.bakhturina@gmail.com>
2017-12-17 00:26:08 -0500

    memnn_extended added

--- parlai/tasks/dbll_babi/agents.py
+++ parlai/tasks/dbll_babi/agents.py
@@ -55,8 +55,113 @@ class TaskTeacher(FbDialogTeacher):
         opt['datafile'] = _path(os.path.join('babi', 'babi1'), params, opt)
         opt['cands_datafile'] = _path(os.path.join('babi', 'babi1'), params,
                                       opt, 'train')
+        self.opt = opt
         super().__init__(opt, shared)
 
+    def setup_data(self, path):
+        if len(self.opt['task'].split(':')) == 4:
+            return self.setup_data_with_feedback(path)
+        else:
+            return super().setup_data(path)
+
+    def setup_data_with_feedback(self, path):
+        """Reads data in the fbdialog format.
+        This method is very similar to FbDialogTeacher.setup_data(..). 
+        The difference is that in this method the feedback is appended to the query 
+        from the current example; in the default setup the feedback is appended to 
+        the x from the next example.
+        
+        To append feedback to the current example, modify the task name like this:
+          python examples/display_data.py -t dbll_babi:task:2_p0.5:f 
+        Default setup: 
+          python examples/display_data.py -t dbll_babi:task:2_p0.5 
+
+        """
+        print("[loading fbdialog data:" + path + "]")
+        with open(path) as read:
+            start = True
+            x = ''
+
+            y = None
+            cands = None
+
+            reward = 0
+            dialog_index = 0
+            read_feedback = False
+            for line in read:
+                line = line.strip().replace('\\n', '\n')
+                if len(line) == 0:
+                    continue
+
+                # first, get conversation index -- '1' means start of episode
+                space_idx = line.find(' ')
+                conv_id = line[:space_idx]
+
+                # split line into constituent parts, if available:
+                # x<tab>y<tab>reward<tab>label_candidates
+                # where y, reward, and label_candidates are optional
+                split = line[space_idx + 1:].split('\t')
+
+                # remove empty items and strip each one
+                for i in range(len(split)):
+                    word = split[i].strip()
+                    if len(word) == 0:
+                        split[i] = ''
+                    else:
+                        split[i] = word
+                # Empty reward string same as None
+                if len(split) > 2 and split[2] == '':
+                    split[2] = None
+
+                # now check if we're at a new episode
+                if conv_id == '1':
+                    dialog_index += 1
+                    x = x.strip()
+                    if x:
+                        yield [x, None, reward], start
+                    start = True
+                    reward = 0
+                    # start a new episode
+                    if self.cloze:
+                        x = 'Fill in the blank in the last sentence.\n{x}'.format(
+                            x=split[0]
+                        )
+                    else:
+                        x = split[0]
+                else:
+                    if x:
+                        # otherwise add current x to what we have so far
+                        x = '{x}\n{next_x}'.format(x=x, next_x=split[0])
+                    else:
+                        x = split[0]
+                if len(split) > 2 and split[2]:
+                    reward += float(split[2])
+
+                if len(split) > 1 and split[1]:
+                    read_feedback = True
+                    # split labels
+                    y = split[1].split('|')
+                    if len(split) > 3:
+                        # split label_candidates
+                        cands = split[3].split('|')
+
+                if read_feedback and not split[1]:
+                    split[0] = x
+                    split[1] = y
+                    if len(split) > 2:
+                        split[2] = reward
+                    else:
+                        split.append(reward)
+                    if start:
+                        yield split, True
+                        start = False
+                    else:
+                        yield split, False
+                    # reset x in case there is unlabeled data still left
+                    x = ''
+                    reward = 0
+                    y = None
+                    read_feedback = False
 
 # Defaults to task 2 with p=0.5.
 class DefaultTeacher(FbDialogTeacher):
