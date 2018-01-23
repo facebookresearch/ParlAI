2d17a71b806b814ff44f2670b3fa7e369ac0e7b2
jaseweston <jaseweston@gmail.com>
2018-01-23 17:02:20 -0500

    oops, comma

--- parlai/tasks/task_list.py
+++ parlai/tasks/task_list.py
@@ -346,7 +346,7 @@ task_list = [
         "task": "scan",
         "tags": [ "Goal", "All" ],
         "description": "SCAN is a set of simple language-driven navigation tasks for studying compositional learning and zero-shot generalization. The SCAN tasks were inspired by the CommAI environment, which is the origin of the acronym (Simplified versions of the CommAI Navigation tasks). See the paper: https://arxiv.org/abs/1711.00350 or data: https://github.com/brendenlake/SCAN"
-    }
+    },
     {
         "id": "Persona-Chat",
         "display_name": "Persona-Chat",
