f04873fd1da76a8813002730991626b693098791
Jason Weston <jase@fb.com>
2018-02-08 16:49:27 -0600

    cleanup

--- parlai/messenger/tasks/overworld_demo/worlds.py
+++ parlai/messenger/tasks/overworld_demo/worlds.py
@@ -3,8 +3,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
-#
-# py parlai/messenger/tasks/overworld_demo/run.py --debug --verbose
 
 from parlai.core.worlds import World
 from parlai.messenger.core.worlds import OnboardWorld
@@ -72,7 +70,92 @@ class MessengerEchoTaskWorld(World):
         self.agent.shutdown()
 
 
-# ---------- Human Chat world -------- #
+# ----------- Onboard Data Demo ----------- #
+class MessengerOnboardDataOnboardWorld(OnboardWorld):
+    """Example messenger onboarding that collects and returns data for use
+    in the real task world
+    """
+    def __init__(self, opt, agent):
+        self.agent = agent
+        self.episodeDone = False
+        self.turn = 0
+
+    @staticmethod
+    def run(agent, task_id):
+        '''Runs an instance of the onboarding world. Data returned from
+        here will be made available in the agent.onboard_data field'''
+        world = MessengerOnboardDataOnboardWorld(opt=None, agent=agent)
+        while not world.episode_done():
+            world.parley()
+        world.shutdown()
+        return {'name': world.user_name, 'color': world.fav_color}
+
+    def parley(self):
+        if self.turn == 0:
+            self.agent.observe({
+                'id': 'Onboarding',
+                'text': 'Welcome to the onboarding world the onboarding '
+                        'data demo.\nEnter your name.',
+            })
+            a = self.agent.act()
+            while a is None:
+                a = self.agent.act()
+            self.user_name = a['text']
+            self.turn = self.turn + 1
+        elif self.turn == 1:
+            self.agent.observe({
+                'id': 'Onboarding',
+                'text': '\nEnter your favorite color.',
+            })
+            a = self.agent.act()
+            while a is None:
+                a = self.agent.act()
+            self.fav_color = a['text']
+            self.episodeDone = True
+
+
+class MessengerOnboardDataTaskWorld(World):
+    """Example one person world that relays data given in the onboard world."""
+
+    MAX_AGENTS = 1
+
+    def __init__(self, opt, agent):
+        self.agent = agent
+        self.episodeDone = False
+
+    @staticmethod
+    def run(messenger_manager, opt, agents, task_id):
+        agent = agents[0]
+        world = MessengerOnboardDataTaskWorld(
+            opt=opt,
+            agent=agent
+        )
+        while not world.episode_done():
+            world.parley()
+        world.shutdown()
+
+    @staticmethod
+    def assign_roles(agents):
+        agents[0].disp_id = 'DataAgent'
+
+    def parley(self):
+        name = self.agent.onboard_data['name']
+        color = self.agent.onboard_data['color']
+        self.agent.observe({
+            'id': 'World',
+            'text': 'During onboarding, you said your name was {} and your '
+                    'favorite color was {}'.format(name, color)
+        })
+        self.episodeDone = True
+
+    def episode_done(self):
+        return self.episodeDone
+
+    def shutdown(self):
+        self.agent.shutdown()
+
+
+# ---------- Chat world -------- #
 class MessengerChatOnboardWorld(OnboardWorld):
     """Example messenger onboarding world for chat task, displays intro and
     explains instructions.
@@ -156,96 +239,14 @@ class MessengerChatTaskWorld(World):
             agent.shutdown()
 
 
-# ---------- Bot Chat world -------- #
-class MessengerBotChatOnboardWorld(OnboardWorld):
-    """Example messenger onboarding world for Echo task, displays.
-    onboarding worlds that only exist to send an introduction message.
-    """
-    @staticmethod
-    def run(agent, task_id):
-        world = MessengerBotChatOnboardWorld(opt=None, agent=agent)
-        while not world.episode_done():
-            world.parley()
-        world.shutdown()
-
-    def parley(self):
-        self.agent.observe({
-            'id': 'Onboarding',
-            'text': "Hey how's it going?"
-                    "Use [DONE] to finish the chat."
-        })
-        self.episodeDone = True
-
-from parlai.core.params import ParlaiParser
-from parlai.core.agents import create_agent
-from parlai.core.worlds import create_task
-
-class MessengerBotChatTaskWorld(World):
-    """Example one person world that uses only user input."""
-
-    MAX_AGENTS = 1
-
-    def __init__(self, opt, agent):
-        self.agent = agent
-        self.episodeDone = False
-        print("init")
-        # create modela
-        opt = {}
-        opt["datapath"]="/Users/jase/src/ParlAI/data"
-        opt["model"]="/Users/jase/src/ParlAI/parlai_external.projects.personachat.memnn1hop.memnn1hop:Memnn1hopAgent"
-        opt["model_file"]="/Users/jase/src/ParlAI/parlai_external/data/personachat/memnn2hop_sweep/persona-none_rephraseTrn-True_rephraseTst-False_lr-0.1_esz-500_margin-0.1_tfidf-False_shareEmb-True_hops0_lins0/model"
-        opt['fixed_candidates_file'] = "/Users/jase/src/ParlAI/data/personachat/cands.txt"
-        print(opt)
-        self.model = create_agent(opt)
-
-    @staticmethod
-    def run(messenger_manager, opt, agents, task_id):
-        agent = agents[0]
-        world = MessengerBotChatTaskWorld(
-            opt=opt,
-            agent=agent
-        )
-        while not world.episode_done():
-            world.parley()
-        world.shutdown()
-
-    @staticmethod
-    def assign_roles(agents):
-        agents[0].disp_id = 'EchoAgent'
-
-    def parley(self):
-        a = self.agent.act()
-        if a is not None:
-            if '[DONE]' in a['text']:
-                self.episodeDone = True
-            else:
-                print("===act====")
-                print(a)
-                print("~~~~~~~~~~~")
-                self.model.observe(a)
-                response = self.model.act()
-                print("===response====")
-                print(response)
-                print("~~~~~~~~~~~")
-                response['id'] = ''
-                # response['text'] = "lol"
-                self.agent.observe(response)
-
-    def episode_done(self):
-        return self.episodeDone
-
-    def shutdown(self):
-        self.agent.shutdown()
-
-
-
 # ---------- Overworld -------- #
 class MessengerOverworld(World):
     """World to handle moving agents to their proper places"""
     DEMOS = {
         'echo': (MessengerEchoOnboardWorld, MessengerEchoTaskWorld),
-        'humanchat': (MessengerChatOnboardWorld, MessengerChatTaskWorld),
-        'botchat': (MessengerBotChatOnboardWorld, MessengerBotChatTaskWorld),
+        'onboard data': (MessengerOnboardDataOnboardWorld,
+                         MessengerOnboardDataTaskWorld),
+        'chat': (MessengerChatOnboardWorld, MessengerChatTaskWorld),
     }
 
     def __init__(self, opt, agent):
