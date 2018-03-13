875d086b0318a52eff7f522d73a247fbd0ee5c22
Emily Dinan <edinan@fb.com>
2018-03-13 08:43:04 -0600

    make directory in ParlAI/data for storing models

--- projects/personachat/scripts/language_model_opensub2018_interactive.sh
+++ projects/personachat/scripts/language_model_opensub2018_interactive.sh
@@ -2,10 +2,13 @@
 # Language model trained on Opensubtitles 2018 dataset
 # Run from ParlAI directory
 
-if [ ! -f languagemodel_esz512_hid1024_nl2.pt ];  then
-  wget https://s3.amazonaws.com/fair-data/parlai/_models/personachat/languagemodel_esz512_hid1024_nl2.pt
+if [ ! -d data/models/opensubtitles2018 ]; then
+  mkdir -p data/models/opensubtitles2018
 fi
-if [ ! -f opensubtitles2018.dict ]; then
-  wget https://s3.amazonaws.com/fair-data/parlai/_models/personachat/opensubtitles2018.dict
+if [ ! -f data/models/opensubtitles2018/languagemodel_esz512_hid1024_nl2.pt ];  then
+  wget https://s3.amazonaws.com/fair-data/parlai/_models/personachat/languagemodel_esz512_hid1024_nl2.pt -P data/models/opensubtitles2018
 fi
-python examples/interactive.py -mf 'languagemodel_esz512_hid1024_nl2.pt' -m 'language_model' --dict-file 'opensubtitles2018.dict' --sampling-mode True
+if [ ! -f data/models/opensubtitles2018/opensubtitles2018.dict ]; then
+  wget https://s3.amazonaws.com/fair-data/parlai/_models/personachat/opensubtitles2018.dict -P data/models/opensubtitles2018
+fi
+python examples/interactive.py -mf 'data/models/opensubtitles2018/languagemodel_esz512_hid1024_nl2.pt' -m 'language_model' --dict-file 'data/models/opensubtitles2018/opensubtitles2018.dict' --sampling-mode True
--- projects/personachat/scripts/profilememory_interactive.sh
+++ projects/personachat/scripts/profilememory_interactive.sh
@@ -2,10 +2,13 @@
 # Profile memory model trained on personachat using persona 'self'
 # Run from ParlAI directory
 
-if [ ! -f profilememory_learnreweight_sharelt_encdropout0.4_s2s_usepersona_self_useall_attn_general_lstm_1024_1_1e-3_0.1 ];  then
-  wget https://s3.amazonaws.com/fair-data/parlai/_models/personachat/profilememory_learnreweight_sharelt_encdropout0.4_s2s_usepersona_self_useall_attn_general_lstm_1024_1_1e-3_0.1
+if [ ! -d data/models/personachat ]; then
+  mkdir -p data/models/personachat
 fi
-if [ ! -f fulldict.dict ]; then
-  wget https://s3.amazonaws.com/fair-data/parlai/_models/personachat/fulldict.dict
+if [ ! -f data/models/personachat/profilememory_learnreweight_sharelt_encdropout0.4_s2s_usepersona_self_useall_attn_general_lstm_1024_1_1e-3_0.1 ];  then
+  wget https://s3.amazonaws.com/fair-data/parlai/_models/personachat/profilememory_learnreweight_sharelt_encdropout0.4_s2s_usepersona_self_useall_attn_general_lstm_1024_1_1e-3_0.1 -P data/models/personachat
 fi
-python examples/interactive.py -mf 'profilememory_learnreweight_sharelt_encdropout0.4_s2s_usepersona_self_useall_attn_general_lstm_1024_1_1e-3_0.1' -m 'projects.personachat.persona_seq2seq:PersonachatSeqseqAgentSplit' --dict-file 'fulldict.dict' --interactive-mode True
+if [ ! -f data/models/personachat/fulldict.dict ]; then
+  wget https://s3.amazonaws.com/fair-data/parlai/_models/personachat/fulldict.dict -P data/models/personachat
+fi
+python examples/interactive.py -mf 'data/models/personachat/profilememory_learnreweight_sharelt_encdropout0.4_s2s_usepersona_self_useall_attn_general_lstm_1024_1_1e-3_0.1' -m 'projects.personachat.persona_seq2seq:PersonachatSeqseqAgentSplit' --dict-file 'data/models/personachat/fulldict.dict' --interactive-mode True
