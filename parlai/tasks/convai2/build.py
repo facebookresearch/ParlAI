e0b87dbd3efbae95a1abbc164081e471f82eddb7
Emily Dinan <edinan@fb.com>
2018-03-14 12:40:38 -0600

    change convai2 build version

--- parlai/tasks/convai2/build.py
+++ parlai/tasks/convai2/build.py
@@ -12,7 +12,7 @@ def build_fb_format():
     pass
 
 def build(opt):
-    version = 'v1.0'
+    version = 'v2.0'
     dpath = os.path.join(opt['datapath'], 'ConvAI2')
 
     if not build_data.built(dpath, version):
