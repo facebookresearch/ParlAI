2ea44283c428e29d9e795cd67dd220e61e01dd47
Emily Dinan <edinan@fb.com>
2018-03-26 15:58:18 -0400

    small fix for personachat collection task

--- parlai/mturk/tasks/personachat_chat/extract_and_save_personas.py
+++ parlai/mturk/tasks/personachat_chat/extract_and_save_personas.py
@@ -52,7 +52,6 @@ def main(opt):
     teacher_name += 'Revised' if opt.get('revised') else 'Original'
     opt['task'] = teacher_name
     assert 'personas_path' in opt, 'Must specify personas path'
-    opt['personas_path'] = personas_path
     opt['datatype'] = 'train:ordered:stream'
     opt['numthreads'] = 1
     opt['batchsize'] = 1
