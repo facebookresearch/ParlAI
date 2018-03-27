8ba319dda185255a11774314c5857fc1cbb47928
Alexander Miller <ahm@fb.com>
2018-03-27 12:29:21 -0600

    fix condition

--- projects/convai2/seq2seq/eval.sh
+++ projects/convai2/seq2seq/eval.sh
@@ -4,7 +4,8 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
-if [ ! -f convai2_self_seq2seq_model ]; then
+if [ ! -f /tmp/convai2_self_seq2seq_model ]; then
+	echo "Downloading model to /tmp/convai2_self_seq2seq_model. Should get 29.73 validation ppl."
 	wget https://s3.amazonaws.com/fair-data/parlai/_models/convai2/convai2_self_seq2seq_model -O /tmp/convai2_self_seq2seq_model 
 fi
 python ~/ParlAI/examples/eval_model.py -t convai2:self -dt valid -bs 128 -m seq2seq -mf /tmp/convai2_self_seq2seq_model 
