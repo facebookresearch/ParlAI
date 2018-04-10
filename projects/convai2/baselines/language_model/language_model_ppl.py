e1017f2286cda84d17063cdeb100089e0454c02d
Emily Dinan <edinan@fb.com>
2018-04-10 12:36:51 -0600

    stashing

--- projects/convai2/baselines/language_model/language_model_ppl.py
+++ projects/convai2/baselines/language_model/language_model_ppl.py
@@ -80,38 +80,39 @@ class LanguageModelEntry(LanguageModelAgent):
 
 
 class PerplexityWorld(World):
-    """Instead of calling act/observe on each agent, this world just calls
+    """Instead of just calling act/observe on each agent, this world just calls
     act on the teacher and then calls `next_word_probability` on the agent.
+    The label for each example is parsed by the provided tokenizer, and then
+    for each word in the parsed label the model is given the input and all of
+    the tokens up to the current word and asked to predict the current word.
+    The model must return a probability of any words it thinks are likely in
+    the form of a dict mapping words to scores. If the scores do not sum to 1,
+    they are normalized to do so. If the correct word is not present or has a
+    probablity of zero, it will be assigned a probability of 1e-8.
+    The API of the next_word_probability function which agents must implement
+    is mentioned in the documentation for this file.
     """
-    def __init__(self, opt, agents, shared=None, tokenizer=None):
+    def __init__(self, opt, agents, shared=None):
         super().__init__(opt)
         if shared:
             # Create agents based on shared data.
-            self.task, self.agent = create_agents_from_shared(shared['agents'])
+            self.task, self.agent, self.dict = create_agents_from_shared(shared['agents'])
             self.metrics = shared['metrics']
         else:
-            if len(agents) != 2:
-                raise RuntimeError('There must be exactly two agents.')
+            if len(agents) != 3:
+                raise RuntimeError('There must be exactly three agents.')
             if opt.get('batchsize', 1) > 1:
                 raise RuntimeError('This world only works with bs=1. Try '
                                    'using multiple threads instead, nt>1.')
-            self.task, self.agent = agents
+            self.task, self.agent, self.dict = agents
             if not hasattr(self.agent, 'next_word_probability'):
                 raise RuntimeError('Agent must implement function '
                                    '`next_word_probability`.')
             self.metrics = {'total': 0, 'loss': 0.0, 'num_tokens': 0}
             if opt.get('numthreads', 1) > 1:
                 self.metrics = SharedTable(self.metrics)
-        self.agents = [self.task, self.agent]
+        self.agents = [self.task, self.agent, self.dict]
         self.acts = [None, None]
-
-        if tokenizer is not None:
-            self.tokenize = tokenizer
-        else:
-            self.tokenize = DictionaryAgent.split_tokenize
-        if opt.get('dict_lower'):
-            self._tokenize = self.tokenize
-            self.tokenize = lambda t: self._tokenize(t.lower())
 
     def _lock(self):
         if hasattr(self.metrics, 'get_lock'):
@@ -123,18 +124,27 @@ class PerplexityWorld(World):
 
     def parley(self):
         action = self.task.act()
-        self.acts[0] = action
-        labels = action.get('eval_labels', action.get('labels'))
+        self.acts[0] = action.copy()
+
+        # hide labels from model
+        labels = action.pop('eval_labels', action.pop('labels', None))
         if labels is None:
+            # empty example, move on
             return
-        parsed = self.tokenize(labels[0])
-        parsed.append('__END__')
+
+        parsed = self.dict.tokenize(labels[0])
         loss = 0
         for i in range(len(parsed)):
-            probs = self.agent.next_word_probability(action, parsed[:i])
-            # get probability of correct answer, divide by total prob mass
-            prob_true = probs.get(parsed[i], 0)
-            loss -= prob_true
+            if parsed[i] in self.dict:
+                # only score words which are in the dictionary
+                probs = self.agent.next_word_probability(action, parsed[:i])
+                # get probability of correct answer, divide by total prob mass
+                prob_true = probs.get(parsed[i], 0)
+                if prob_true > 0:
+                    prob_true /= sum(probs.values())
+                    loss -= math.log(prob_true)
+                else:
+                    loss = float('inf')
         with self._lock():
             self.metrics['total'] += 1
             self.metrics['loss'] += loss
@@ -167,37 +177,29 @@ class PerplexityWorld(World):
             if m['total'] > 0:
                 m['loss'] = round_sigfigs(self.metrics['loss'] / self.metrics['num_tokens'], 3)
                 m['ppl'] = round_sigfigs(math.exp(self.metrics['loss'] / self.metrics['num_tokens']), 4)
-                m['num_tokens'] = self.metrics['num_tokens']
         return m
 
 
-if __name__ == '__main__':
-    parser = ParlaiParser(True, True)
-    parser.add_argument('-vme', '--validation-max-exs', type=int, default=-1)
-    parser.set_defaults(
-        task='convai2:self',
-        model='projects.convai2.baselines.language_model.language_model_ppl:LanguageModelEntry',
-        model_file='/checkpoint/edinan/20180328/languagemodel_convai2_328/hs=512_esz=512_nl=3_lr=20_dr=0.3_bs=40/model.checkpoint',
-        dict_file='/checkpoint/edinan/20180328/languagemodel_convai2_328/hs=512_esz=512_nl=3_lr=20_dr=0.3_bs=40/model.dict',
-        datatype='test',
-        batchsize=1,
-    )
+def eval_ppl(parser):
+    """Evaluates the the perplexity and f1 of a model (and hits@1 if model has
+    ranking enabled.
+    """
+    dict_agent = build_dict()
+
+    parser.set_defaults(task='convai2:self:no_cands', hide_labels=False)
     opt = parser.parse_args()
-    # opt['model_type'] = 'seq2seq'
-    # fnames = ['convai2_self_seq2seq_model.tgz', 'dict_convai2_self']
-    # download_models(opt, fnames, 'convai2')
 
     # create agents
     agent = create_agent(opt)
-    world = create_task(opt, agent, default_world=PerplexityWorld)
+    world = create_task(opt, [agent, dict_agent], default_world=PerplexityWorld)
+    world.dict = dict_agent
 
     # set up logging
     log_time = Timer()
     tot_time = 0
 
-    # Show some example dialogs:
     while not world.epoch_done():
-        world.parley()
+        world.parley()  # process an example
 
         if log_time.time() > 1:  # log every 1 sec
             tot_time += log_time.time()
@@ -207,10 +209,11 @@ if __name__ == '__main__':
                 round_sigfigs(report['total'] / world.num_examples() * 100, 2),
                 report))
             log_time.reset()
-            if opt['validation_max_exs'] > 0 and report['total'] >= opt['validation_max_exs']:
-                break
     if world.epoch_done():
         print('EPOCH DONE')
     tot_time += log_time.time()
     final_report = world.report()
     print('{}s elapsed: {}'.format(int(tot_time), final_report))
+
+if __name__ == '__main__':
+    eval_ppl(setup_args())
