def092f27ce9594846608f922a6d199582e8c9c4
Alexander Miller <ahm@fb.com>
2018-05-09 18:02:47 -0600

    remove observation from next_word_prob

--- projects/convai2/baselines/seq2seq/eval_ppl.py
+++ projects/convai2/baselines/seq2seq/eval_ppl.py
@@ -23,13 +23,15 @@ class Seq2seqEntry(Seq2seqAgent):
         else:
             # default minimum probability mass for all tokens
             self.probs = {k: 1e-7 for k in build_dict().keys()}
+        self.prev_enc = None
+        self.prev_vec = None
 
     def share(self):
         shared = super().share()
         shared['probs'] = self.probs.copy()
         return shared
 
-    def next_word_probability(self, observation, partial_out):
+    def next_word_probability(self, partial_out):
         """Return probability distribution over next words given an input and
         partial true output. This is used to calculate the per-word perplexity.
 
@@ -43,14 +45,6 @@ class Seq2seqEntry(Seq2seqAgent):
         e.g.
         {'text': 'Run test program.'}, ['hello'] => {'world': 1.0}
         """
-        if not hasattr(self, 'prev_enc'):
-            self.prev_enc = None
-            self.last_text = None
-        if observation['text'] != self.last_text:
-            self.prev_enc = None
-            self.last_text = observation.get('text')
-            self.observe(observation)
-
         obs = self.observation
         obs['eval_labels'] = [' '.join(partial_out)]
         batch = self.vectorize([obs])
@@ -58,8 +52,7 @@ class Seq2seqEntry(Seq2seqAgent):
         self.model.longest_label = 1  # no need to predict farther ahead
         out = self.model(
             batch[0], # xs
-            ys=(batch[1] if len(partial_out) > 0 else None),
-            prev_enc=self.prev_enc)
+            ys=(batch[1] if len(partial_out) > 0 else None))
         scores, self.prev_enc = out[1], out[3]
         # scores is bsz x seqlen x num_words, so select probs of current index
         assert len(partial_out) == scores.size(1) - 1
@@ -71,7 +64,6 @@ class Seq2seqEntry(Seq2seqAgent):
             except AttributeError:
                 val = probs[i][0]
             dist[self.dict[i]] = val
-        self.batch = batch
         return dist
 
 
