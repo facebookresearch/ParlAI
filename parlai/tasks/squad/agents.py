361510a431510f6d5aed920b9009ece0fa6711b1
Kurt Shuster <kshuster@fb.com>
2018-05-31 15:33:45 -0400

    Update agents.py

--- parlai/tasks/squad/agents.py
+++ parlai/tasks/squad/agents.py
@@ -119,7 +119,7 @@ class OpenSquadTeacher(DialogTeacher):
     """This version of SQuAD inherits from the core Dialog Teacher, which just
     requires it to define an iterator over its data `setup_data` in order to
     inherit basic metrics, a default `act` function.
-    Note: This teacher omits the context
+    Note: This teacher omits the context paragraph
     """
 
     def __init__(self, opt, shared=None):
