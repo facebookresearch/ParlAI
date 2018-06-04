7f814b13cfd96601465f74b75a8e7eafe1323334
Jason Weston <jase@fb.com>
2018-06-04 08:07:58 -0600

    fixes

--- parlai/tasks/fromfile/agents.py
+++ parlai/tasks/fromfile/agents.py
@@ -31,6 +31,60 @@ class FbformatTeacher(FbDialogTeacher):
         super().__init__(opt, shared)
 
 
+class Fbformat2Teacher(FbDialogTeacher):
+    """This task simply loads the specified file: useful for quick tests without
+    setting up a new task.
+    """
+    
+    @staticmethod
+    def add_cmdline_args(argparser):
+        agent = argparser.add_argument_group('FromFile Task Arguments')
+        agent.add_argument('--fromfile-datapath2', type=str,
+                           help="Data file")
+        
+    def __init__(self, opt, shared=None):
+        opt = copy.deepcopy(opt)
+        if not opt.get('fromfile_datapath2'):
+            raise RuntimeError('fromfile_datapath2 not specified')
+        opt['datafile'] = opt['fromfile_datapath2']
+        super().__init__(opt, shared)
+        
+
+class Parlaiformat2Teacher(FixedDialogTeacher):
+    @staticmethod
+    def add_cmdline_args(argparser):
+        agent = argparser.add_argument_group('FromFile Task Arguments')
+        agent.add_argument('--fromfile-datapath2', type=str,
+                           help="Data file")
+
+    def __init__(self, opt, shared=None):
+        super().__init__(opt, shared)
+        opt = copy.deepcopy(opt)
+        if not opt.get('fromfile_datapath2'):
+            raise RuntimeError('fromfile_datapath2 not specified')
+        datafile = opt['fromfile_datapath2']
+        self.data = self._setup_data(datafile)
+        self.id = datafile
+        self.reset()
+        
+    def num_examples(self):
+        return len(self.examples)
+
+    def num_episodes(self):
+        return self.num_examples()
+
+    def get(self, episode_idx, entry_idx=None):
+        return self.examples[episode_idx]
+
+    def _setup_data(self, path):
+        print("[loading parlAI text data:" + path + "]")
+        self.examples = []
+        with open(path) as read:
+            for line in read:
+                msg = str_to_msg(line.rstrip('\n'))
+                if msg:
+                    self.examples.append(msg)
+        
 class ParlaiformatTeacher(FixedDialogTeacher):
     """This module provides access to data in the ParlAI Text Dialog format.
 
