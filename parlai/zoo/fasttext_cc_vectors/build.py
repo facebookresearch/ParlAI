45a9731937f139f6c52d7b902c81086f887abdef
Emily Dinan <edinan@fb.com>
2018-06-15 12:55:53 -0600

    fast text common crawl

--- parlai/zoo/fasttext_cc_vectors/build.py
+++ parlai/zoo/fasttext_cc_vectors/build.py
@@ -3,12 +3,16 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
-"""Fastext vectors, e.g. use with filename "models:fasttext_vectors/wiki.en.vec"
+"""Fastext Common Crawl vectors, e.g. use with filename
+"models:fasttext_cc_vectors/crawl-300d-2M.vec"
 """
 
 import torchtext.vocab as vocab
 
 
 def download(datapath):
-    embs = vocab.FastText(language='en',
-                          cache=datapath + '/models/fasttext_vectors')
+    embs = vocab.Vectors(
+        name='crawl-300d-2M.vec',
+        url='https://s3-us-west-1.amazonaws.com/fasttext-vectors/crawl-300d-2M.vec.zip',
+        cache=datapath + '/models/fasttext_cc_vectors'
+    )
