c67b5ad912c29e843aa6cce8864ae08403f30985
Jason Weston <jase@fb.com>
2018-06-18 08:14:27 -0600

    dict update

--- parlai/core/dict.py
+++ parlai/core/dict.py
@@ -393,16 +393,20 @@ class DictionaryAgent(Agent):
         """
         print('Dictionary: loading dictionary from {}'.format(
               filename))
-        with open(filename) as read:
+        import codecs
+        with codecs.open(filename, "r",encoding='utf-8', errors='ignore') as read:
             for line in read:
-                split = line.strip().split('\t')
-                token = unescape(split[0])
-                cnt = int(split[1]) if len(split) > 1 else 0
-                self.freq[token] = cnt
-                if token not in self.tok2ind:
-                    index = len(self.tok2ind)
-                    self.tok2ind[token] = index
-                    self.ind2tok[index] = token
+                try:
+                    split = line.strip().split('\t')
+                    token = unescape(split[0])
+                    cnt = int(split[1]) if len(split) > 1 else 0
+                    self.freq[token] = cnt
+                    if token not in self.tok2ind:
+                        index = len(self.tok2ind)
+                        self.tok2ind[token] = index
+                        self.ind2tok[index] = token
+                except:
+                    pass
         print('[ num words =  %d ]' % len(self))
 
     def save(self, filename=None, append=False, sort=True):
