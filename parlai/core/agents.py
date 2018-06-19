4f70434526a18033a827d5355435d04b34c3e000
Alexander Miller <ahm@fb.com>
2018-06-19 10:17:10 -0400

    update override to be dict-only (#884)

--- parlai/core/agents.py
+++ parlai/core/agents.py
@@ -288,12 +288,11 @@ def load_agent_module(opt):
             del new_opt['batchindex']
         # only override opts specified in 'override' dict
         if opt.get('override'):
-            for k in opt['override']:
-                v = opt[k]
-                if str(v) != str(str(new_opt.get(k, None))):
-                    print("[ warning: overriding opt['" + str(k) + "'] to " +
-                          str(v) + " (previously:" +
-                          str(str(new_opt.get(k, None))) + ") ]")
+            for k, v in opt['override'].items():
+                if str(v) != str(new_opt.get(k, None)):
+                    print("[ warning: overriding opt['{}'] to {} ("
+                          "previously: {} )]".format(
+                            k, v, new_opt.get(k, None)))
                 new_opt[k] = v
         # add model arguments to new_opt if they aren't in new_opt already
         for k, v in opt.items():
