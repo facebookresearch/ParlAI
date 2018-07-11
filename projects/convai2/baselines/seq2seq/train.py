a6a4dd8890fb0476c75796af024c1f51b0aa3276
Alexander Miller <ahm@fb.com>
2018-07-11 09:22:13 -0600

    remove extraneous change

--- projects/convai2/baselines/seq2seq/train.py
+++ projects/convai2/baselines/seq2seq/train.py
@@ -42,6 +42,5 @@ if __name__ == '__main__':
         validation_metric_mode='min',
         validation_patience=12,
         log_every_n_secs=10,
-        dict_tokenizer='split',
     )
     TrainLoop(parser).train()
