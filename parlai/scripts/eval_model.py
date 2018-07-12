4ed5c2c98c568a4a535d15893b19148ff7591713
Alexander Miller <ahm@fb.com>
2018-07-12 14:34:29 -0400

    Update eval_model.py (#985)

--- parlai/scripts/eval_model.py
+++ parlai/scripts/eval_model.py
@@ -73,7 +73,7 @@ def eval_model(opt, printargs=None, print_parser=None):
     # Show some example dialogs:
     cnt = 0
     while not world.epoch_done():
-        cnt += 1
+        cnt += opt.get('batchsize', 1)
         world.parley()
         if opt['display_examples']:
             print(world.display() + "\n~~")
