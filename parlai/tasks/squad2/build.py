ea5786dc521261ef59ffffcce0d2820759c18348
Arjoonn Sharma <theSage21@users.noreply.github.com>
2018-07-20 14:47:20 +0530

    Add separate squad2.0 task for #969 (#983)
    * add squad2.0 task
    * fix empty answer label generation for sentence level task

---
+++ parlai/tasks/squad2/build.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+# Download and build the data if it does not exist.
+
+import parlai.core.build_data as build_data
+import os
+
+
+def build(opt):
+    dpath = os.path.join(opt['datapath'], 'SQuAD2')
+    version = None
+
+    if not build_data.built(dpath, version_string=version):
+        print('[building data: ' + dpath + ']')
+        if build_data.built(dpath):
+            # An older version exists, so remove these outdated files.
+            build_data.remove_dir(dpath)
+        build_data.make_dir(dpath)
+
+        # Download the data.
+        fname1 = 'train-v2.0.json'
+        fname2 = 'dev-v2.0.json'
+        url = 'https://rajpurkar.github.io/SQuAD-explorer/dataset/'
+        build_data.download(url + fname1, dpath, fname1)
+        build_data.download(url + fname2, dpath, fname2)
+
+        # Mark the data as built.
+        build_data.mark_done(dpath, version_string=version)
