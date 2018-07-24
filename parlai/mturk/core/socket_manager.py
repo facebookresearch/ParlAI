c40320f4cb17e628a3d31b1475110a142be74966
Jack Urbanek <jju@fb.com>
2018-07-24 17:57:59 -0400

    reaper thread actually a thread

--- parlai/mturk/core/socket_manager.py
+++ parlai/mturk/core/socket_manager.py
@@ -315,22 +315,30 @@ class SocketManager():
                 self._safe_put(connection_id, (t, packet))
 
     def _spawn_reaper_thread(self):
-        start_time = time.time()
-        wait_time = self.DEF_MISSED_PONGS * self.HEARTBEAT_RATE
-        while time.time() - start_time < wait_time:
-            if self.is_shutdown:
-                return
-            if self.alive:
-                return
-            time.sleep(0.3)
-        if self.server_death_callback is not None:
-            shared_utils.print_and_log(
-                logging.WARN,
-                'Server has disconnected and could not reconnect. Assuming the'
-                ' worst and calling the death callback. (Usually shutdown)',
-                should_print=True,
-            )
-            self.server_death_callback()
+        def _reaper_thread(*args):
+            start_time = time.time()
+            wait_time = self.DEF_MISSED_PONGS * self.HEARTBEAT_RATE
+            while time.time() - start_time < wait_time:
+                if self.is_shutdown:
+                    return
+                if self.alive:
+                    return
+                time.sleep(0.3)
+            if self.server_death_callback is not None:
+                shared_utils.print_and_log(
+                    logging.WARN,
+                    'Server has disconnected and could not reconnect. '
+                    'Assuming the worst and calling the death callback. '
+                    '(Usually shutdown)',
+                    should_print=True,
+                )
+                self.server_death_callback()
+        reaper_thread = threading.Thread(
+            target=_reaper_thread,
+            name='socket-reaper-{}'.format(self.task_group_id)
+        )
+        reaper_thread.daemon = True
+        reaper_thread.start()
 
     def _setup_socket(self):
         """Create socket handlers and registers the socket"""
