d57483f79f755b3b90c26613825bf325034563b7
Stephen Roller <roller@fb.com>
2018-08-20 08:31:00 -0700

    Hanging indents.

--- parlai/core/agents.py
+++ parlai/core/agents.py
@@ -310,8 +310,7 @@ def load_agent_module(opt):
             for k, v in opt['override'].items():
                 if str(v) != str(new_opt.get(k, None)):
                     print("[ warning: overriding opt['{}'] to {} ("
-                          "previously: {} )]".format(
-                            k, v, new_opt.get(k, None)))
+                          "previously: {} )]".format(k, v, new_opt.get(k, None)))
                 new_opt[k] = v
         # add model arguments to new_opt if they aren't in new_opt already
         for k, v in opt.items():
