cbb7fed88ea1661196ce9e2212c3b278fae5a2b8
Stephen Roller <roller@fb.com>
2018-08-22 16:12:49 -0700

    Touch it, own it. Add strict linting to changed files.

---
+++ tests/lint_changed.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+# This shell script lints only the things that changed in the most recent change.
+# It's much more strict than our check for lint across the entire code base.
+
+CHANGED_FILES="$(git diff --name-only origin/master HEAD | grep '\.py$' | tr '\n' ' ')"
+if [ "$CHANGED_FILES" != "" ]
+then
+    echo "Linting: $CHANGED_FILES"
+    exec flake8 $CHANGED_FILES
+else
+    echo "No python files changed :)" >&2
+fi
