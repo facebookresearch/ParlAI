b370b92b05af3e5f34149f4294f0016a17770372
Jack Urbanek <jju@fb.com>
2018-08-23 14:21:11 -0400

    Run tests with travis if needed

---
+++ tests/run_mturk_tests_if_needed.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+CHANGES=$(git diff --name-only HEAD~1)
+if echo "$CHANGES" | grep -q "parlai/mturk/"; then
+  python3 ../parlai/mturk/core/test/test_full_system.py
+  python3 ../parlai/mturk/core/test/test_mturk_agent.py
+  python3 ../parlai/mturk/core/test/test_mturk_manager.py
+  python3 ../parlai/mturk/core/test/test_socket_manager.py
+  python3 ../parlai/mturk/core/test/test_worker_manager.py
+fi
