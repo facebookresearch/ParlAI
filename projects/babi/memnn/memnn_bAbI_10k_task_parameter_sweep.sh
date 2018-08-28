d623e5a38a3004a697790620ff1f48fe783ea72a
Stephen Roller <roller@fb.com>
2018-08-28 13:31:35 -0700

    Update copyrights.

--- projects/babi/memnn/memnn_bAbI_10k_task_parameter_sweep.sh
+++ projects/babi/memnn/memnn_bAbI_10k_task_parameter_sweep.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
 
 # Reproduce results from https://arxiv.org/abs/1503.08895 bAbI tasks 1-3 with
 # 10k training examples
--- projects/personachat/scripts/kvmemnn_train.sh
+++ projects/personachat/scripts/kvmemnn_train.sh
@@ -1,3 +1,9 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
 # This trains a (KV) Memory Net model on personachat using the (original) self profile
 # Run from ParlAI directory
 
