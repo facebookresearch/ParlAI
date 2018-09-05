91bd1c3074624eae0f759b058965094baa546f34
Stephen Roller <roller@fb.com>
2018-09-05 08:51:40 -0700

    Fix grading for make_samples.

--- parlai/agents/fairseq/fairseq.py
+++ parlai/agents/fairseq/fairseq.py
@@ -515,7 +515,6 @@ class FairseqAgent(TorchAgent):
 
         # Grade each of the candidate sequences
         if batch.candidate_vecs is not None:
-            raise NotImplementedError("Not fixed with new make_Sample")
             bsz = len(batch.text_vec)
             reranked_cands = []
             # score the candidates for each item in the batch separately, so that
@@ -532,7 +531,7 @@ class FairseqAgent(TorchAgent):
                 xs = xs[:, :batch.text_lengths[i]]
                 # and appropriately pack the outputs
                 ys, _ = padded_tensor(cands, self.NULL_IDX, self.use_cuda)
-                s = self._make_sample(xs, ys)
+                s = self._make_sample(xs=xs, ys=ys)
                 # perform the actual grading, extract the scores
                 scored = list(self.scorer.score_batched_itr([s], cuda=self.use_cuda))
                 scores = [s[3][0]['score'].item() for s in scored]
@@ -633,11 +632,17 @@ class FairseqAgent(TorchAgent):
         result[:, 1:] = ys[:, :-1]
         return result
 
-    def _make_sample(self, batch):
+    def _make_sample(self, batch=None, xs=None, ys=None):
         """Generate a sample object that Fairseq expects."""
         # add extra info to samples
-        xs = batch.text_vec
-        ys = batch.label_vec
+        if batch is None and xs is None:
+            raise ValueError("Must supply either batch or xs")
+        if batch is None and ys is None:
+            raise ValueError("Must supply either batch or ys")
+        if xs is None:
+            xs = batch.text_vec
+        if ys is None:
+            ys = batch.label_vec
         repadded = convert_padding_direction(xs, self.dict.pad(), right_to_left=True)
         sample = {}
         sample["id"] = torch.arange(len(xs) - 1)
