ec99973d1557098faf4d1dc3618ce3c1b6dfa706
Stephen Roller <roller@fb.com>
2018-10-01 08:01:37 -0700

    Lint docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,12 +36,13 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import parlai
 import sphinx_rtd_theme
 
 
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.githubpages']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.githubpages'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
