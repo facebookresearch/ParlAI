71f4b7e917f751d75d33d4790c8693c2df4bc499
Jack Urbanek <jju@fb.com>
2018-10-05 18:01:46 -0400

    Updating tests for new blocking logic

--- parlai/mturk/core/socket_manager.py
+++ parlai/mturk/core/socket_manager.py
@@ -301,7 +301,7 @@ class SocketManager():
         """Sends a packet, blocks if the packet is blocking"""
         # Send the packet
         pkt = packet.as_dict()
-        if pkt['data'] is None:
+        if pkt['data'] is None or packet.status == Packet.STATUS_ACK:
             return  # This packet was _just_ acked.
         shared_utils.print_and_log(
             logging.DEBUG,
