853af88b59e4ee706e536f7a06293491974fe3d1
Jack Urbanek <jju@fb.com>
2018-10-26 14:17:25 -0400

    Fixing pickle saves, fixing dir management

--- parlai/mturk/core/mturk_data_handler.py
+++ parlai/mturk/core/mturk_data_handler.py
@@ -24,7 +24,7 @@ def force_dir(path):
 
 
 data_dir = os.path.dirname(os.path.abspath(__file__)) + '/run_data'
-force_dir(data_dir)
+os.makedirs(data_dir, exist_ok=True)
 
 # Run data table:
 CREATE_RUN_DATA_SQL_TABLE = (
@@ -118,25 +118,25 @@ class MTurkDataHandler():
         target = 'sandbox' if sandbox else 'live'
         target_dir = os.path.join(
             data_dir, target, task_group_id, conversation_id)
-        force_dir(target_dir)
         custom_data = prepped_save_data['custom_data']
         if custom_data is not None:
             target_dir_custom = os.path.join(target_dir, 'custom')
-            force_dir(target_dir_custom)
             if custom_data.get('needs-pickle') is not None:
                 pickle_file = os.path.join(target_dir_custom, 'data.pickle')
-                with open(pickle_file, 'w') as outfile:
-                    json.dump(custom_data['needs-pickle'], outfile)
+                force_dir(pickle_file)
+                with open(pickle_file, 'wb') as outfile:
+                    pickle.dump(custom_data['needs-pickle'], outfile)
                 del custom_data['needs-pickle']
             custom_file = os.path.join(target_dir_custom, 'data.json')
+            force_dir(custom_file)
             with open(custom_file, 'w') as outfile:
                 json.dump(custom_data, outfile)
         worker_data = prepped_save_data['worker_data']
         target_dir_workers = os.path.join(target_dir, 'workers')
-        force_dir(target_dir_workers)
         for worker_id, w_data in worker_data.items():
             worker_file = os.path.join(
                 target_dir_workers, '{}.json'.format(worker_id))
+            force_dir(worker_file)
             with open(worker_file, 'w') as outfile:
                 json.dump(w_data, outfile)
 
