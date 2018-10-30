6b1f6a195a94dc9c9ea3c48f812db8961550d52f
Alexander Miller <ahm@fb.com>
2018-10-30 18:10:16 +0100

    fix wmt datatype checking

--- parlai/tasks/wmt/agents.py
+++ parlai/tasks/wmt/agents.py
@@ -13,11 +13,9 @@ import copy
 import os
 
 
-def _path(task, opt, dt=''):
+def _path(task, opt, dt):
     # Build the data if it doesn't exist.
     build(opt)
-    if dt == '':
-        dt = opt['datatype'].split(':')[0]
     return os.path.join(opt['datapath'], 'wmt',
                         '{task}_{type}.txt'.format(task=task, type=dt))
 
@@ -27,7 +25,8 @@ class EnDeTeacher(FbDialogTeacher):
         opt = copy.deepcopy(opt)
         task = opt.get('task', 'wmt:en_de')
         self.task_name = task.split(':')[1] if ':' in task else 'en_de'
-        opt['datafile'] = _path(self.task_name, opt, opt['datatype'])
+        dt = opt['datatype'].split(':')[0]
+        opt['datafile'] = _path(self.task_name, opt, dt)
         super().__init__(opt, shared)
 
 
