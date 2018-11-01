eac269700fa2d2b4af8aa64f0cc74c017e0845a0
Jack Urbanek <jju@fb.com>
2018-11-01 16:12:48 -0400

    test fixes, removed comment

--- parlai/mturk/webapp/dev/app.jsx
+++ parlai/mturk/webapp/dev/app.jsx
@@ -36,19 +36,17 @@ function resolveState(state_string) {
 }
 
 function postData(url = ``, data = {}) {
-  // Default options are marked with *
   return fetch(url, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, cors, *same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
       headers: {
           "Content-Type": "application/json; charset=utf-8",
-          // "Content-Type": "application/x-www-form-urlencoded",
       },
-      redirect: "follow", // manual, *follow, error
-      referrer: "no-referrer", // no-referrer, *client
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
+      redirect: "follow",
+      referrer: "no-referrer",
+      body: JSON.stringify(data),
   });
 }
 
