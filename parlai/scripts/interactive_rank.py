de7a35c632d70390d202c49e8930ad77b2150167
Alexander Miller <ahm@fb.com>
2018-11-28 11:26:27 -0500

    double header block

--- parlai/scripts/interactive_rank.py
+++ parlai/scripts/interactive_rank.py
@@ -5,13 +5,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
-#!/usr/bin/env python3
 
-# Copyright (c) 2017-present, Facebook, Inc.
-# All rights reserved.
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory.
 """Does human evaluation on a task with label_candidates.
 
 Human can exit with ctrl + c and metrics will be computed and displayed.
