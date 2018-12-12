2e1d8532bd1cc8b10012ca8cf34665874b5781ac
Alexander Miller <ahm@fb.com>
2018-12-12 10:04:20 -0500

    okay one more (#1280)

--- parlai/core/torch_agent.py
+++ parlai/core/torch_agent.py
@@ -550,7 +550,7 @@ class TorchAgent(Agent):
                 vecs = obs['label_candidates_vecs']
                 for i, c in enumerate(vecs):
                     vecs[i] = self._check_truncate(c, truncate)
-        elif self.rank_candidates and 'label_candidates' in obs:
+        elif self.rank_candidates and obs.get('label_candidates'):
             obs['label_candidates'] = list(obs['label_candidates'])
             obs['label_candidates_vecs'] = [
                 self._vectorize_text(c, add_start, add_end, truncate, False)
