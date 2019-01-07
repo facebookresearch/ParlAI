ca37cf7dcba3e0ca6cfb86836cc2c002bead8e2f
Samuel Humeau <samuel.humeau@gmail.com>
2019-01-07 02:59:40 -0800

    licenses

--- parlai/agents/bert_ranker/bert_dictionary.py
+++ parlai/agents/bert_ranker/bert_dictionary.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
 from parlai.core.dict import DictionaryAgent
 try:
     from pytorch_pretrained_bert import BertTokenizer
--- parlai/agents/bert_ranker/bert_ranker.py
+++ parlai/agents/bert_ranker/bert_ranker.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
 # disabling flake8 on those lines to improve call syntax
 from .biencoder_ranker import BiEncoderRankerAgent  # NOQA
 from .crossencoder_ranker import CrossEncoderRankerAgent  # NOQA
