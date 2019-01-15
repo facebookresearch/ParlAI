9194c2734de108c05863d355ec3b4ec4e415e6c8
Stephen Roller <roller@fb.com>
2019-01-15 13:55:24 -0800

    lint.

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -15,7 +15,7 @@ import unittest
 import parlai
 import subprocess
 try:
-    import sphinx
+    import sphinx  # noqa: F401
     SKIP_TEST = False
 except ImportError:
     SKIP_TEST = True
@@ -25,9 +25,8 @@ SOURCE_ROOT = os.path.dirname(os.path.dirname(parlai.__file__))
 DOCS_DIR = os.path.join(SOURCE_ROOT, "docs")
 
 
-
 class TestDocs(unittest.TestCase):
-    unittest.skipIf(SKIP_TEST, "Sphinx not installed.")
+    @unittest.skipIf(SKIP_TEST, "Sphinx not installed.")
     def test_docs_build(self):
         call = subprocess.run(
             ["make", "html"],
