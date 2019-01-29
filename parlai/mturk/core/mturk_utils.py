48ba19c5a0261e6bb163452ff27be4937476f0e7
Jack Urbanek <jju@fb.com>
2019-01-29 18:01:32 -0500

    Comment about chat_titles

--- parlai/mturk/core/mturk_utils.py
+++ parlai/mturk/core/mturk_utils.py
@@ -144,6 +144,8 @@ def create_hit_config(opt, task_description, unique_worker, is_sandbox):
         'frame_height': opt.get('frame_height', 650),
         'allow_reviews': opt.get('allow_reviews', False),
         'block_mobile': opt.get('block_mobile', True),
+        # Populate the chat pane title from chat_title, defaulting to the 
+        # hit_title if the task provides no chat_title
         'chat_title': opt.get('chat_title', opt.get('hit_tile', 'Live Chat')),
     }
     hit_config_file_path = os.path.join(parent_dir, 'hit_config.json')
