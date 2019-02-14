ae1919565c3a22ba6ae5a02e8fc2989a953d1371
Jack Urbanek <jju@fb.com>
2019-02-14 16:31:37 -0500

    Log to db again

--- parlai/mturk/core/mturk_manager.py
+++ parlai/mturk/core/mturk_manager.py
@@ -205,7 +205,8 @@ class MTurkManager():
 
     def _init_logging_config(self):
         """Initialize logging settings from the opt"""
-        if self.use_db and not self.opt['is_debug']:
+        # TODO remove the false flag
+        if False and self.use_db and not self.opt['is_debug']:
             shared_utils.disable_logging()
         else:
             shared_utils.set_is_debug(self.opt['is_debug'])
