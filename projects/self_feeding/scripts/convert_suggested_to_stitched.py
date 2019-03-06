ef32e838072e2da7349ea7f9b43339c086e252cd
Stephen Roller <roller@fb.com>
2019-03-06 08:13:09 -0800

    Lint.

--- projects/self_feeding/scripts/convert_suggested_to_stitched.py
+++ projects/self_feeding/scripts/convert_suggested_to_stitched.py
@@ -28,8 +28,8 @@ def main(config):
     input: a .suggested file of logs (in ParlaiDialog format) from Mturk task 2, each of
         which starts with an initial prompt or topic request, and ends with a y
         that corresponds to the y_exp given in the previous turn
-    output: a .stitched file (in self-feeding format) with the original mistake by the bot
-        replace with the mturked y (based on y_exp)
+    output: a .stitched file (in self-feeding format) with the original mistake by the
+        bot replace with the mturked y (based on y_exp)
     """
     examples = []
     episodes = [e for e in extract_parlai_episodes(config['infile'])]
