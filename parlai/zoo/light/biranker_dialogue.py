36c426b56c991b0e11dad51dab9a63191c04293b
Jason Weston <jase@fb.com>
2019-03-20 05:57:26 -0700

    lint

--- parlai/zoo/light/biranker_dialogue.py
+++ parlai/zoo/light/biranker_dialogue.py
@@ -10,12 +10,8 @@ https://arxiv.org/pdf/1903.03094.pdf
 
 from parlai.core.build_data import download_models
 
+
 def download(datapath):
-    opt = {
-        'datapath': datapath, 
-        'model_type': 'biranker_dialogue' # for builder
-    }
+    opt = {'datapath': datapath, 'model_type': 'biranker_dialogue'}  # for builder
     fnames = ['biranker_dialogue.tar.gz']
-    download_models(
-        opt, fnames, 'light', version='v0.5', use_model_type=True
-    )
+    download_models(opt, fnames, 'light', version='v0.5', use_model_type=True)
