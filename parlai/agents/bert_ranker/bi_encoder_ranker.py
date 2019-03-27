c1599a5085d3aac47fb03b82bbbc11258ac8dfcf
Stephen Roller <roller@fb.com>
2019-03-27 07:36:01 -0700

    Quote nit.

--- parlai/agents/bert_ranker/bi_encoder_ranker.py
+++ parlai/agents/bert_ranker/bi_encoder_ranker.py
@@ -60,8 +60,8 @@ class BiEncoderRankerAgent(TorchRankerAgent):
 
     def init_optim(self, params, optim_states=None, saved_optim_type=None):
         self.optimizer = get_bert_optimizer([self.model],
-                                            self.opt["type_optimization"],
-                                            self.opt["learningrate"],
+                                            self.opt['type_optimization'],
+                                            self.opt['learningrate'],
                                             fp16=self.opt.get('fp16'))
 
     def set_vocab_candidates(self, shared):
