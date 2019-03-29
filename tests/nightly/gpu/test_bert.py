b45a66d52c364dd3d20ced52c622f96e4550feb2
Samuel Humeau <samuel.humeau@gmail.com>
2019-03-29 17:31:37 +0100

    nightly check for Bert (#1584)
    * nightly check for Bert
    
    * docstring
    
    * install pretrained pytorch bert for nightly gpu
    
    * pretty aggressive truncation to fit in mem
    
    * tss, compile
    
    * even more aggressive truncation
    
    * handling of options
    
    * lower truncation
    
    * making sure memory is ok + doc
    
    * config cleaning

---
+++ tests/nightly/gpu/test_bert.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+import parlai.core.testing_utils as testing_utils
+
+
+@testing_utils.skipUnlessGPU
+class TestBertModel(unittest.TestCase):
+    """
+    Test of Bert biencoder and crossencoder.
+
+    Checks that Both Biencoder and CrossEncoder of Bert can be trained
+    for about 100 samples on convai2
+    """
+
+    def test_biencoder(self):
+        stdout, valid, test = testing_utils.train_model(dict(
+            task='convai2:LimitedSelfOriginalTeacher',
+            model='bert_ranker/bi_encoder_ranker',
+            num_epochs=1.0,
+            batchsize=8,
+            text_truncate=32,
+        ))
+        # can't conclude much from the biencoder after that little iterations.
+        # accuracy should be present and somewhere between 0.01 and 0.2
+        # basically it's still a random classifier
+        self.assertGreaterEqual(
+            test['accuracy'], 0.01,
+            'test accuracy = {}\nLOG:\n{}'.format(test['accuracy'], stdout)
+        )
+        self.assertLessEqual(
+            test['accuracy'], 0.2,
+            'test accuracy = {}\nLOG:\n{}'.format(test['accuracy'], stdout)
+        )
+
+    def test_crossencoder(self):
+        stdout, valid, test = testing_utils.train_model(dict(
+            task='convai2:LimitedSelfOriginalTeacher',
+            model='bert_ranker/cross_encoder_ranker',
+            num_epochs=1.0,
+            batchsize=1,
+            candidates="inline",
+            type_optimization="all_encoder_layers",
+            warmup_updates=100,
+            text_truncate=32,
+            label_truncate=32,
+        ))
+        # The cross encoder reaches an interesting state MUCH faster
+        # accuracy should be present and somewhere between 0.2 and 0.8
+        # (large interval so that it doesn't flake.)
+        self.assertGreaterEqual(
+            test['accuracy'], 0.15,
+            'test accuracy = {}\nLOG:\n{}'.format(test['accuracy'], stdout)
+        )
+        self.assertLessEqual(
+            test['accuracy'], 0.8,
+            'test accuracy = {}\nLOG:\n{}'.format(test['accuracy'], stdout)
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
