e3415a16a28bb083e35c1f00a86397516a298789
Stephen Roller <roller@fb.com>
2019-04-01 14:56:45 -0700

    Add integration test.

--- parlai/tasks/integration_tests/agents.py
+++ parlai/tasks/integration_tests/agents.py
@@ -13,6 +13,7 @@ The corpora are all randomly, but deterministically generated
 """
 
 from parlai.core.teachers import DialogTeacher
+import copy
 import random
 import itertools
 
@@ -169,6 +170,60 @@ class MultiturnNocandidateTeacher(MultiturnCandidateTeacher):
         raw = super().setup_data(fold)
         for (t, a, _r, _c), e in raw:
             yield (t, a), e
+
+
+class BadExampleTeacher(CandidateTeacher):
+    """
+    Teacher which produces a variety of examples that upset verify_data.py.
+
+    Useful for checking how models respond when the following assumptions are
+    violated:
+
+        0. text is empty string
+        1. missing text
+        2. label is empty string
+        3. missing label
+        4. label candidates is empty
+        5. label candidates contains an empty string
+        6. label isn't in the candidates
+        7. missing label candidates:
+    """
+    NUM_CASES = 8
+
+    def __init__(self, opt, shared=None):
+        super().__init__(opt, shared)
+        # gross hack: override sub thing to force things the way we want
+        self.data.get = self._wrapperfn(self.data.get)
+
+    def _wrapperfn(self, oldget):
+        def newget(*args):
+            item, eod = oldget(*args)
+            item = copy.deepcopy(item)
+            newget.case = (newget.case + 1) % self.NUM_CASES
+            case = newget.case
+            # print(self.case)
+            if case == 0:
+                item['text'] = ''
+            elif case == 1:
+                del item['text']
+            elif case == 2:
+                item['labels'] = ['']
+            elif case == 3:
+                del item['labels']
+            elif case == 4:
+                item['label_candidates'] = []
+            elif case == 5:
+                item['label_candidates'] = tuple(list(item['label_candidates']) + [''])
+            elif case == 6:
+                item['label_candidates'] = list(item['label_candidates'])
+                item['label_candidates'].remove(item['labels'][0])
+                item['label_candidates'] = tuple(item['label_candidates'])
+            elif case == 7:
+                del item['label_candidates']
+            return item, eod
+
+        newget.case = random.randint(0, self.NUM_CASES)
+        return newget
 
 
 class DefaultTeacher(CandidateTeacher):
