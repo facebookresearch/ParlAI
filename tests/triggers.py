c29eac19d57347d522e717e5ac66d4dace9bae61
Stephen Roller <roller@fb.com>
2019-04-02 07:47:50 -0700

    Copyright.

--- tests/triggers.py
+++ tests/triggers.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 """
 Detects whether specific jobs should run.
 
