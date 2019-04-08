66291f4bb96a17d067641dc167e0dc719d1286f8
Emily Dinan <edinan@fb.com>
2019-04-08 13:23:46 -0700

    add interactive retrieval model and mturk evaluation task for wizard

--- parlai/zoo/wizard_of_wikipedia/full_dialogue_retrieval_model.py
+++ parlai/zoo/wizard_of_wikipedia/full_dialogue_retrieval_model.py
@@ -11,6 +11,7 @@ from parlai.core.build_data import download_models
 
 def download(datapath):
     opt = {'datapath': datapath}
-    fnames = ['full_dialogue_retrieval_model.tgz']
+    fnames = ['wizard_retrieval_2.tgz']
+    # TODO: rezip this with new files
     opt['model_type'] = 'full_dialogue_retrieval_model'
-    download_models(opt, fnames, 'wizard_of_wikipedia')
+    download_models(opt, fnames, 'wizard_of_wikipedia', version='v2.0')
---
+++ projects/wizard_of_wikipedia/mturk_evaluation_task/README.md
@@ -0,0 +1,5 @@
+This is the Mechanical Turk task for evaluating models trained on the Wizard of Wikipedia task.
+
+As an example, we have one of the pre-trained models loaded inside the task. Please edit `config` in `run.py` to swap out the model for one of yours.
+
+In order to run the task with two humans speaking to each other, run with the flag `--human-eval True`.
