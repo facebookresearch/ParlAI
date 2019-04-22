55e6a2ed19a82183ba734ceb6823d2aad5eff3e2
Stephen Roller <roller@fb.com>
2019-04-22 08:28:51 -0700

    New numpy changed default api.

--- parlai/agents/tfidf_retriever/utils.py
+++ parlai/agents/tfidf_retriever/utils.py
@@ -44,7 +44,7 @@ def save_sparse_tensor(filename, matrix, metadata=None):
 
 
 def load_sparse_csr(filename):
-    loader = np.load(filename + '.npz')
+    loader = np.load(filename + '.npz', allow_pickle=True)
     matrix = sp.csr_matrix((loader['data'], loader['indices'],
                             loader['indptr']), shape=loader['shape'])
     return matrix, loader['metadata'].item(0) if 'metadata' in loader else None
