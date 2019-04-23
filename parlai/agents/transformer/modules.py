86b4c2da5880b57ef0435fa094c09919f947cc54
klshuster <kshuster@fb.com>
2019-04-23 14:26:43 -0700

    changes to use image

--- parlai/agents/transformer/modules.py
+++ parlai/agents/transformer/modules.py
@@ -271,7 +271,6 @@ class TransformerEncoder(nn.Module):
         if embedding is not None:
             self.embeddings = embedding
         else:
-            assert False
             assert padding_idx is not None
             self.embeddings = nn.Embedding(
                 vocabulary_size, embedding_size, padding_idx=padding_idx
