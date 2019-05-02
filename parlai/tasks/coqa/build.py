20cf819eb800e0a62eeded70ebc16d0324f394b4
Dexter Ju <da.ju.fr@gmail.com>
2019-05-02 07:55:08 -0700

    fix comments

--- parlai/tasks/coqa/build.py
+++ parlai/tasks/coqa/build.py
@@ -9,29 +9,32 @@ import parlai.core.build_data as build_data
 import os
 import json
 
-tfname = 'coqa-train-v1.0.json'
-dfname = 'coqa-dev-v1.0.json'
-url = 'https://nlp.stanford.edu/data/coqa/'
+TRAIN_FILENAME = 'coqa-train-v1.0.json'
+VALID_FILENAME = 'coqa-dev-v1.0.json'
+URL = 'https://nlp.stanford.edu/data/coqa/'
 
 
 def make_parlai_format(outpath, dtype, data):
     print('building parlai:' + dtype)
-    fout = open(os.path.join(outpath, dtype + '.txt'), 'w')
     for each in data:
-        output = ""
+        output = []
         story = each['story'].replace('\n', '\\n')
         for question, ans in zip(each['questions'], each['answers']):
-            question_txt = story + '\\n' + question['input_text'] \
-                if question['turn_id'] == 1 else question['input_text']
-            output += "text:{question}\tlabels:{labels}".format(
+            question_txt = ''
+            if question['turn_id'] == 1:
+                # include the context in the very first turn
+                question_txt = story + '\\n' + question['input_text']
+            else:
+                question_txt = question['input_text']
+            output.append("text:{question}\tlabels:{labels}".format(
                 question=question_txt,
                 labels=ans['input_text'].replace("|", " __PIPE__ ")
-            )
+            ))
             if question['turn_id'] < len(each['questions']):
-                output += '\n'
-        output += "\t\tepisode_done:True\n"
-        fout.write(output)
-    fout.close()
+                output.append('\n')
+        output.append('\t\tepisode_done:True\n')
+    with open(os.path.join(outpath, dtype + '.txt'), 'w') as fout:
+        fout.write("".join(output))
 
 
 def build(opt):
@@ -46,25 +49,16 @@ def build(opt):
         build_data.make_dir(dpath)
 
         # Download the data.
-        build_data.download(url + tfname, dpath, tfname)
-        build_data.download(url + dfname, dpath, dfname)
+        build_data.download(URL + TRAIN_FILENAME, dpath, TRAIN_FILENAME)
+        build_data.download(URL + VALID_FILENAME, dpath, VALID_FILENAME)
 
-        with open(os.path.join(dpath, tfname)) as f:
+        with open(os.path.join(dpath, TRAIN_FILENAME)) as f:
             data = json.load(f)['data']
+            make_parlai_format(dpath, 'train', data)
 
-        train_p = 0.8
-        valid_p = 0.2
-        assert train_p > 0
-        assert valid_p > 0
-        data_len = len(data)
-        first_valid = int(data_len * train_p)
-
-        make_parlai_format(dpath, 'train', data[:first_valid])
-        make_parlai_format(dpath, 'valid', data[first_valid:])
-
-        with open(os.path.join(dpath, dfname)) as f:
+        with open(os.path.join(dpath, VALID_FILENAME)) as f:
             data = json.load(f)['data']
-        make_parlai_format(dpath, 'dev', data)
+            make_parlai_format(dpath, 'valid', data)
 
         # Mark the data as built.
         build_data.mark_done(dpath, version_string=version)
