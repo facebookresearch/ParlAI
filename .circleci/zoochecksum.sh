d04de56b1885699344e314deb65605a4d976dfe5
Stephen Roller <roller@fb.com>
2019-05-03 10:34:42 -0700

    Update cache invalidation.

--- .circleci/zoochecksum.sh
+++ .circleci/zoochecksum.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-git ls-files parlai/zoo/ tests/nightly/gpu | sort | xargs md5sum
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+git ls-files '**/build.py' 'parlai/zoo/' 'tests/nightly/gpu' | sort | xargs md5sum
