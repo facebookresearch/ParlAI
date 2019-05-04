0af504deaf4ead8535ab3602e91a032d6047e820
Stephen Roller <roller@fb.com>
2019-05-03 19:16:30 -0700

    Undo the cache parts of this change.

--- .circleci/zoochecksum.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Copyright (c) Facebook, Inc. and its affiliates.
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-git ls-files '**/build.py' 'parlai/zoo/' 'tests/nightly/gpu' | sort | xargs md5sum
