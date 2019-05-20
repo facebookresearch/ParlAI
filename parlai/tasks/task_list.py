1f87ba70096d8edeb56dd8e86570567fb421a920
Dexter JU <5313281+dexterju@users.noreply.github.com>
2019-05-20 15:22:39 +0200

    Add QuAC dataset (#1711)
    * add quac dataset

--- parlai/tasks/task_list.py
+++ parlai/tasks/task_list.py
@@ -367,6 +367,24 @@ task_list = [
         ),
     },
     {
+        "id": "QuAC",
+        "display_name": "Question Answering in Context",
+        "task": "quac",
+        "tags": ["All", "QA"],
+        "description": (
+            "Question Answering in Context is a dataset for modeling, "
+            "understanding, and participating in information seeking dialog. Data "
+            "instances consist of an interactive dialog between two crowd workers: "
+            "(1) a student who poses a sequence of freeform questions to learn as "
+            "much as possible about a hidden Wikipedia text, and (2) a teacher who "
+            "answers the questions by providing short excerpts (spans) from the text. "
+            "QuAC introduces challenges not found in existing machine comprehension "
+            "datasets: its questions are often more open-ended, unanswerable, "
+            "or only meaningful within the dialog context. link: "
+            "https://arxiv.org/abs/1808.07036"
+        ),
+    },
+    {
         "id": "SelfFeedingChatbot",
         "display_name": "Self-Feeding Chatbot",
         "task": "self_feeding",
