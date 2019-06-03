86df9b6eec5e06df5958c4b76c4bc780efe81b39
klshuster <kshuster@fb.com>
2019-06-03 14:18:32 -0700

    add gpu test for transresnet [gpu]

--- parlai/tasks/personality_captions/agents.py
+++ parlai/tasks/personality_captions/agents.py
@@ -230,5 +230,52 @@ class PersonalityCaptionsTeacher(FixedDialogTeacher):
         return shared
 
 
+class PersonalityCaptionsTestTeacher(PersonalityCaptionsTeacher):
+    """
+    Test PersonalityCaptions teacher for ensuring pretrained model does not
+    break.
+    """
+    def _setup_data(self, data_path, personalities_data_path):
+        super()._setup_data(data_path, personalities_data_path)
+        from parlai.zoo.personality_captions.transresnet import download
+        download(self.opt['datapath'])
+        image_features_path = os.path.join(
+            self.opt['datapath'],
+            'models/personality_captions/transresnet/test_image_feats'
+        )
+        import torch
+        self.image_features = torch.load(image_features_path)
+
+    def reset(self):
+        super().reset()
+        self.example = None
+
+    def num_episodes(self):
+        return len(self.image_features)
+
+    def num_examples(self):
+        return len(self.image_features)
+
+    def get(self, episode_idx, entry_idx=0):
+        data = self.data[episode_idx]
+
+        action = {
+            'text': data['personality'] if self.include_personality else '',
+            'image': self.image_features[data['image_hash']],
+            'episode_done': True,
+            'labels': [data['comment']],
+        }
+        if self.num_test_labels == 5 and 'test' in self.datatype:
+            action['labels'] += data['additional_comments']
+
+        if 'candidates' in data:
+            if self.num_test_labels == 5 and 'test' in self.datatype:
+                action['label_candidates'] = data['500_candidates']
+            else:
+                action['label_candidates'] = data['candidates']
+
+        return action
+
+
 class DefaultTeacher(PersonalityCaptionsTeacher):
     pass
