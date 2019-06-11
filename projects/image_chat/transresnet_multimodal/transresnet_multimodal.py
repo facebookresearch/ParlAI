ebb349ac8ae268b581b954fe05a3ba6f1e21277f
klshuster <kshuster@fb.com>
2019-06-11 12:31:58 -0700

    override setup cands

--- projects/image_chat/transresnet_multimodal/transresnet_multimodal.py
+++ projects/image_chat/transresnet_multimodal/transresnet_multimodal.py
@@ -10,10 +10,12 @@ from parlai.core.utils import round_sigfigs
 from .modules import TransresnetMultimodalModel
 from projects.personality_captions.transresnet.transresnet import TransresnetAgent
 
+import torch
 from torch import optim
 import random
 import os
 import numpy as np
+import tqdm
 from collections import deque
 
 
@@ -85,6 +87,36 @@ class TransresnetMultimodalAgent(TransresnetAgent):
             self.load(init_model_path)
         if self.use_cuda:
             self.model.cuda()
+
+    def _setup_cands(self):
+        """Override for different call to model."""
+        self.fixed_cands = None
+        self.fixed_cands_enc = None
+        if self.fcp is not None:
+            with open(self.fcp) as f:
+                self.fixed_cands = [c.replace('\n', '') for c in f.readlines()]
+            cands_enc_file = '{}.cands_enc'.format(self.fcp)
+            print('loading saved cand encodings')
+            if os.path.isfile(cands_enc_file):
+                self.fixed_cands_enc = torch.load(
+                    cands_enc_file, map_location=lambda cpu, _: cpu
+                )
+            else:
+                print('Extracting cand encodings')
+                self.model.eval()
+                pbar = tqdm.tqdm(
+                    total=len(self.fixed_cands), unit='cand', unit_scale=True,
+                    desc='Extracting candidate encodings'
+                )
+                fixed_cands_enc = []
+                for _, batch in enumerate(
+                    [self.fixed_cands[i:i + 50] for i in range(0, len(self.fixed_cands)-50, 50)]
+                ):
+                    embedding = self.model.forward_text_encoder(batch).detach()
+                    fixed_cands_enc.append(embedding)
+                    pbar.update(50)
+                self.fixed_cands_enc = torch.cat(fixed_cands_enc, 0)
+                torch.save(self.fixed_cands_enc, cands_enc_file)
 
     def share(self):
         """Override to share optimizer."""
