5c3080a39fee065e448db772c12796f2f0ef02f0
Stephen Roller <roller@fb.com>
2019-06-17 14:13:17 -0700

    Follow fairseq and divide the loss by update_freq when using gradient accumulation.

--- parlai/core/torch_agent.py
+++ parlai/core/torch_agent.py
@@ -1666,6 +1666,10 @@ class TorchAgent(ABC, Agent):
         loss.backward(), for integration with distributed training and FP16
         training.
         """
+        if self.opt.get('update_freq', 1) > 1:
+            # gradient accumulation, but still need to average across the minibatches
+            loss = loss / self.opt['update_freq']
+
         if self.fp16:
             self.optimizer.backward(loss, update_master_grads=False)
         else:
