d93a30147dc50c67b5a5c0e494e381d3e9010cc3
Stephen Roller <roller@fb.com>
2019-06-19 13:26:13 -0700

    Clamp the softmax in MHA.

--- parlai/agents/transformer/modules.py
+++ parlai/agents/transformer/modules.py
@@ -32,6 +32,7 @@ except ImportError:
     warn_once("Installing APEX can give a significant speed boost.")
     from torch.nn import LayerNorm
 
+SOFTMAX_CLAMP = 1e5  # multihead attention dots are clamped here
 LAYER_NORM_EPS = 1e-5  # Epsilon for layer norm.
 
 
@@ -906,6 +907,7 @@ class MultiHeadAttention(nn.Module):
             .view(batch_size * n_heads, query_len, key_len)
         )
         assert attn_mask.shape == dot_prod.shape
+        dot_prod.clamp_(-SOFTMAX_CLAMP, SOFTMAX_CLAMP)
         dot_prod.masked_fill_(attn_mask, neginf(dot_prod.dtype))
 
         attn_weights = F.softmax(dot_prod, dim=-1).type_as(query)
