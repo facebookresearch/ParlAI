38e076a916655ffbf053e472d6dfd1720e95d24a
Samuel Humeau <samuel.humeau@gmail.com>
2019-06-21 09:51:03 -0700

    no need for that line

--- parlai/agents/transformer/biencoder.py
+++ parlai/agents/transformer/biencoder.py
@@ -2,7 +2,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# hack to make sure -m transformer/generator works as expected
 from .transformer import TransformerRankerAgent
 from parlai.core.torch_ranker_agent import TorchRankerAgent
 import torch
