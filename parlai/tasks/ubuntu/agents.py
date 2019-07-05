1466f723adcfd36603c78a15b2207063fad7f121
Shaojie Jiang <s.jiang@uva.nl>
2019-07-05 16:21:40 -0700

    Added multi-turn teacher for Ubuntu task (#1828)
    * Added multi-turn teacher to Ubuntu task.
    
    * Exclude dialogues with less than 2 turns.
    
    * Resolve comments.
    
    * num_exs count.
    
    * Reformatted with black.

--- parlai/tasks/ubuntu/agents.py
+++ parlai/tasks/ubuntu/agents.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from parlai.core.teachers import DialogTeacher
+from parlai.core.teachers import DialogTeacher, FixedDialogTeacher
 from .build import build
 
 import csv
@@ -46,3 +46,68 @@ class DefaultTeacher(DialogTeacher):
                     cands.append(response)
                     random.shuffle(cands)
                 yield (context, [response], None, cands), True
+
+
+class MultiturnTeacher(FixedDialogTeacher):
+    def __init__(self, opt, shared=None):
+        self.datatype = opt['datatype']
+        opt['datafile'] = os.path.join(
+            opt['datapath'], 'Ubuntu', opt['datatype'].split(':')[0] + '.csv'
+        )
+        super().__init__(opt, shared)
+
+        if shared:
+            self.data = shared['data']
+        else:
+            build(opt)
+            fold = opt.get('datatype', 'train').split(':')[0]
+            self._setup_data(fold)
+
+        self.num_exs = sum(len(d) for d in self.data)
+
+        # we learn from both sides of every conversation
+        self.num_eps = 2 * len(self.data)
+        self.reset()
+
+    def num_episodes(self):
+        return self.num_eps
+
+    def num_examples(self):
+        return self.num_exs
+
+    def _setup_data(self, fold):
+        self.data = []
+        fpath = os.path.join(self.opt['datapath'], 'Ubuntu', fold + '.csv')
+        print('loading: ' + fpath)
+        with open(fpath, 'r', newline='') as read:
+            csv_read = csv.reader(read)
+            next(csv_read)  # eat header
+
+            for line in csv_read:
+                fields = line[0].strip().split('__eou__ __eot__')
+                fields.append(line[1].strip())
+                dialog = []
+                for field in fields:
+                    if field != '':
+                        dialog.append(field.replace('__eou__', '.').strip())
+                if len(dialog) > 2:
+                    self.data.append(dialog)
+
+    def get(self, episode_idx, entry_idx=0):
+        # Sometimes we're speaker 1 and sometimes we're speaker 2
+        speaker_id = episode_idx % 2
+        full_eps = self.data[episode_idx // 2]
+
+        entries = full_eps
+        their_turn = entries[speaker_id + 2 * entry_idx]
+        my_turn = entries[1 + speaker_id + 2 * entry_idx]
+
+        episode_done = 1 + speaker_id + 2 * entry_idx >= len(full_eps) - 2
+
+        action = {'text': their_turn, 'labels': [my_turn], 'episode_done': episode_done}
+        return action
+
+    def share(self):
+        shared = super().share()
+        shared['data'] = self.data
+        return shared
