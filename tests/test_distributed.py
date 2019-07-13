71700a247776abf862e0ecd7acd203239e04e920
Stephen Roller <roller@fb.com>
2019-07-12 19:42:41 -0700

    And get rid of the semaphore leak.

--- tests/test_distributed.py
+++ tests/test_distributed.py
@@ -8,7 +8,6 @@ import os
 import unittest
 import torch.distributed as dist
 import parlai.core.testing_utils as testing_utils
-import parlai.scripts.multiprocessing_train as mp_train
 import parlai.scripts.build_dict as build_dict
 
 
@@ -27,6 +26,11 @@ def _forced_parse(parser, opt):
 @testing_utils.skipUnlessGPU
 class TestDistributed(unittest.TestCase):
     def _distributed_train_model(self, opt):
+        # we have to delay our import to here, because the set_spawn_method call
+        # inside multiprocessing_train will break the multithreading tests, even
+        # when we skip the test.
+        import parlai.scripts.multiprocessing_train as mp_train
+
         with testing_utils.capture_output() as output:
             with testing_utils.tempdir() as tmpdir:
                 if 'model_file' not in opt:
