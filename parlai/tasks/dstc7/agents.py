6e7d5e216099d22c5381fe730ec015934acefa9c
Samuel Humeau <samuel.humeau@gmail.com>
2019-07-17 09:45:23 -0700

    wrong copyright

--- parlai/tasks/dstc7/agents.py
+++ parlai/tasks/dstc7/agents.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2017-present, Facebook, Inc.
-# All rights reserved.
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory.
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 """ The Ubuntu dataset used for dstc 7 """
 
 import parlai.core.build_data as build_data
