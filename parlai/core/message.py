f91ff042ca7d1ac9d5ae30ad286fbb5ae9a7f1dd
Emily Dinan <edinan@fb.com>
2019-08-06 13:00:59 -0700

    update docstring

--- parlai/core/message.py
+++ parlai/core/message.py
@@ -3,14 +3,19 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-"""File for miscellaneous utility functions and constants."""
+"""File for Message object and associated functions.
+
+The Message object's key function is to prevent users from editing
+fields in an action or observation dict unintentionally.
+"""
 
 
 class Message(dict):
     """
-    Class for observations and actions.
+    Class for observations and actions in ParlAI
 
-    Functions like a dict, but breaks when writing to fields that already exist.
+    Functions like a dict, but triggers a RuntimeError when calling __setitem__
+    for a key that already exists in the dict.
     """
 
     def __setitem__(self, key, val):
