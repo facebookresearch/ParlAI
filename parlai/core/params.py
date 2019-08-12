43315969dd6624257266a4390f6d31a7680fba83
Stephen Roller <roller@fb.com>
2019-08-12 15:18:57 -0700

    And make TorchAgent fail loudly when we get it wrong.

--- parlai/core/params.py
+++ parlai/core/params.py
@@ -432,6 +432,11 @@ class ParlaiParser(argparse.ArgumentParser):
             help='Specify location to use for scratch builds and such.',
         )
 
+        # it helps to indicaste to agents that they're in interactive mode, and
+        # can avoid some otherwise troublesome behavior (not having candidates,
+        # sharing self.replies, etc).
+        self.set_defaults(interactive_mode=True)
+
         mturk.set_defaults(is_sandbox=True)
         mturk.set_defaults(is_debug=False)
         mturk.set_defaults(verbose=False)
