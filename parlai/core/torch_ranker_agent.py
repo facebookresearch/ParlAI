e7062741fe5caa23cffcfa589e6be391c9a7baa5
Eric Smith <ems@fb.com>
2019-08-28 15:50:11 -0400

    Space fix

--- parlai/core/torch_ranker_agent.py
+++ parlai/core/torch_ranker_agent.py
@@ -154,7 +154,7 @@ class TorchRankerAgent(TorchAgent):
             if self.use_cuda:
                 self.model.cuda()
                 self.criterion.cuda()
-                
+            
             print("Total parameters: {}".format(self._total_parameters()))
             print("Trainable parameters:  {}".format(self._trainable_parameters()))
             
