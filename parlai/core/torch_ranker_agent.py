7c294d523428fa29f8cb9824664752022f534cd7
Dexter JU <5313281+dexterju@users.noreply.github.com>
2019-09-12 11:15:02 -0400

    [Bug Fix]put model in eval when encode candidates (#1991)
    * put model in eval when encode candidates
    
    * fix my english LOL

--- parlai/core/torch_ranker_agent.py
+++ parlai/core/torch_ranker_agent.py
@@ -921,6 +921,8 @@ class TorchRankerAgent(TorchAgent):
             "[ Encoding fixed candidates set from ({} batch(es) of up to 256) ]"
             "".format(len(vec_batches))
         )
+        # Put model into eval mode when encoding candidates
+        self.model.eval()
         with torch.no_grad():
             for vec_batch in tqdm(vec_batches):
                 cand_encs.append(self.encode_candidates(vec_batch))
