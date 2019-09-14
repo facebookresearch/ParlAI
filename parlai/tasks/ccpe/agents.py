68460b8417d0a100663586ea158119caaecf9491
juuhnpark <juuhnpark@gmail.com>
2019-09-14 16:38:51 -0400

    Black formatting

--- parlai/tasks/ccpe/agents.py
+++ parlai/tasks/ccpe/agents.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python3
-
 # Copyright (c) Facebook, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
 from parlai.core.teachers import FixedDialogTeacher
 from .build import build
-import os, json
+import os
+import json
+
 
 class CCPETeacher(FixedDialogTeacher):
     def __init__(self, opt, shared=None):
@@ -23,7 +23,7 @@ class CCPETeacher(FixedDialogTeacher):
         else:
             build(opt)
             self._setup_data()
-            
+
         self.reset()
 
     def num_episodes(self):
@@ -33,12 +33,8 @@ class CCPETeacher(FixedDialogTeacher):
         return sum([len(x) for x in self.data])
 
     def _setup_data(self):
-        
-        fpath = os.path.join(
-            self.opt['datapath'],
-            'CCPE',
-            'ccpe.json',
-        )
+
+        fpath = os.path.join(self.opt['datapath'], 'CCPE', 'ccpe.json')
 
         with open(fpath, 'r') as infile:
             data = infile.read()
@@ -52,16 +48,28 @@ class CCPETeacher(FixedDialogTeacher):
             for i, utterance in enumerate(json_data[ep]['utterances']):
                 entry = []
                 cnt = 0
-                if i > 0 and json_data[ep]['utterances'][i-1]['speaker'] == "USER" and utterance['speaker'] == "ASSISTANT":
+                if (
+                    i > 0
+                    and json_data[ep]['utterances'][i - 1]['speaker'] == "USER"
+                    and utterance['speaker'] == "ASSISTANT"
+                ):
                     entry.append(cnt)
-                    cnt +=1
-                    entry.append(json_data[ep]['utterances'][i-1]['text'])
+                    cnt += 1
+                    entry.append(json_data[ep]['utterances'][i - 1]['text'])
                     entry.append(utterance['text'])
-                    entry.append(json_data[ep]['utterances'][i-1]['segments'] if 'segments' in json_data[ep]['utterances'][i-1] else [])
-                    entry.append(json_data[ep]['utterances'][i]['segments'] if 'segments' in json_data[ep]['utterances'][i] else [])
+                    entry.append(
+                        json_data[ep]['utterances'][i - 1]['segments']
+                        if 'segments' in json_data[ep]['utterances'][i - 1]
+                        else []
+                    )
+                    entry.append(
+                        json_data[ep]['utterances'][i]['segments']
+                        if 'segments' in json_data[ep]['utterances'][i]
+                        else []
+                    )
                     entry.append(False)
                     currEp.append(entry)
-            
+
             currEp[-1][5] = True
             self.data.append(currEp)
 
@@ -74,14 +82,15 @@ class CCPETeacher(FixedDialogTeacher):
             'labels': [entry[2]],
             'textAnnotation': entry[3],
             'labelAnnotation': entry[4],
-            'episode_done': entry[5]
+            'episode_done': entry[5],
         }
         return action
-    
+
     def share(self):
         shared = super().share()
         shared['data'] = self.data
         return shared
+
 
 class DefaultTeacher(CCPETeacher):
     pass
--- parlai/tasks/ccpe/build.py
+++ parlai/tasks/ccpe/build.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # Copyright (c) Facebook, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
@@ -7,6 +5,7 @@
 
 import parlai.core.build_data as build_data
 import os
+
 
 def build(opt):
     dpath = os.path.join(opt['datapath'], 'CCPE')
