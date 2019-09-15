640abe8c8e1267282edaa0b18b0a8096fd309fde
vedantpuri <vedantpuri13@gmail.com>
2019-09-14 20:03:52 -0400

    Fix errors and Modify var names

--- parlai/tasks/taskmaster/agents.py
+++ parlai/tasks/taskmaster/agents.py
@@ -3,9 +3,14 @@ from .build import build
 import os, json, copy
 
 class TaskMasterTeacher(FixedDialogTeacher):
+    """
+    Base class to define a Teacher for TaskMaster-1 Google 2019
+    """
     def __init__(self, opt, shared=None):
         super().__init__(opt)
 
+        # Defaut case
+        opt['fn'] = "self-dialogs.json"
         if shared and 'convos' in shared:
             # another instance was set up already, just reference its data
             self.convos = shared['convos']
@@ -20,78 +25,110 @@ class TaskMasterTeacher(FixedDialogTeacher):
         print('loading: ' + data_path)
         with open(data_path) as data_file:
             self.convos = json.load(data_file)
+        convos_update = []
+        for convo in self.convos:
+            # Filter out single greet messages
+            if len(convo['utterances']) > 1:
+                convos_update += [convo]
+        self.convos = convos_update
 
+
+    # Number of time the assistant speaks
     def num_examples(self):
-        return len(self.convos)
+        ctr = 0
+        for convo in self.convos:
+            for sentence in convo["utterances"]:
+                if sentence["speaker"] == "ASSISTANT":
+                    ctr += 1
+        return ctr
 
     def num_episodes(self):
-        return self.num_examples()
+        return len(self.convos)
 
     def get(self, episode_idx, entry_idx):
         conversation = self.convos[episode_idx]
         conv_len = len(conversation['utterances'])
-        user_q = conversation['utterances'][entry_idx * 2]['text']
-        assistant_a = conversation['utterances'][entry_idx * 2 + 1]['text']
-        ep_done = False
+        predecessor = conversation['utterances'][entry_idx]['text']
+        successor = conversation['utterances'][entry_idx + 1]['text']
 
-        if entry_idx * 2 + 1 == conv_len - 1:
+        # Check if episode is complete
+        ep_done = False
+        if entry_idx + 1 == conv_len - 1:
             ep_done = True
+
         action = {
             'id': self.id,
-            'text': user_q,
+            'text': predecessor,
             'episode_done': ep_done
         }
-        action['labels'] = [assistant_a]
+        action['labels'] = [successor]
 
         return action
 
 class SelfDialogueTeacher(TaskMasterTeacher):
+    """
+    Teach self-dialogs.json
+    """
     def __init__(self, opt, shared=None):
         opt['fn'] = "self-dialogs.json"
         super().__init__(opt, shared)
 
 class WozDialogueTeacher(TaskMasterTeacher):
+    """
+    Teach woz-dialogs.json
+    """
     def __init__(self, opt, shared=None):
         opt['fn'] = "woz-dialogs.json"
         super().__init__(opt, shared)
 
 class SelfDialogueSegmentTeacher(SelfDialogueTeacher):
+    """
+    Teach "self-dialogs.json" with segment texts as labels
+    """
     def get(self, episode_idx, entry_idx):
         conversation = self.convos[episode_idx]
         conv_len = len(conversation['utterances'])
-        # print(conv_len, entry_idx, conversation)
         utterance = conversation['utterances'][entry_idx]['text']
-        ep_done = False
 
+        # Check if episode is complete
+        ep_done = False
         if entry_idx == conv_len - 1:
             ep_done = True
+
         action = {
             'id': self.id,
             'text': utterance,
             'episode_done': ep_done
         }
 
+        # Setup Labels as "text" from segments
+        action['labels'] = []
+        segments = conversation['utterances'][entry_idx]["segments"]
+        for segment in segments:
+            action['labels'] += [segment["text"]]
+
         return action
 
     def _setup_data(self, data_path):
-        super()._setup_data(data_path)
-        convos_update = []
+        print('loading: ' + data_path)
+        with open(data_path) as data_file:
+            self.convos = json.load(data_file)
+
+        # Filter out instances which do not have "segment" in them
+        convos_updated = []
         for convo in self.convos:
-            x = copy.deepcopy(convo['utterances'])
-            y = []
-            for i in range(0, len(x)):
-                if "segments" in x[i]:
-                    y += [x[i]]
-            convo['utterances'] = y
+            convo_copy = copy.deepcopy(convo['utterances'])
+            updated_dialog = []
+            for i in range(0, len(convo_copy)):
+                if "segments" in convo_copy[i]:
+                    updated_dialog += [convo_copy[i]]
+            convo['utterances'] = updated_dialog
             if convo['utterances']:
-                convos_update += [convo]
-        self.convos = convos_update
+                convos_updated += [convo]
+        self.convos = convos_updated
 
 # Utils
 def _path(opt):
     # ensure data is built
     build(opt)
     return os.path.join(opt['datapath'], 'taskmaster-1', opt['fn'])
-
-class DefaultTeacher(TaskMasterTeacher):
-    pass
