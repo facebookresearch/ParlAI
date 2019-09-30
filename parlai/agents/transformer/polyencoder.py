d3233450353d23f4adb92fbf2523497278523cc8
hadasah <margaretli@fb.com>
2019-09-30 13:21:36 -0400

    Check for shared before dataparallel call (#2003)

--- parlai/agents/transformer/polyencoder.py
+++ parlai/agents/transformer/polyencoder.py
@@ -95,7 +95,8 @@ class PolyencoderAgent(TorchRankerAgent):
 
             if is_distributed():
                 raise ValueError('Cannot combine --data-parallel and distributed mode')
-            self.model = torch.nn.DataParallel(self.model)
+            if shared is None:
+                self.model = torch.nn.DataParallel(self.model)
 
     def build_model(self, states=None):
         """Return built model."""
