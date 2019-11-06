2d108214f16c32cc889dd76eba2cc07c00cb2441
Young Kim <yk465@cornell.edu>
2019-11-06 10:06:55 -0500

    Fix lint

--- parlai/chat_service/core/manager_utils.py
+++ parlai/chat_service/core/manager_utils.py
@@ -3,7 +3,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from abc import ABC, abstractmethod
 import errno
 import json
 import logging
