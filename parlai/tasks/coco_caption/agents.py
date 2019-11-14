40c94b70a0c3bb96604647883e8e1bd1888858a7
Dom Rigoglioso <drrigoglioso@gmail.com>
2019-11-14 15:38:41 -0500

    Change image_mode from none to no_image_model (#2112)
    * changed lines with image mode to stop using none
    
    * update TestAbstractImageTeacher CircleCI test to use no_image_model

--- parlai/tasks/coco_caption/agents.py
+++ parlai/tasks/coco_caption/agents.py
@@ -280,7 +280,7 @@ class DefaultTeacher(FixedDialogTeacher):
     def __init__(self, opt, shared=None, version='2017'):
         super().__init__(opt, shared)
         self.version = version
-        self.image_mode = opt.get('image_mode', 'none')
+        self.image_mode = opt.get('image_mode', 'no_image_model')
         self.use_intro = opt.get('use_intro', False)
         self.num_cands = opt.get('num_cands', -1)
         self.include_rest_val = opt.get('include_rest_val', False)
@@ -425,14 +425,14 @@ class DefaultTeacher(FixedDialogTeacher):
         ready = None
         # pull up the currently queued example
         if self.example is not None:
-            if self.image_mode != 'none' and 'image_id' in self.example:
+            if self.image_mode != 'no_image_model' and 'image_id' in self.example:
                 # move the image we loaded in the background into the example
                 image = self.data_queue.get()
                 self.example['image'] = image
             ready = (self.example, self.imageEpochDone)
         # get the next base example: super().next_example() calls self.get()
         self.example, self.imageEpochDone = super().next_example()
-        if self.image_mode != 'none' and 'image_id' in self.example:
+        if self.image_mode != 'no_image_model' and 'image_id' in self.example:
             # load the next image in the background
             image_id = self.example['image_id']
             split = self.example.get('split', None)
