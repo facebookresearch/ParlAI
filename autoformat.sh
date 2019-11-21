8ff718ebe5b7c0547056166e5340baee3aada51c
Stephen Roller <roller@fb.com>
2019-11-21 14:21:08 -0800

    comment change.

--- autoformat.sh
+++ autoformat.sh
@@ -4,9 +4,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# This shell script lints only the things that changed in the most recent change.
-
-
 # exit on errors
 set -e
 
