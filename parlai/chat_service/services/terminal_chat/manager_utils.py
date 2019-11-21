8a3eb33f1e52a176accd2c8016147cecb1c05dff
vedantpuri <vedantpuri13@gmail.com>
2019-11-21 12:24:35 -0500

    init manager utils

---
+++ parlai/chat_service/services/terminal_chat/manager_utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+from parlai.chat_service.core.manager_utils import ChatServiceMessageSocket
+
+# Socket handler
+class TerminalMessageSocket(ChatServiceMessageSocket):
+    """ChatServiceMessageSocket is a wrapper around websocket to forward messages from the
+    remote server to the ChatServiceManager."""
+
+
+x = TerminalMessageSocket('https://localhost', 12345, None)
+print(x.ws)
--- parlai/chat_service/services/terminal_chat/playground/client.py
+++ parlai/chat_service/services/terminal_chat/playground/client.py
@@ -10,7 +10,7 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 12345)
+server_address = ('ws://localhost', 12345)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
 
