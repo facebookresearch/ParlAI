557ae6bae5f8dd88f397f0dcca29e51ebd4f3f32
Stephen Roller <roller@fb.com>
2019-11-21 12:13:00 -0800

    More explicit.

--- autoformat.sh
+++ autoformat.sh
@@ -7,74 +7,130 @@
 # This shell script lints only the things that changed in the most recent change.
 
 
-DOCOPTS="--pre-summary-newline --wrap-descriptions 88 --wrap-summaries 88 --make-summary-multi-line"
+DOCOPTS="--pre-summary-newline --wrap-descriptions 88 --wrap-summaries 88 \
+    --make-summary-multi-line"
 
 set -e
 
-onlyexists() {
+usage () {
+    cat <<EOF
+Usage: $0
+
+Autoformats (or checks) code so it conforms with ParlAI standards. By default,
+runs black, flake8, and docformatter only on the changed files in the current branch.
+
+Optional Arguments
+
+  -a, --all          run on all files, not just changed ones.
+  -b, --black        only run black.
+  -c, --check        perform a check, but don't make any changes.
+  -d, --doc          only run docformatter.
+  -f, --flake8       only run flake8.
+  -h, --help         print this help message and exit
+  -i, --internal     run within parlai_internal
+EOF
+}
+
+reroot() {
+    # possibly rewrite all filenames if root is nonempty
     if [[ "$1" != "" ]]; then
-        PRE="$1/"
+        cat | xargs -I '{}' realpath --relative-to=. $1/'{}'
     else
-        PRE=""
+        cat
     fi
+}
+
+onlyexists() {
+    # filter filenames based on what exists on disk
     while read fn; do
-        if [ -f "${PRE}${fn}" ]; then
+        if [ -f "${fn}" ]; then
             echo "$fn"
         fi
     done
 }
 
+RUN_ALL_FILES=0
 RUNALL=1
 INTERNAL=0
 CHECK=0
 CMD=""
-CHANGED_FILES="$(git diff --name-only master... | grep '\.py$' | onlyexists | tr '\n' ' ')"
-while getopts bidcf opt; do
-  case $opt in
-    f)
+while true; do
+  case $1 in
+    -h | --help)
+      usage
+      exit 0
+      ;;
+    -a | --all)
+      RUN_ALL_FILES=1
+      ;;
+    -f | --flake8)
       [[ "$CMD" != "" ]] && (echo "Don't mix args." && false);
       RUNALL=0
       CMD="flake8"
       ;;
-    c)
+    -c | --check)
       CHECK=1
       ;;
-    d)
+    -d | --doc)
       [[ "$CMD" != "" ]] && (echo "Don't mix args." && false);
       CMD="docformatter"
       RUNALL=0
       ;;
-    i)
+    -i | --internal)
       INTERNAL=1
-      ROOT="$(git -C ./parlai_internal/ rev-parse --show-toplevel)"
-      CHANGED_FILES="$(git -C ./parlai_internal/ diff --name-only master... | onlyexists $ROOT |
-      xargs -I '{}' realpath --relative-to=. $ROOT/'{}' |
-      grep '\.py$' | tr '\n' ' ')"
       ;;
-    b)
+    -b | --black)
       [[ "$CMD" != "" ]] && (echo "Don't mix args." && false);
       CMD="black"
       RUNALL=0
       ;;
+    "")
+      break
+      ;;
+    *)
+      usage
+      echo
+      echo "Cannot handle arg $1."
+      exit 1
+      ;;
   esac
+  shift
 done
+
+if [[ $INTERNAL -eq 1 ]]; then
+    ROOT="$(git -C ./parlai_internal/ rev-parse --show-toplevel)"
+    REPO="-C ./parlai_internal"
+else
+    ROOT=""
+    REPO=""
+fi
+
+if [[ $RUN_ALL_FILES -eq 1 ]]; then
+    CHECK_FILES="$(git $REPO ls-files | grep '\.py$' | reroot $ROOT | onlyexists $ROOT | tr '\n' ' ')"
+else
+    CHECK_FILES="$(git $REPO diff --name-only master... | grep '\.py$' | reroot $ROOT | onlyexists | tr '\n' ' ')"
+fi
 
 if [[ $RUNALL -eq 1 ]]
 then
     if [[ $CHECK -eq 1 ]]; then A="$A -c"; fi
     if [[ $INTERNAL -eq 1 ]]; then A="$A -i"; fi
+    if [[ $RUN_ALL_FILES -eq 1 ]]; then A="$A -a"; fi
     echo "Black:"
-    bash $0 -b $A
+    bash $0 --black $A
     echo "------------------------------------------------------------------------------"
     echo "Doc formatting:"
-    bash $0 -d $A
+    bash $0 --doc $A
     echo "------------------------------------------------------------------------------"
     echo "Flake8:"
-    bash $0 -f $A
+    bash $0 --flake8 $A
     exit 0
 fi
 
-if [ "$CHANGED_FILES" != "" ]
+echo "Checking files:"
+echo $CHECK_FILES
+
+if [ "$CHECK_FILES" != "" ]
 then
     if [[ "$CMD" == "black" ]]
     then
@@ -82,9 +138,9 @@ then
             ( echo "Please run \`pip install black\` and rerun $0." && false )
         if [[ $CHECK -eq 0 ]]
         then
-            black $CHANGED_FILES
+            black $CHECK_FILES
         else
-            black --check $CHANGED_FILES
+            black --check $CHECK_FILES
         fi
     elif [[ "$CMD" == "docformatter" ]]
     then
@@ -92,10 +148,10 @@ then
             ( echo "Please run \`pip install docformatter\` and rerun $0." && false )
         if [[ $CHECK -eq 0 ]]
         then
-            docformatter -i $DOCOPTS $CHANGED_FILES
+            docformatter -i $DOCOPTS $CHECK_FILES
         else
             echo "The following require doc formatting:"
-            docformatter -c $DOCOPTS $CHANGED_FILES
+            docformatter -c $DOCOPTS $CHECK_FILES
         fi
     elif [[ "$CMD" == "flake8" ]]
     then
@@ -103,9 +159,9 @@ then
             ( echo "Please run \`pip install flake8\` and rerun $0." && false )
 
         # soft complaint on too-long-lines
-        flake8 --select=E501 --show-source $CHANGED_FILES
+        flake8 --select=E501 --show-source $CHECK_FILES
         # hard complaint on really long lines
-        exec flake8 --max-line-length=127 --show-source $CHANGED_FILES
+        exec flake8 --max-line-length=127 --show-source $CHECK_FILES
     else
         echo "Don't know how to \`$CMD\`."
         false
