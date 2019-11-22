edbe4847d802e749611ff19d4cf11f4b2d93f83b
Jason Weston <jase@fb.com>
2019-11-22 13:57:39 -0800

    WIP

--- parlai/tasks/wizard_of_wikipedia/agents.py
+++ parlai/tasks/wizard_of_wikipedia/agents.py
@@ -17,6 +17,8 @@
     E.g. `wizard_of_wikipedia:WizardDialogKnowledgeTeacher:random_split`
 """
 
+import copy
+import parlai.core.agents as core_agents
 from parlai.core.agents import create_task_agent_from_taskname
 from parlai.core.teachers import FixedDialogTeacher
 from .build import build
@@ -420,6 +422,12 @@ class BasicApprenticeDialogTeacher(BasicdialogTeacher):
         self.speaker_label = 'apprentice'
         self.add_topic = True
 
+class BasicBothDialogTeacher(core_agents.MultiTaskTeacher):
+    def __init__(self, opt, shared=None):
+        opt = copy.deepcopy(opt)
+        opt['task'] = 'wizard_of_wikipedia:BasicApprenticeDialog,wizard_of_wikipedia:BasicWizardDialog'
+        super().__init__(opt, shared)    
+    
 
 ###############################################################
 #                                                             #
@@ -907,7 +915,7 @@ class DefaultTeacher(WizardDialogKnowledgeTeacher):
     pass
 
 
-class SelfchatTeacher(WizardDialogKnowledgeTeacher):
+class SelfchatTeacher(BasicBothDialogTeacher):
     """
     Teacher used to create candidates for selfchats, if needed.
     """
