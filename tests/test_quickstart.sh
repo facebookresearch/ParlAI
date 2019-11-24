aaca5d5abf0df6abe4c7e6c158b325172dcbe689
vedantpuri <vedantpuri13@gmail.com>
2019-11-24 14:37:35 -0500

    echo node index qs

--- tests/test_quickstart.sh
+++ tests/test_quickstart.sh
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+echo $CIRCLE_NODE_INDEX && exit
+
 set -e  # error and exit on any failure
 
 # view a task & train a model
