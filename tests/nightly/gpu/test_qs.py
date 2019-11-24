c2ff76b90a877525d1ec1777d5e61104c782d023
vedantpuri <vedantpuri13@gmail.com>
2019-11-24 00:10:49 -0500

    Try to singularize quickstart

---
+++ tests/nightly/gpu/test_qs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+import subprocess
+
+
+@testing_utils.skipUnlessGPU
+class TestQuickStart(unittest.TestCase):
+    """Runs the quickstart test script"""
+
+    def run_quickstart_sh(self):
+        subprocess.call(['sh tests/test_quickstart.sh'])
+
+
+if __name__ == '__main__':
+    unittest.main()
