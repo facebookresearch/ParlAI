7e89198432bb0b2673a4d58a15af40dbc7ebd5a9
klshuster <kshuster@fb.com>
2019-12-10 15:36:08 -0800

    image seq2seq agent

---
+++ parlai/agents/image_seq2seq/agents.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+"""
+Image+Seq2Seq Agent.
+"""
+from fairseq import bleu
+import torch
+from typing import Dict, List, Tuple
+
+from .modules import ImageSeq2seqModel
+from parlai.agents.transformer.transformer import TransformerGeneratorAgent
+from parlai.core.dict import DictionaryAgent
+from parlai.core.message import Message
+from parlai.core.torch_agent import Batch, Output
+from parlai.utils.misc import round_sigfigs, Opt
+
+try:
+    from nltk.translate import bleu_score as nltkbleu
+except ImportError:
+    nltkbleu = None
+
+TOKEN_IMAGE = '__image__'
+TOKEN_NO_IMAGE = '__no_image__'
+
+
+class ImageSeq2seqAgent(TransformerGeneratorAgent):
+    """
+    ImageSeq2seqAgent Agent.
+
+    Combines a transformer generator with images.
+    """
+
+    def __init__(self, opt: Opt, shared: dict = None):
+        super().__init__(opt, shared)
+        self.compute_tokenized_bleu = opt.get('compute_tokenized_bleu', False)
+        if not shared:
+            self._init_bleu_scorers()
+        else:
+            self.fairseq_bleu_scorer = shared['fairseq_bleu_scorer']
+            self.my_parlai_bleu = shared['my_parlai_bleu']
+            self.my_parlai_bleu_cnts = shared['my_parlai_bleu_cnts']
+
+    def _init_bleu_scorers(self):
+        if not hasattr(self, 'fairseq_bleu_scorer'):
+            self.fairseq_bleu_scorer = bleu.Scorer(
+                self.NULL_IDX, self.END_IDX, self.dict[self.dict.unk_token]
+            )
+        self.my_parlai_bleu = {f'bleu-{i}': 0 for i in range(1, 5)}
+        self.my_parlai_bleu_cnts = {f'bleu-{i}': 0 for i in range(1, 5)}
+
+    def share(self) -> dict:
+        """
+        Override to include BLEU scorer.
+        """
+        shared = super().share()
+        shared['fairseq_bleu_scorer'] = self.fairseq_bleu_scorer
+        shared['my_parlai_bleu'] = self.my_parlai_bleu
+        shared['my_parlai_bleu_cnts'] = self.my_parlai_bleu_cnts
+        return shared
+
+    def build_model(self) -> ImageSeq2seqModel:
+        """
+        Override to build appropriate model.
+        """
+        self.model = ImageSeq2seqModel(self.opt, self.dict)
+        if self.opt['embedding_type'] != 'random':
+            self._copy_embeddings(
+                self.model.embeddings.weight, self.opt['embedding_type']
+            )
+        if self.use_cuda:
+            self.model = self.model.cuda()
+
+        return self.model
+
+    def reset_metrics(self):
+        super().reset_metrics()
+        self._init_bleu_scorers()
+
+    @classmethod
+    def add_cmdline_args(cls, argparser):
+        """
+        Override to add one arg.
+        """
+        super(ImageSeq2seqAgent, cls).add_cmdline_args(argparser)
+        group = argparser.add_argument_group('Image Encoder Args')
+        group.add_argument(
+            '--image-features-dim', type=int, default=2048, help='dim for image feats'
+        )
+        group.add_argument(
+            '--image-encoder-num-layers',
+            type=int,
+            default=1,
+            recommended=1,
+            help='Number of layers for image encoder',
+        )
+        group.add_argument(
+            '--include-image-token',
+            type='bool',
+            default=True,
+            recommended=True,
+            help='if true, include image token (or no image token) for each example',
+        )
+        group.add_argument(
+            '--compute-tokenized-bleu',
+            type='bool',
+            default=False,
+            help='if true, include image token (or no image token) for each example',
+        )
+
+    def build_dictionary(self) -> DictionaryAgent:
+        """
+        Override to include image tokens.
+        """
+        self.dict = super().build_dictionary()
+        if self.opt.get('include_image_token') and TOKEN_IMAGE not in self.dict:
+            self.dict[TOKEN_IMAGE] = 1
+            self.dict[TOKEN_NO_IMAGE] = 1
+
+        return self.dict
+
+    def _set_text_vec(self, *args, **kwargs) -> dict:
+        """
+        Override to include image token.
+        """
+        obs = super()._set_text_vec(*args, **kwargs)
+        if 'text' not in obs or 'text_vec' not in obs:
+            return obs
+        if self.opt.get('include_image_token', False):
+            truncate = args[2]
+            vec = torch.LongTensor(
+                self._check_truncate(obs['text_vec'], truncate - 1, True)
+            )
+            token = TOKEN_NO_IMAGE
+            if obs.get('image', None) is not None:
+                token = TOKEN_IMAGE
+            obs.force_set(
+                'text_vec',
+                torch.cat([vec, vec.new_tensor(self.dict[token]).unsqueeze(0)], 0),
+            )
+        return obs
+
+    def _dummy_batch(self, batchsize: int, maxlen: int) -> Batch:
+        """
+        Override to include image feats.
+        """
+        return Batch(
+            text_vec=torch.ones(batchsize, maxlen).long().cuda(),
+            label_vec=torch.ones(batchsize, 2).long().cuda(),
+            image=torch.ones(batchsize, self.opt.get('image_features_dim')).cuda(),
+            personalities=torch.ones(batchsize, self.opt.get('embedding_size')).cuda(),
+        )
+
+    def batchify(self, obs_batch: List[Message], sort: bool = False) -> Batch:
+        """
+        Override to handle images.
+        """
+        batch = super().batchify(obs_batch, sort)
+
+        def _process_img(img):
+            if img is not None and isinstance(img, torch.Tensor):
+                if img.dim() == 4:
+                    img = img[0, :, 0, 0]
+                if self.use_cuda:
+                    img = img.cuda()
+                if self.opt.get('fp16'):
+                    img = img.half()
+                else:
+                    img = img.float()
+
+            return img
+
+        if type(batch.image) == list and any(b is not None for b in batch):
+            images = []
+            for img in batch.image:
+                images.append(_process_img(img))
+            batch.image = images
+        return batch
+
+    def _model_input(self, batch: Batch) -> Tuple[torch.Tensor, List[object]]:
+        return (batch.text_vec, batch.image)
+
+    def load_state_dict(self, state_dict: Dict[str, torch.Tensor]):
+        """
+        Override for custom loading.
+
+        Three reasons:
+            1. When using an init model without an image encoder
+            2. When using an init model with only an encoder provided
+                In this case, we may need to add the START token to the state_dict
+            3. When using an init model without image tokens in the embeddings
+        """
+        # Case 1 -> No Image Encoder
+        if 'encoder.image_encoder.0.weight' not in state_dict:
+            for k, v in self.model.encoder.image_encoder.state_dict().items():
+                state_dict[f'encoder.image_encoder.{k}'] = v
+
+        # Case 2 -> Only an Encoder provided
+        if not (any('decoder' in state_key for state_key in state_dict)):
+            for k, v in self.model.decoder.state_dict().items():
+                state_dict[f'decoder.{k}'] = v
+            state_dict['decoder.embeddings.weight'] = state_dict['embeddings.weight']
+            if 'START' not in state_dict:
+                state_dict['START'] = self.model.START
+
+        cnt = 0
+        if self.opt['init_model'] is not None:
+            try:
+                self.model.load_state_dict(state_dict)
+                return
+            except Exception as e:
+                # Case 3 --> Check for Embedding Diffs
+                print('exception in loading state dict: {}'.format(e))
+                # make sure dims match up
+                embs = state_dict['embeddings.weight']
+                enc_embs = state_dict['encoder.embeddings.weight']
+                dec_embs = state_dict['decoder.embeddings.weight']
+                num_embs = min(self.model.embeddings.weight.shape[0], embs.shape[0])
+                num_enc_embs = min(
+                    self.model.encoder.embeddings.weight.shape[0], enc_embs.shape[0]
+                )
+                num_dec_embs = min(
+                    self.model.decoder.embeddings.weight.shape[0], dec_embs.shape[0]
+                )
+                for i in range(num_embs):
+                    self.model.embeddings.weight.data[i] = embs[i]
+                    cnt += 1
+                for i in range(num_enc_embs):
+                    self.model.encoder.embeddings.weight.data[i] = enc_embs[i]
+                for i in range(num_dec_embs):
+                    self.model.decoder.embeddings.weight.data[i] = dec_embs[i]
+                state_dict['embeddings.weight'] = self.model.embeddings.weight
+                state_dict[
+                    'encoder.embeddings.weight'
+                ] = self.model.encoder.embeddings.weight
+                state_dict[
+                    'decoder.embeddings.weight'
+                ] = self.model.decoder.embeddings.weight
+
+                print(
+                    'Initialized embeddings for {} tokens ({}%).'
+                    ''.format(cnt, round(cnt * 100 / len(self.dict), 1))
+                )
+        self.model.load_state_dict(state_dict)
+
+    def eval_step(self, batch: Batch) -> Output:
+        """
+        Evaluate a single batch of examples.
+
+        Override to compute correct BLEU scores.
+        """
+        output = super().eval_step(batch)
+        if self.skip_generation and not self.compute_tokenized_bleu:
+            return output
+
+        texts = output.text
+        self._compute_fairseq_bleu(batch, texts)
+        self._compute_parlai_bleu(batch, texts)
+
+        return output
+
+    def report(self) -> dict:
+        """
+        Override to include custom BLEU computations.
+        """
+        metrics = super().report()
+        if not self.skip_generation and self.compute_tokenized_bleu:
+            try:
+                fairseq_bleu_scores = {
+                    k: self.fairseq_bleu_scorer.result_string(order=k) for k in range(1, 5)
+                }
+            except ZeroDivisionError:
+                # some preds are REAL bad
+                fairseq_bleu_scores = {
+                    k: 0 for k in range(1, 5)
+                }
+
+            metrics['fairseq_bleu'] = {
+                k: v[v.index('= '):v.index(',')] for k, v in fairseq_bleu_scores.items()
+            }
+            metrics['parlai_bleu_unnormalized'] = {
+                k: round_sigfigs(
+                    v / self.my_parlai_bleu_cnts[k], 4
+                ) for k, v in self.my_parlai_bleu.items()
+            }
+        return metrics
+
+    def _compute_fairseq_bleu(self, batch: Batch, texts: List[str]):
+        """
+        Compute BLEU score between text and label, using the FAIRSeq BLEU Scorer.
+
+        :param batch:
+        """
+        aa = torch.IntTensor(1)
+        for i, t in enumerate(texts):
+            self.fairseq_bleu_scorer.add(
+                batch.label_vec[i].type_as(aa),
+                self._vectorize_text(t, True, True, self.label_truncate, False).type_as(
+                    aa
+                ),
+            )
+
+    def _compute_parlai_bleu(self, batch: Batch, texts: List[str]):
+
+        def _bleu(guess: str, answers: List[str], weights: List[float]):
+            """
+            Compute approximate BLEU score between guess and a set of answers.
+
+            This function does not process guess or answers, as opposed to the normal
+            bleu function in metrics.py
+            """
+            if nltkbleu is None:
+                return 0
+            return nltkbleu.sentence_bleu(
+                [a.split(" ") for a in answers],
+                guess.split(" "),
+                smoothing_function=nltkbleu.SmoothingFunction(epsilon=1e-12).method1,
+                weights=weights,
+            )
+
+        for i, p in enumerate(texts):
+            obs = batch.observations[i]
+            references = []
+            for lbl in obs['eval_labels']:
+                references.append(
+                    self._v2t(
+                        self._vectorize_text(
+                            lbl, True, True, self.label_truncate, False
+                        )
+                    )
+                )
+            for i in range(4):
+                weights = [1 / (i + 1) for _ in range(i + 1)]
+                self.my_parlai_bleu[f'bleu-{i + 1}'] += _bleu(p, references, weights)
+                self.my_parlai_bleu_cnts[f'bleu-{i + 1}'] += 1
+
+    # def announce_bleu_score(self):
+    #     """
+    #     Announce final bleu scores.
+    #
+    #     The fairseq scorer works with just 1 reference, while the parlai bleu scorer
+    #     works with multiple
+    #     """
+    #     bleus = {k: self.fairseq_bleu_scorer.result_string(order=k) for k in range(1, 5)}
+    #     print(f'FINAL BLEU: {bleus}')
+    #     self.my_parlai_bleu = {
+    #         k: v / self.my_parlai_bleu_cnts[k] for k, v in self.my_parlai_bleu.items()
+    #     }
+    #     print(f'FINAL PARLAI BLEU: {self.my_parlai_bleu}')
---
+++ tests/test_image_seq2seq.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+import parlai.utils.testing as testing_utils
+
+BASE_ARGS = {
+    # Model Args
+    'model': 'image_seq2seq',
+    'embedding_size': 32,
+    'n_heads': 2,
+    'n_layers': 2,
+    'n_positions': 128,
+    'truncate': 128,
+    'ffn_size': 128,
+    'image_features_dim': 2048,
+    'variant': 'xlm',
+    'activation': 'gelu',
+    'embeddings_scale': True,
+    'gradient_clip': 0.1,
+    'image_mode': 'resnet152',
+    # Train args
+    'learningrate': 7e-3,
+    'batchsize': 16,
+    'optimizer': 'adamax',
+    'learn_positional_embeddings': True,
+}
+
+TEXT_ARGS = {
+    'task': 'integration_tests:nocandidate',
+    'num_epochs': 4
+}
+
+IMAGE_ARGS = {
+    'task': 'integration_tests:ImageTeacher',
+    'validation_every_n_epochs': 50,
+    'num_epochs': 200,
+}
+
+MULTITASK_ARGS = {
+    'task': ','.join([m['task'] for m in [IMAGE_ARGS, TEXT_ARGS]]),
+    'validation_every_n_epochs': 2,
+    'num_epochs': 10,
+    'multitask_weights': [1, 50]
+}
+
+EVAL_ARGS = {
+    'task': 'integration_tests:nocandidate',
+    'skip_generation': False,
+    'inference': 'beam',
+    'beam_size': 2,
+    'metrics': 'all'
+}
+
+
+class TestImageSeq2Seq(unittest.TestCase):
+    """
+    Unit tests for the ImageSeq2Seq Agent.
+
+    Mostly testing that the agent cooperates with tasks accordingly.
+    """
+    @testing_utils.retry(ntries=3)
+    def test_text_task(self):
+        """Test that model correctly handles text task"""
+        args = BASE_ARGS.copy()
+        args.update(TEXT_ARGS)
+        stdout, valid, test = testing_utils.train_model(args)
+        self.assertLessEqual(
+            valid['ppl'], 1.5,
+            f'failed to train image_seq2seq on text task: {stdout}'
+        )
+
+    @testing_utils.retry(ntries=3)
+    def test_image_task(self):
+        """Test that model correctly handles image task.
+        No training, only eval
+        """
+        args = BASE_ARGS.copy()
+        args.update(IMAGE_ARGS)
+
+        stdout, valid, test = testing_utils.train_model(args)
+        self.assertLessEqual(valid['ppl'], 6.6,
+            f'failed to train image_seq2seq on image task: {stdout}')
+
+    @testing_utils.retry(ntries=3)
+    def test_multitask(self):
+        """
+        Test that model can handle multiple inputs.
+        """
+        args = BASE_ARGS.copy()
+        args.update(MULTITASK_ARGS)
+
+        stdout, valid, test = testing_utils.train_model(args)
+        self.assertLessEqual(valid['ppl'], 5.0,
+            f'failed to train image_seq2seq on image+text task: {stdout}'
+        )
+
+    def test_compute_tokenized_bleu(self):
+        """
+        Test that the model outputs self-computed bleu correctly.
+        """
+        args = BASE_ARGS.copy()
+        args.update(EVAL_ARGS)
+
+        stdout, valid, _ = testing_utils.eval_model(args, skip_test=True)
+        self.assertIn('fairseq_bleu', valid)
+        self.assertIn('parlai_bleu_unnormalized', valid)
+
+
+if __name__ == '__main__':
+    unittest.main()
