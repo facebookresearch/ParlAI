ea19959833d3250e03803d1c513c9686c9ea0ad7
jaseweston <jase@fb.com>
2019-12-17 15:36:42 -0500

    [light] Small light interactive bugs (#2271)
    * moar
    
    * lint

--- parlai/tasks/light_dialog/worlds.py
+++ parlai/tasks/light_dialog/worlds.py
@@ -43,7 +43,8 @@ class InteractiveSimpleWorld(DialogPartnerWorld):
             p[t.split(' ')[0]] = t
 
         a1_persona = (
-            p['_setting_name']
+            ' _task_speech\n'
+            + p['_setting_name']
             + '\n'
             + p['_setting_desc']
             + '\n'
@@ -57,9 +58,11 @@ class InteractiveSimpleWorld(DialogPartnerWorld):
         )
 
         a2_persona = (
-            p['_setting_name']
+            ' _task_speech\n'
+            + p['_setting_name']
             + '\n'
             + p['_setting_desc']
+            + '\n'
             + p['_partner_name']
             + '\n'
             + p['_self_name']
