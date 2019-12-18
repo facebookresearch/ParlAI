01b77d22c1537e16be27006b69c0861aff36e347
Kurt Shuster <kshuster@fb.com>
2019-12-18 13:49:39 -0500

    remove commented code

--- docs/source/generate_agent_list.py
+++ docs/source/generate_agent_list.py
@@ -35,41 +35,15 @@ def write_agent(fout, agent_readme):
     """
     Write agent README to file.
 
+    Handle a few special cases:
+    - Attempt to convert section headings in MD to RST
+    - Attempt to convert MD bash blocks to RST bash blocks
+
     :param fout:
         file object
-
     :param agent_readme:
         string to write
     """
-
-    # pound_to_heading = {
-    #     0: '',
-    #     1: '#',
-    #     2: '*',
-    #     3: '=',
-    #     4: '-',
-    #     5: '^',
-    #     6: '\"'
-    # }
-
-    pound_to_heading = {
-        0: '',
-        1: '*',
-        2: '-',
-        3: '-',
-        4: '-',
-        5: '-'
-    }
-
-    # pound_to_heading = {
-    #     0: '',
-    #     1: '*',
-    #     2: '=',
-    #     3: '-',
-    #     4: '^',
-    #     5: '\"'
-    # }
-
     i = 0
     while i < len(agent_readme):
         line = agent_readme[i]
@@ -78,29 +52,18 @@ def write_agent(fout, agent_readme):
             fout.write('\n')
             i += 1
             continue
-        header_count = split[0].count('#')
-        if i != 0 and header_count > 0:
-            header_count += 1  # Not a title
-        header = pound_to_heading[header_count]
+        header = ''
+        is_header = split[0].count('#') > 0
+        if i == 0 and is_header:
+            header = '*'  #  title
+        elif is_header:
+            header = '-'
         if header:  # Format Headings
             title = ' '.join(split[1:])
             fout.write(f'{title}\n')
             fout.write(f'{header * len(title)}\n')
             i += 1
         elif '```bash' in line:  # Format Bash Commands
-            # if '```\n' in agent_readme[i + 1:]:
-            #     last_bash_line = agent_readme.index('```\n', i + 1)
-            # elif '```' in agent_readme:
-            #     last_bash_line = agent_readme.index('```', i + 1)
-            # else:
-            #     last_bash_line = i + 1
-            # new_lines = agent_readme[i: last_bash_line + 1]
-            # new_lines = [l.replace('```', '') for l in new_lines]
-            # new_lines = ['.. code-block:: bash'] + [f"  {l.replace('```', '')}" for l in new_lines[1:]]
-            # new_line = '\n\n'.join(new_lines)
-            #
-            # fout.write(f'{new_line}\n')
-            # i += len(new_lines)
             line = '\n\n.. code-block:: bash\n'
             fout.write(line)
             agent_readme[i + 1] = f'\n  {agent_readme[i+1]}'
