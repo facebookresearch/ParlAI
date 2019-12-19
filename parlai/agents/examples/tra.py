bfe9bc6087fe7d920f2d9c4699a7711c0c807082
Dexter Ju <da.ju.fr@gmail.com>
2019-12-19 10:36:44 -0800

    updates docstring

--- parlai/agents/examples/tra.py
+++ parlai/agents/examples/tra.py
@@ -14,18 +14,32 @@ from torch import nn
 
 
 class ExampleBagOfWordsModel(nn.Module):
+    """
+    This constructs a simple bag of words model. 
+    It contains a encoder for encoding candidates and context.
+    """
+
     def __init__(self, opt, dictionary):
         super().__init__()
         self.hidden_dim = opt.get('hidden_dim', 512)
         self.dict = dictionary
-        self.context_encoder = nn.EmbeddingBag(len(self.dict), self.hidden_dim)
-        self.candidate_encoder = nn.EmbeddingBag(len(self.dict), self.hidden_dim)
+        self.encoder = nn.EmbeddingBag(len(self.dict), self.hidden_dim)
 
     def forward_candidates(self, cand_vecs):
-        return self.candidate_encoder.forward(cand_vecs)
+        """
+        This function will encode all the candidates for a batch. It can be different cases:
+        1. inline candidate: we have inline candidates provided for each sample, it usually happens during eval / test time
+        In this case, the cand_vecs is a 3D tensor, [btz, num_candidates, seq_length]
+        2. batch candidates, it usually happens during training, cand_vecs will be [btz, seq_length]
+        3. fixed, when you have a fixed set of candidates to use, cand_vecs will be [num_candidates, seq_length]
+        """
+        return self.encoder.forward(cand_vecs)
 
     def forward_context(self, batch):
-        return self.context_encoder(batch.text_vec)
+        """
+        This function takes in a Batch object and will encode the context.
+        """
+        return self.encoder(batch.text_vec)
 
     def forward(self, batch, cand_vecs, cand_encs=None):
         btz = cand_vecs.size(0)
@@ -34,7 +48,7 @@ class ExampleBagOfWordsModel(nn.Module):
                 # if dim = 3, btz *  num_candidates * seq_length
                 # In this case, we are using inline candidates
                 cand_vecs = cand_vecs.reshape(-1, cand_vecs.size(2))
-            cand_encs = self.candidate_encoder.forward(cand_vecs)
+            cand_encs = self.forward_candidates(cand_vecs)
             if cand_encs.size(0) != btz:
                 # Some cases, we could also use batch candidates,
                 # they will be size of btz * seq_length
@@ -42,7 +56,6 @@ class ExampleBagOfWordsModel(nn.Module):
                 cand_encs = cand_encs.reshape(btz, -1, self.hidden_dim)
         context_encodings = self.forward_context(batch)
         if context_encodings.dim() != cand_encs.dim():
-            # it means in eval mode
             return torch.sum(
                 context_encodings.unsqueeze(1).expand_as(cand_encs) * cand_encs, 2
             )
@@ -50,6 +63,14 @@ class ExampleBagOfWordsModel(nn.Module):
 
 
 class TraAgent(TorchRankerAgent):
+    """
+    This subclass the torch ranker agent.
+    What you need to do:
+    1. add the cmd flags you need.
+    2. Implements the build_model function
+    3. Implements the score candidates function
+    """
+
     @classmethod
     def add_cmdline_args(cls, argparser):
         """
@@ -59,13 +80,9 @@ class TraAgent(TorchRankerAgent):
         arg_group = argparser.add_argument_group('ExampleBagOfWordsModel Arguments')
         arg_group.add_argument('--hidden-dim', type=int, default=512)
 
-    def __init__(self, opt, shared=None):
-        super().__init__(opt, shared)
-        self.opt = opt
-        return
-
     def score_candidates(self, batch, cand_vecs, cand_encs=None):
-        """This function takes in a Batch object as well as a Tensor of
+        """
+        This function takes in a Batch object as well as a Tensor of
         candidate vectors. It must return a list of scores corresponding to
         the likelihood that the candidate vector at that index is the
         proper response. If `cand_encs` is not None (when we cache the
@@ -76,7 +93,8 @@ class TraAgent(TorchRankerAgent):
         return scores
 
     def build_model(self):
-        """This function is required to build the model and assign to the
+        """
+        This function is required to build the model and assign to the
         object `self.model`.
         """
         self.model = ExampleBagOfWordsModel(self.opt, self.dict)
