85705572b7f453cdf18122769ac011cc825bf54c
klshuster <kshuster@fb.com>
2020-01-06 11:37:31 -0800

    update docstring

--- parlai/tasks/dstc7/agents.py
+++ parlai/tasks/dstc7/agents.py
@@ -101,7 +101,27 @@ class DSTC7Teacher(FixedDialogTeacher):
 
 class DSTC7TeacherAugmented(DSTC7Teacher):
     """
-    Augmented.
+    Augmented Data.
+
+    To mimic the way ParlAI generally handles dialogue datasets, the data associated
+    with this teacher is presented in a format such that a single "episode" is split
+    across multiple entries.
+
+    I.e., suppose we have the following dialogue between speakers 1 and 2:
+    utterances: [A, B, C, D, E],
+    label: F
+
+    The data in this file is split such that we have the following episodes:
+
+    ep1:
+        utterances: [A],
+        label: B
+    ep2:
+        utterances [A, B, C]
+        label: D
+    ep3:
+        utterances: [A, B, C, D, E],
+        label: F
     """
 
     def get_suffix(self):
