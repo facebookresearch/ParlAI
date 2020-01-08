43249724449795efcfed8ecfe93c0ac9b77cce5a
Eric Smith <ems@fb.com>
2020-01-08 11:53:33 -0500

    Add stub

---
+++ tests/test_metrics.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from parlai.core.metrics import AverageMetric, SumMetric
+
+import unittest
+
+
+class TestUtils(unittest.TestCase):
+    def test_sum_metric(self):
+        pass
+        # {{{TODO}}}
+
+    def test_average_metric(self):
+        pass
+        # {{{TODO}}}
