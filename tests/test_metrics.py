de3a8c8734f87b483a676500b05f73ce9e320a0f
Eric Smith <ems@fb.com>
2020-01-08 12:13:17 -0500

    Frameworks

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -10,10 +10,28 @@ import unittest
 
 
 class TestUtils(unittest.TestCase):
-    def test_sum_metric(self):
-        pass
-        # {{{TODO}}}
+    def test_sum_metric_inputs(self):
+        for input, output in passing_inputs_and_outputs:
+            self.assertEqual(SumMetric(input).report(), output)
+        for input in failing_inputs:
+            self.assertRaises(AssertionError, SumMetric(input))
 
-    def test_average_metric(self):
-        pass
-        # {{{TODO}}}
+    def test_sum_metric_additions(self):
+        for input1, input2, output in input_pairs_and_outputs:
+            self.assertEqual((SumMetric(input1) + SumMetric(input2)).report(), output)
+
+    def test_average_metric_inputs(self):
+        for input, output in passing_inputs_and_outputs:
+            self.assertEqual(AverageMetric(input[0], input[1]).report(), output)
+        for input in failing_inputs:
+            self.assertRaises(AssertionError, AverageMetric(input[0], input[1]))
+
+    def test_average_metric_additions(self):
+        for input1, input2, output in input_pairs_and_outputs:
+            self.assertEqual(
+                (
+                    AverageMetric(input1[0], input1[1])
+                    + AverageMetric(input2[0], input2[1])
+                ).report(),
+                output,
+            )
