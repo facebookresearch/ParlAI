084d96bc2deaaff1b06fb8c3c3da15e9b37f6ae7
Eric Smith <ems@fb.com>
2020-01-08 16:18:48 -0500

    More test cases

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -30,6 +30,13 @@ class TestUtils(unittest.TestCase):
             self.assertRaises(AssertionError, SumMetric(input_))
 
     def test_sum_metric_additions(self):
+
+        input_pairs_and_outputs = [
+            (1, 2, 3.0),
+            (3, -5.0, -2.0),
+            (torch.HalfTensor([[[4.2]]]), 3, 7.2),
+            (torch.DoubleTensor([1]), torch.LongTensor([[-1]]), 0.0),
+        ]
         for input1, input2, output in input_pairs_and_outputs:
             actual_output = (SumMetric(input1) + SumMetric(input2)).report()
             self.assertEqual(actual_output, output)
@@ -37,15 +44,36 @@ class TestUtils(unittest.TestCase):
 
     def test_average_metric_inputs(self):
 
+        passing_inputs_and_outputs = [
+            ((2, 4), 0.5),
+            ((17.0, 10), 1.7),
+            ((torch.Tensor([2.3]), torch.LongTensor([2])), 1.15),
+            ((torch.Tensor([2.3]), torch.Tensor([2])), 1.15),
+        ]
         for input, output in passing_inputs_and_outputs:
             actual_output = AverageMetric(input[0], input[1]).report()
             self.assertEqual(actual_output, output)
             self.assertIsInstance(actual_output, float)
 
+        failing_inputs = [
+            (2, '4'),
+            (2, 4.0),  # Can't pass in floats as the denominator
+            (torch.Tensor([1, 1]), torch.Tensor([2])),
+            (torch.Tensor([3.2]), torch.Tensor([4.0])),
+        ]
         for input in failing_inputs:
             self.assertRaises(AssertionError, AverageMetric(input[0], input[1]))
 
     def test_average_metric_additions(self):
+
+        input_pairs_and_outputs = [
+            ((2, 4), (1.5, 1), 0.7),
+            (
+                (torch.FloatTensor([[[2]]]), torch.Tensor([4])),
+                (torch.LongTensor(1.5), 1),
+                0.7,
+            ),
+        ]
         for input1, input2, output in input_pairs_and_outputs:
             actual_output = (
                 AverageMetric(input1[0], input1[1])
