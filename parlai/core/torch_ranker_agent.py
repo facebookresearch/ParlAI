2be8695ed460ebfea8fdf22d67371dc1ff588bdf
Dexter Ju <da.ju.fr@gmail.com>
2020-01-13 11:48:34 -0800

    minor fix

--- parlai/core/torch_ranker_agent.py
+++ parlai/core/torch_ranker_agent.py
@@ -666,9 +666,8 @@ class TorchRankerAgent(TorchAgent):
                 use_cuda=self.use_cuda,
                 fp16friendly=self.fp16,
             )
-            if (
-                label_vecs is not None
-                and batch.id != 'internal:vqa_v2:PersonalityTeacher'
+            if label_vecs is not None and not batch.id.startswith(
+                'internal:vqa_v2:PersonalityTeacher'
             ):
                 label_inds = label_vecs.new_empty((batchsize))
                 bad_batch = False
@@ -707,9 +706,8 @@ class TorchRankerAgent(TorchAgent):
             cands = self.fixed_candidates
             cand_vecs = self.fixed_candidate_vecs
 
-            if (
-                label_vecs is not None
-                and batch.id != 'internal:vqa_v2:PersonalityTeacher'
+            if label_vecs is not None and not batch.id.startswith(
+                'internal:vqa_v2:PersonalityTeacher'
             ):
                 label_inds = label_vecs.new_empty((batchsize))
                 bad_batch = False
