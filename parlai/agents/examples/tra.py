b2abfd2a2c78dd20959490c81baaed60842299b6
Dexter Ju <da.ju.fr@gmail.com>
2020-01-13 12:08:31 -0800

    update comments

--- parlai/agents/examples/tra.py
+++ parlai/agents/examples/tra.py
@@ -25,36 +25,26 @@ class ExampleBagOfWordsModel(nn.Module):
         self.dict = dictionary
         self.encoder = nn.EmbeddingBag(len(self.dict), self.hidden_dim)
 
-    def forward_candidates(self, cand_vecs):
+    def encode_text(self, text_vecs):
         """
-        This function will encode all the candidates for a batch. It can be different cases:
-        1. inline candidate: we have inline candidates provided for each sample, it usually happens during eval / test time
-        In this case, the cand_vecs is a 3D tensor, [btz, num_candidates, seq_length]
-        2. batch candidates, it usually happens during training, cand_vecs will be [btz, seq_length]
-        3. fixed, when you have a fixed set of candidates to use, cand_vecs will be [num_candidates, seq_length]
+        This function encodes a text_vec to a text encoding. 
         """
-        return self.encoder.forward(cand_vecs)
-
-    def forward_context(self, batch):
-        """
-        This function takes in a Batch object and will encode the context.
-        """
-        return self.encoder(batch.text_vec)
+        return self.encoder(text_vecs)
 
     def forward(self, batch, cand_vecs, cand_encs=None):
-        btz = cand_vecs.size(0)
+        bsz = cand_vecs.size(0)
         if cand_encs is None:
             if cand_vecs.dim() == 3:
-                # if dim = 3, btz *  num_candidates * seq_length
+                # if dim = 3, bsz *  num_candidates * seq_length
                 # In this case, we are using inline candidates
                 cand_vecs = cand_vecs.reshape(-1, cand_vecs.size(2))
-            cand_encs = self.forward_candidates(cand_vecs)
-            if cand_encs.size(0) != btz:
+            cand_encs = self.encode_text(cand_vecs)
+            if cand_encs.size(0) != bsz:
                 # Some cases, we could also use batch candidates,
-                # they will be size of btz * seq_length
+                # they will be size of bsz * seq_length
                 # so we don't have to use the extra treatment
-                cand_encs = cand_encs.reshape(btz, -1, self.hidden_dim)
-        context_encodings = self.forward_context(batch)
+                cand_encs = cand_encs.reshape(bsz, -1, self.hidden_dim)
+        context_encodings = self.encode_text(batch.text_vec)
         if context_encodings.dim() != cand_encs.dim():
             return torch.sum(
                 context_encodings.unsqueeze(1).expand_as(cand_encs) * cand_encs, 2
@@ -64,11 +54,11 @@ class ExampleBagOfWordsModel(nn.Module):
 
 class TraAgent(TorchRankerAgent):
     """
-    This subclass the torch ranker agent.
-    What you need to do:
-    1. add the cmd flags you need.
-    2. Implements the build_model function
-    3. Implements the score candidates function
+    Example subclass of TorchRankerAgent.
+
+    This particular implementation is a simple bag-of-words model, which
+    demonstrates the minimum implementation requirements to make a
+    new ranking model.
     """
 
     @classmethod
@@ -97,5 +87,4 @@ class TraAgent(TorchRankerAgent):
         This function is required to build the model and assign to the
         object `self.model`.
         """
-        self.model = ExampleBagOfWordsModel(self.opt, self.dict)
-        return self.model
+        return ExampleBagOfWordsModel(self.opt, self.dict)
