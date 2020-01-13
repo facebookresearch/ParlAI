812eb88e7bc9269164f12d4701e63bf85792c80c
Stephen Roller <roller@fb.com>
2020-01-13 12:53:32 -0500

    Fix bugs with new actions setup. (#2331)
    * Fix bugs with new actions setup.

--- parlai/utils/flake8.py
+++ parlai/utils/flake8.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 """
 Custom flake8 rules for ParlAI.
 
@@ -47,7 +51,7 @@ class ParlAIChecker:
                 '',
             )
 
-        # check copyrighting next
+        # check doc formatting
         source = "".join(self.lines)
         formatted_source = docformatter.format_code(
             source,
@@ -76,7 +80,7 @@ class ParlAIChecker:
                     # that -1 says the first line changed, and 3 lines were removed
                     # with a new offset belonging at the first line, and 9
                     # inserted lines.
-                    line_no, _ = fields[1].split(',')
+                    line_no, *_ = fields[1].split(',')
                     line_no = -int(line_no)
                     yield (
                         line_no,
@@ -86,6 +90,8 @@ class ParlAIChecker:
                     )
 
         # the rest is checking copyright, but there are some exceptions
+        # copyright must appear in the first 16 lines of the file.
+        source = "".join(self.lines[:16])
         if any(wl in source for wl in WHITELIST_PHRASES):
             return
 
