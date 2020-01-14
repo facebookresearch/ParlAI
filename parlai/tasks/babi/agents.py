6bbe7f01e4d163b994c92fabc2ce047fbab68bcc
Eric Smith <ems@devfair0229.h2.fair>
2020-01-14 06:29:50 -0800

    bAbI agent fix

--- parlai/tasks/babi/agents.py
+++ parlai/tasks/babi/agents.py
@@ -44,10 +44,12 @@ def mod_labels(ys, task):
 # Single bAbI task (1k training).
 class Task1kTeacher(FbDialogTeacher):
     def __init__(self, opt, shared=None):
-        task = opt.get('task', 'babi:Task1k:1')
-        self.task_num = task.split(':')[2]
+        default = '1'
+        task = opt.get('task', f'babi:Task1k:{default}')
+        self.task_num = task.split(':')[2] if len(task.split(':')) >= 3 else default
+        # Default to self.task_num == '1' if not specified
         opt['datafile'] = _path('', self.task_num, opt)
-        opt['cands_datafile'] = _path('', task.split(':')[2], opt, 'train')
+        opt['cands_datafile'] = _path('', self.task_num, opt, 'train')
         super().__init__(opt, shared)
 
     def setup_data(self, path):
@@ -62,10 +64,12 @@ class Task1kTeacher(FbDialogTeacher):
 # Single bAbI task (10k training).
 class Task10kTeacher(FbDialogTeacher):
     def __init__(self, opt, shared=None):
-        task = opt.get('task', 'babi:Task10k:1')
-        self.task_num = task.split(':')[2]
+        default = '1'
+        task = opt.get('task', f'babi:Task10k:{default}')
+        self.task_num = task.split(':')[2] if len(task.split(':')) >= 3 else default
+        # Default to self.task_num == '1' if not specified
         opt['datafile'] = _path('-10k', self.task_num, opt)
-        opt['cands_datafile'] = _path('-10k', task.split(':')[2], opt, 'train')
+        opt['cands_datafile'] = _path('-10k', self.task_num, opt, 'train')
         super().__init__(opt, shared)
 
     def setup_data(self, path):
