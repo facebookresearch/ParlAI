13023bffdf5c8a9c38663138d194322191caa254
Stephen Roller <roller@fb.com>
2020-01-16 13:16:15 -0800

    lol

--- parlai/utils/testing.py
+++ parlai/utils/testing.py
@@ -97,7 +97,7 @@ def skipUnlessTorch14(testfn, reason='Test requires pytorch 1.4+'):
     if not TORCH_AVAILABLE:
         skip = True
     else:
-        version = torch.__version__.replace("+cpu").split('.')  # type: ignore
+        version = torch.__version__.replace('+cpu', '').split('.')  # type: ignore
         version_ = tuple(int(x) for x in version)  # type: ignore
         if version_ < (1, 4, 0):
             skip = True
