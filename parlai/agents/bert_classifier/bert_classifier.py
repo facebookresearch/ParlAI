237d3b7a5e79e730e44ab01c0f5b17bee969c753
Stephen Roller <roller@fb.com>
2020-01-18 08:11:21 -0800

    yet another bert boi.

--- parlai/agents/bert_classifier/bert_classifier.py
+++ parlai/agents/bert_classifier/bert_classifier.py
@@ -25,13 +25,10 @@ import torch
 
 try:
     from pytorch_pretrained_bert import BertModel
+
+    BERT_AVAILABLE = True
 except ImportError:
-    raise Exception(
-        (
-            "BERT rankers needs pytorch-pretrained-BERT installed. \n "
-            "pip install pytorch-pretrained-bert"
-        )
-    )
+    BERT_AVAILABLE = False
 
 
 class BertClassifierHistory(History):
@@ -146,6 +143,11 @@ class BertClassifierAgent(TorchClassifierAgent):
         Construct the model.
         """
         num_classes = len(self.class_list)
+        if not BERT_AVAILABLE:
+            raise ImportError(
+                "BERT rankers needs pytorch-pretrained-BERT installed. \n "
+                "pip install pytorch-pretrained-bert"
+            )
         return BertWrapper(BertModel.from_pretrained(self.pretrained_path), num_classes)
 
     def init_optim(self, params, optim_states=None, saved_optim_type=None):
