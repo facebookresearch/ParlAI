8d85a334bcc1f744882979dea6c614c7a628dc32
Eric Smith <ems@fb.com>
2020-01-27 17:18:41 -0500

    Other build files

---
+++ parlai/zoo/blended_skill_talk/ed_single_task.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+"""
+Pretrained polyencoder retrieval model on the ConvAI2 dialogue task.
+"""
+
+from parlai.core.build_data import download_models
+
+
+def download(datapath):
+    opt = {'datapath': datapath}
+    version = ['v1.0']
+    fnames = [f'ed_single_task_{version}.tar.gz']
+    download_models(
+        opt=opt,
+        fnames=fnames,
+        model_folder='blended_skill_talk',
+        version=version,
+        path='http://localhost:8000/',
+    )
+    # TODO: remove `path` arg
