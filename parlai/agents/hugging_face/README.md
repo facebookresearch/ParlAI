0456d2ae7964e6914275633ef422499fb6592d9c
Emily Dinan <edinan@fb.com>
2020-01-31 16:35:57 -0500

    Hugging Face Integration: GPT2 (#2364)
    * initial implementation
    
    * fixed
    
    * linty lint lint
    
    * add additional model sizes
    
    * fix special tokens and finetuning
    
    * fixed!!
    
    * dummy batch
    
    * bsz
    
    * add option for not adding special tokens, so we can eval gpt2 as is
    
    * some fixes, adding more documentation
    
    * change to torch index select
    
    * "bump cache"
    
    * fffff

---
+++ parlai/agents/hugging_face/README.md
@@ -0,0 +1,7 @@
+# Hugging Face
+
+We offer wrappers for generative transformers from [Hugging Face's transformers repository](https://github.com/huggingface/transformers) for fine-tuning and evaluating in ParlAI
+
+## Agent Variations
+
+- ``hugging_face/gpt2`` uses the Hugging Face GPT2 implementation.
--- parlai/agents/transformer/crossencoder.py
+++ parlai/agents/transformer/crossencoder.py
@@ -8,7 +8,7 @@ from .modules import TransformerEncoder
 from .modules import get_n_positions_from_options
 from parlai.core.torch_ranker_agent import TorchRankerAgent
 from .transformer import TransformerRankerAgent
-from parlai.agents.bert_ranker.cross_encoder_ranker import concat_without_padding
+from parlai.utils.torch import concat_without_padding
 import torch
 
 
