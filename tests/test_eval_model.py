0678996c59aef1c3a850acd2193b5c83ffa28579
Stephen Roller <roller@fb.com>
2020-02-06 12:23:00 -0500

    Bring back code coverage. (#2389)

--- tests/test_eval_model.py
+++ tests/test_eval_model.py
@@ -3,7 +3,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from examples.eval_model import setup_args
+from parlai.scripts.eval_model import setup_args
 
 import unittest
 import parlai.utils.testing as testing_utils
