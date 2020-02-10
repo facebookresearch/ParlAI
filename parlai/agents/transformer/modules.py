7b23b67e1b76e11550d87162dc07fe9de302fcca
Dexter Ju <da.ju.fr@gmail.com>
2020-02-10 08:20:26 -0800

    fix decoder typo bug

--- parlai/agents/transformer/modules.py
+++ parlai/agents/transformer/modules.py
@@ -99,7 +99,7 @@ def _build_decoder(
     return TransformerDecoder(
         n_heads=opt['n_heads'],
         n_layers=(
-            opt['n_encoder_layers'] if opt['n_encoder_layers'] > 0 else opt['n_layers']
+            opt['n_decoder_layers'] if opt['n_decoder_layers'] > 0 else opt['n_layers']
         ),
         embedding_size=opt['embedding_size'],
         ffn_size=opt['ffn_size'],
