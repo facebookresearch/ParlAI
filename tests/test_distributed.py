aa30be4c7a228650e0e2548b9a68bf85cee9ef48
Mary Williamson <51925253+mwillwork@users.noreply.github.com>
2020-02-26 15:07:30 -0500

    Agent metrics mw (#2433)
    * Sorting out the original agent metrics.
    
    * Rename.
    
    * Initial implmentation of record_local_metric.
    
    * Convert ppl/token_acc/nll_loss to Metric.
    
    * Introduce another couple helper Metrics.
    
    * Upgrade TRA.
    
    * Partial upgrade of TCA.
    
    * Handle report aggregation well, better logging.
    
    * Distributed and better logging.
    
    * Lint.
    
    * Update tests.
    
    * Fix some tests.
    
    * Another test fixed.
    
    * Last one maybe [long]
    
    * Get fairseq/nltk tokenized blue running.
    
    * Fix older tests.
    
    * Remove debug.
    
    * mypy.
    
    * Move on if no fairseq/nltk.
    
    * Updating self-feeding.
    
    * Fun.
    
    * Fixed it. Wow.
    
    * Don't block forever.
    
    * Broken tests.
    
    * Fix some mypy items.
    
    * Revert CI/test changes.
    
    * Also sync the agent global metrics.
    
    * Add a general way of temporarily disabling local metrics.
    
    * And actually implement the skip.
    
    * Temporarily destroy CI.
    
    * Fix hogwild race condition, probably break everything else.
    
    * yup.
    
    * Fix broken cases.
    
    * Lol.
    
    * whoo.
    
    * Restore circleci.
    
    * Fix updates in hogwild, add tokens_per_batch.
    
    * Bug fixes. Bring back one-per-worker eval.
    
    * Whatever, drop this test.
    
    * Typo. Eliminate use_batch_act.
    
    * Fine.
    
    * Fix quickstart, nice catch.
    
    * Lint fix.
    
    * Added functionality such that eval can be done on all the workers in distributed mode instead of solely on the primary worker. Works for non-streaming currently.
    
    * fix hardcoding of examples for testing. commented out streaming test
    
    * blacking
    
    * fixed logic for max exs and added tests for StreamDialogData
    
    * committing repro for the moment of pytorch bug in distributed
    
    * implementation has an off by 3 bug
    
    * feature now works for both DialogData and StreamDialogData and all tests pass for distributed.
    
    * cleanup of a couple unneeded variables i had added in teachers.py
    
    * fix test transformers and blacking
    
    * linting on test_transformers
    
    * linting still failing even though I blacked it locally. fixed lines manually.
    
    * fixes as per PR comments
    
    Co-authored-by: Stephen Roller <roller@fb.com>

--- tests/test_distributed.py
+++ tests/test_distributed.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import os
+import copy
 import unittest
 import torch.distributed as dist
 import parlai.utils.testing as testing_utils
@@ -26,6 +27,30 @@ def _forced_parse(parser, opt):
 
 @testing_utils.skipUnlessGPU
 class TestDistributed(unittest.TestCase):
+    _base_config = dict(
+        task='integration_tests:nocandidate',
+        model='transformer/generator',
+        optimizer='adamax',
+        learningrate=7e-3,
+        batchsize=7,
+        validation_every_n_epochs=5,
+        num_epochs=20,
+        n_layers=1,
+        n_heads=1,
+        ffn_size=32,
+        embedding_size=32,
+        beam_size=1,
+        verbose=True,
+    )
+
+    def setUp(self):
+        print(f'[Setting up test {self._testMethodName}]')
+
+    def tearDown(self):
+        # we need to de-initialize the distributed world, otherwise other
+        # tests will they're we're distributed when we're really not.
+        dist.destroy_process_group()
+
     def _distributed_train_model(self, opt):
         with testing_utils.tempdir() as tmpdir:
             if 'model_file' not in opt:
@@ -44,33 +69,68 @@ class TestDistributed(unittest.TestCase):
 
         return (valid, test)
 
-    def tearDown(self):
-        # we need to de-initialize the distributed world, otherwise other
-        # tests will they're we're distributed when we're really not.
-        dist.destroy_process_group()
-
     def test_generator_distributed(self):
-        valid, test = self._distributed_train_model(
-            dict(
-                task='integration_tests:nocandidate',
-                model='transformer/generator',
-                optimizer='adamax',
-                learningrate=7e-3,
-                batchsize=32,
-                validation_every_n_epochs=5,
-                num_epochs=20,
-                n_layers=1,
-                n_heads=1,
-                ffn_size=32,
-                embedding_size=32,
-                beam_size=1,
-            )
-        )
+        valid, test = self._distributed_train_model(self._base_config)
 
         self.assertLessEqual(valid['ppl'], 1.20)
         self.assertGreaterEqual(valid['bleu-4'], 0.95)
         self.assertLessEqual(test['ppl'], 1.20)
         self.assertGreaterEqual(test['bleu-4'], 0.95)
+
+        # Tests that DialogData.get() is doing the right thing
+        # Ensure no duplication of examples among workers
+        # It would be 200 if each worker did all the examples
+        self.assertEqual(valid['exs'].value(), 100)
+        self.assertEqual(test['exs'].value(), 100)
+
+    def test_distributed_eval_max_exs(self):
+        config = copy.deepcopy(self._base_config)
+        config['validation_max_exs'] = 90
+        config['short_final_eval'] = True
+        valid, test = self._distributed_train_model(config)
+
+        # Tests that DialogData.get() is doing the right thing
+        # Ensure no duplication of examples among workers
+        # It would be 200 if each worker did all the examples
+        # Note: we decided that it was OK for the total count to be slightly off
+        # when using validation_max_exs and distributed.
+        # It's > 90 b/c there are two workers, told to do 45 each, & BatchWorld
+        # parley() does batchsize examples each time, so each worker will do 49
+        # example. In the future, if we fix VME, this assert should be changed
+        # to exactly 90.
+        self.assertEqual(valid['exs'].value(), 98)
+        self.assertEqual(test['exs'].value(), 98)
+
+    def test_distributed_eval_stream_mode(self):
+        config = copy.deepcopy(self._base_config)
+        config['datatype'] = 'train:stream'
+        valid, test = self._distributed_train_model(config)
+
+        # Tests that StreamDialogData.get() is doing the right thing
+        # Ensure no duplication of examples among workers
+        # It would be 200 if each worker did all the examples
+        self.assertEqual(valid['exs'].value(), 100)
+        self.assertEqual(test['exs'].value(), 100)
+
+    def test_distributed_eval_stream_mode_max_exs(self):
+        config = copy.deepcopy(self._base_config)
+        config['datatype'] = 'train:stream'
+        config['validation_max_exs'] = 90
+        config['short_final_eval'] = True
+
+        valid, test = self._distributed_train_model(config)
+
+        # Tests that StreamDialogData.get() is doing the right thing
+        # Ensure no duplication of examples among workers
+        # It would be 200 if each worker did all the examples
+        # As in the test above:
+        # It does 98 instead of 90 b/c there are two workers, told to do 45
+        # each, and BatchWorld parley() does batchsize examples each time, so
+        # each worker will do 49 examples.
+        # In the future, if we fix VME, this assert should be changed to
+        # exactly 90.
+        self.assertEqual(valid['exs'].value(), 98)
+        self.assertEqual(test['exs'].value(), 98)
 
 
 if __name__ == '__main__':
