1fa6d31006d03eb0a7c1c7cf14c3f542477d9f07
klshuster <kshuster@fb.com>
2020-03-03 11:13:09 -0800

    change len batch reply

--- parlai/core/torch_agent.py
+++ parlai/core/torch_agent.py
@@ -1766,9 +1766,12 @@ class TorchAgent(ABC, Agent):
         self._local_metrics.clear()
 
         # initialize a list of replies with this agent's id
+        # in background prep, `batch_size` is the number of valid obs, whereas
+        # in normal training, `batch_size` is the number of total obs
+        reply_length = max(max(batch.valid_indices) + 1, batch_size)
         batch_reply = [
             Message({'id': self.getID(), 'episode_done': False})
-            for _ in range(max(batch.valid_indices) + 1)
+            for _ in range(reply_length)
         ]
 
         if batch_size == 0:
