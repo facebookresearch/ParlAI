b80528a853f14780f527a00a6c83d1c955535dc4
Stephen Roller <roller@fb.com>
2020-03-04 07:02:39 -0800

    lol.

--- parlai/core/torch_ranker_agent.py
+++ parlai/core/torch_ranker_agent.py
@@ -25,6 +25,7 @@ from parlai.utils.distributed import is_distributed
 from parlai.core.torch_agent import TorchAgent, Output
 from parlai.utils.misc import warn_once
 from parlai.utils.torch import padded_3d
+from parlai.utils.fp16 import FP16SafeCrossEntropy
 from parlai.core.metrics import AverageMetric
 
 
