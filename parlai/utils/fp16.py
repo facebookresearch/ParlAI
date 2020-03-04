11d3635a41b2c97c913046332b1dd7abbd632851
Stephen Roller <roller@fb.com>
2020-03-04 07:49:36 -0800

    Whoops.

--- parlai/utils/fp16.py
+++ parlai/utils/fp16.py
@@ -30,16 +30,16 @@ class FP16SafeCrossEntropy(torch.nn.Module):
     This avoids overflow in the softmax by doing the operation in FP32.
     """
 
-    def __init__(self, ignore_index, reduction='none'):
+    def __init__(self, ignore_index=None, reduction='none'):
         super().__init__()
-        self.NULL_IDX = ignore_index
+        self.ignore_index = ignore_index
         self.reduction = reduction
 
     def forward(self, scores, targets):
         return F.nll_loss(
             F.log_softmax(scores, 1, dtype=torch.float32),
             targets,
-            ignore_index=self.NULL_IDX,
+            ignore_index=self.ignore_index,
             reduction=self.reduction,
         )
 
