783ef63fa2a824e6e778a843d3099d295370e769
JING XU <jingxu23@fb.com>
2020-03-19 20:27:29 -0400

    Add ANLI dataset to ParlAI
    Sign-off-by: Jing Xu <jingxu23@fb.com>

---
+++ parlai/tasks/anli/agents.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+import copy
+import json
+
+from parlai.core.teachers import DialogTeacher, MultiTaskTeacher
+from .build import build
+
+from .anli_constants import ANLI, ANLI_VERSION, ANLI_ANSWER_KEY, ANLI_LABEL_DICT, ANLI_HYPO_KEY, \
+    ANLI_HYPO_PREFIX, ANLI_LABELS, ANLI_PREFIX, ANLI_PREMISE_KEY, ANLI_PREMISE_PREFIX, ANLI_ROUNDS
+
+
+def _path(opt):
+    build(opt)
+
+    dt = opt['datatype'].split(':')[0]
+
+    if dt == 'train':
+        suffix = 'train'
+    elif dt == 'valid':
+        suffix = 'dev'
+    elif dt == 'test':
+        suffix = 'test'
+    else:
+        raise RuntimeError('Not valid datatype.')
+
+    data_path = os.path.join(
+        opt['datapath'],
+        ANLI,
+        ANLI_PREFIX + ANLI_VERSION,
+        opt['round'],
+        suffix + '.jsonl',
+    )
+
+    return data_path
+
+
+def setup_data(path):
+    print('loading: ' + path)
+    with open(path, 'r') as data_file:
+        for pair_line in data_file:
+            pair = json.loads(pair_line)
+            premise = ANLI_PREMISE_PREFIX + pair[ANLI_PREMISE_KEY]
+            hypo = ANLI_HYPO_PREFIX + pair[ANLI_HYPO_KEY]
+            answer = [ANLI_LABEL_DICT[pair[ANLI_ANSWER_KEY]]]
+
+            if answer == ['-']:
+                continue
+
+            question = premise + '\n' + hypo
+
+            yield (question, answer, None, ANLI_LABELS), True
+
+
+class RoundBaseTeacher(DialogTeacher):
+    def __init__(self, opt, shared=None):
+        opt = copy.deepcopy(opt)
+        opt['round'] = opt['task'].split(':')[1] if len(opt['task'].split(':')) > 1 else opt['task']
+        opt['round'] = opt['round'].upper()
+
+        assert opt['round'] in ANLI_ROUNDS, print('Undefined task round: ', opt['round'])
+
+        data_path = _path(opt)
+        opt['datafile'] = data_path
+        self.id = opt['task'].upper()
+        self.round = opt['round']
+        super().__init__(opt, shared)
+
+    def label_candidates(self):
+        return ANLI_LABELS
+
+    def setup_data(self, path):
+        return setup_data(path)
+
+
+class R1Teacher(RoundBaseTeacher):
+    pass
+
+
+class R2Teacher(RoundBaseTeacher):
+    pass
+
+
+class R3Teacher(RoundBaseTeacher):
+    pass
+
+
+class DefaultTeacher(MultiTaskTeacher):
+    def __init__(self, opt, shared=None):
+        anli_tasks = [
+            'anli:r1',
+            'anli:r2',
+            'anli:r3',
+        ]
+        opt = copy.deepcopy(opt)
+        opt['task'] = ', '.join(anli_tasks)
+        super().__init__(opt, shared)
---
+++ parlai/tasks/anli/anli_constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+ANLI = 'ANLI'
+ANLI_VERSION = 'v0.1'
+ANLI_PREFIX = 'anli_'
+ANLI_PREMISE_PREFIX = 'Premise: '
+ANLI_HYPO_PREFIX = 'Hypothesis: '
+ANLI_LABEL_DICT = {'e': 'entailment', 'c': 'contradiction', 'n': 'neutral'}
+ANLI_LABELS = list(ANLI_LABEL_DICT.values())
+ANLI_PREMISE_KEY = 'context'
+ANLI_HYPO_KEY = 'hypothesis'
+ANLI_ANSWER_KEY = 'label'
+ANLI_ROUNDS = ['R1', 'R2', 'R3']
