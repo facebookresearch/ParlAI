1112dc8632a5aa044af04ff14b781de4d255c401
Stephen Roller <roller@fb.com>
2020-03-20 06:56:15 -0700

    Test model parallel.

---
+++ tests/test_model_parallel.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+import parlai.utils.testing as testing_utils
+
+MODEL_OPTS = {
+    'task': 'integration_tests',
+    'n_layers': 4,
+    'embedding_size': 16,
+    'ffn_size': 32,
+    'n_heads': 2,
+    'num_epochs': 0.1,
+    'batchsize': 32,
+    'truncate': 8,
+    'model_parallel': True,
+}
+
+
+@testing_utils.skipUnlessGPU
+class TestModelParallel(unittest.TestCase):
+    def test_ranker(self):
+        testing_utils.train_model(
+            {'model': 'transformer/ranker', 'candidates': 'batch', **MODEL_OPTS}
+        )
+
+        with self.assertRaises(RuntimeError):
+            testing_utils.train_model(
+                {
+                    'model': 'transformer/ranker',
+                    'data_parallel': True,
+                    'candidates': 'batch',
+                    **MODEL_OPTS,
+                }
+            )
+
+    def test_classifier(self):
+        testing_utils.train_model(
+            {
+                'classes': ['one', 'zero'],
+                'model': 'transformer/classifier',
+                'data_parallel': True,
+                **MODEL_OPTS,
+            }
+        )
+        with self.assertRaises(RuntimeError):
+            testing_utils.train_model(
+                {
+                    'classes': ['one', 'zero'],
+                    'model': 'transformer/classifier',
+                    'data_parallel': True,
+                    **MODEL_OPTS,
+                }
+            )
+
+    def test_transformer_generator(self):
+        testing_utils.train_model({'model': 'transformer/generator', **MODEL_OPTS})
