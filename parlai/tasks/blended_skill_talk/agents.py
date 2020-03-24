f3f12cc06c24d5198a544db14d2fffd5b4f402c9
Eric Smith <ems@fb.com>
2020-03-24 10:36:55 -0400

    Add minimal BST teacher

---
+++ parlai/tasks/blended_skill_talk/agents.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import copy
+import os
+
+from parlai.core.teachers import ParlAIDialogTeacher
+from parlai.tasks.blended_skill_talk.build import build
+
+
+def _path(opt):
+    # Build the data if it doesn't exist.
+    build(opt)
+    dt = opt['datatype'].split(':')[0]
+    return os.path.join(opt['datapath'], 'blended_skill_talk', dt + '.txt')
+
+
+class BlendedSkillTalkTeacher(ParlAIDialogTeacher):
+    def __init__(self, opt, shared=None):
+        opt = copy.deepcopy(opt)
+        opt['parlaidialogteacher_datafile'] = _path(opt)
+        super().__init__(opt, shared)
+
+
+class DefaultTeacher(BlendedSkillTalkTeacher):
+    pass
