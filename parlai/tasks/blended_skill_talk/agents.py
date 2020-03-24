1d79afbcf8c58d8454f8cee9b0a8df5594d66303
Eric Smith <ems@fb.com>
2020-03-24 12:29:17 -0400

    Add PT teacher

--- parlai/tasks/blended_skill_talk/agents.py
+++ parlai/tasks/blended_skill_talk/agents.py
@@ -7,24 +7,24 @@
 import copy
 import json
 import os
-from typing import List, Optional
+import re
+from collections import defaultdict
+from typing import List, Optional, Dict
 
 from parlai.core.teachers import ParlAIDialogTeacher, create_task_agent_from_taskname
-from parlai.tasks.convai2.agents import DefaultTeacher as Convai2DefaultTeacher
-from parlai.tasks.empathetic_dialogues.agents import EmpatheticDialoguesTeacher
-from parlai.tasks.wizard_of_wikipedia.agents import (
-    WizardDialogKnowledgeTeacher,
-    BasicdialogTeacher,
-)
-from parlai_internal.projects.blended_skill_talk.add_personas_topics import (
-    PersonaTopicifier,
-)
 from parlai.tasks.blended_skill_talk.dataset_classification_teacher import (
     AbstractDatasetClassificationTeacher,
 )
 from parlai.tasks.blended_skill_talk.mixed_candidates_teacher import (
     AbstractMixedCandidatesTeacher,
 )
+from parlai.tasks.convai2.agents import DefaultTeacher as Convai2DefaultTeacher
+from parlai.tasks.empathetic_dialogues.agents import EmpatheticDialoguesTeacher
+from parlai.tasks.wizard_of_wikipedia.agents import (
+    WizardDialogKnowledgeTeacher,
+    BasicdialogTeacher,
+)
+from parlai.utils.misc import warn_once
 from .build import build
 
 
@@ -201,6 +201,11 @@ def create_agents(opt):
         return []
 
 
+################################################################################
+## Teachers for adding ConvAI2 personas and WoW topics to existing datasets   ##
+################################################################################
+
+
 class ConvAI2PersonaTopicifierTeacher(Convai2DefaultTeacher):
     """
     Adds WoW topics to ConvAI2 data.
@@ -344,3 +349,180 @@ class EDPersonaTopicifierTeacher(EmpatheticDialoguesTeacher):
         if entry_idx is None:
             entry_idx = 0
         return self.persona_topic_data[episode_idx][entry_idx]
+
+
+class PersonaTopicifier:
+    def __init__(
+        self,
+        should_have_personas=False,
+        should_have_topics=False,
+        no_persona_is_error: bool = False,
+    ):
+        print('IN PERSONA TOPICIFIER INIT')
+        self.utterance_to_persona_map = {}
+        self.should_have_personas = should_have_personas
+        self.should_have_topics = should_have_topics
+        self.no_persona_is_error = no_persona_is_error
+        # Throw an exception if a persona is not found for the input WoW topic
+
+        # this returns map of persona line str to WoW topic
+        (
+            self.wow_topics_to_persona_strings_map,
+            self.persona_strings_to_wow_topics_map,
+        ) = self._setup_personas_to_wow_topics()
+        self.personas_file_path = '/checkpoint/marywilliamson/all_in_one_dialogue/context_personas_self_original_train_20191203'
+        with open(self.personas_file_path, 'r') as f:
+            self.personas = f.read().strip().split('||')
+            # There's an extra line at the end of the file which is ''
+            self.personas = [p for p in self.personas if p]
+            print(f'Got {len(self.personas)} personas.')
+
+    def _setup_personas_to_wow_topics(self) -> Dict[str, List[str]]:
+        topic_to_persona_path = (
+            '/checkpoint/parlai/tasks/wizard_of_wikipedia/topic_to_persona_list.txt'
+        )
+        persona_strings_to_topics = defaultdict(list)
+        topics_to_persona_strings = defaultdict(list)
+        with open(topic_to_persona_path, 'r') as f:
+            for line in f:
+                match = re.fullmatch(r'([^[]+): (\[.+\])\n', line)
+                topic = match.group(1)
+                # if topic not in self.wow_topics_to_episode_idxes:
+                #     continue
+                persona_strings = eval(match.group(2))
+                assert isinstance(persona_strings, list)
+                topics_to_persona_strings[topic] = persona_strings
+                for str_ in persona_strings:
+                    persona_strings_to_topics[str_].append(topic)
+
+        print(
+            f'FINISHED MAPPING personas to topics, got: {len(list(persona_strings_to_topics.keys()))} persona strings to map to topics.'
+        )
+        return topics_to_persona_strings, persona_strings_to_topics
+
+    def __calculate_word_overlap(self, a, b):
+        """
+        Super stupid way of calculating.
+        """
+        score = 0
+        tokens_a = a.split(' ')
+        tokens_a = [ta for ta in tokens_a if len(ta) >= 5]
+        for ta in tokens_a:
+            if ta in b:
+                score += 1
+
+        tokens_b = b.split(' ')
+        tokens_b = [tb for tb in tokens_b if len(tb) >= 5]
+        for tb in tokens_b:
+            if tb in a:
+                score += 1
+        return score
+
+    def __choose_persona_from_text(self, utt):
+        utt = utt.strip()
+        if utt not in self.utterance_to_persona_map:
+            best_word_overlap = 0
+            best_persona = None
+            for p in self.personas:
+                word_overlap = self.__calculate_word_overlap(utt, p)
+                if word_overlap >= best_word_overlap:
+                    best_word_overlap = word_overlap
+                    best_persona = p
+            if not best_persona:
+                raise Exception(
+                    f'No persona found for utterance: \"{utt}\". This should not happen.'
+                )
+            self.utterance_to_persona_map[utt] = best_persona
+            # Should have a \n at the end of it already
+            return best_persona
+        return self.utterance_to_persona_map[utt]
+
+    def __choose_persona_from_topic(self, topic):
+        topic = topic.strip()
+        persona_strings = self.wow_topics_to_persona_strings_map[topic]
+        for p in persona_strings:
+            for persona in self.personas:
+                if p in persona:
+                    return persona
+        if self.no_persona_is_error:
+            raise ValueError(f'ERROR: Found no persona for topic: {topic}.')
+        else:
+            warn_once(
+                f'Found no persona for topic: {topic}. Returning first ' f'persona.'
+            )
+            return self.personas[0]
+
+    def __choose_topic(self, persona):
+        persona_lines = persona.strip().split('\n')
+        for p in persona_lines:
+            p_str = p.replace('your persona:', '')
+            p_str = p_str.strip()
+            # print(f'Looking for \"{p_str}\" in wow topics map.')
+            if p_str in self.persona_strings_to_wow_topics_map:
+                topics = self.persona_strings_to_wow_topics_map[p_str]
+                topic = topics[0] + '\n'
+                # print(f'Found topic: {topic} for persona: \"{persona}\"')
+                return topic
+
+        for utt, topics in self.persona_strings_to_wow_topics_map.items():
+            utt_words = utt.split()
+            utt_words_long = [utt for utt in utt_words if len(utt) > 6]
+            for long_utt in utt_words_long:
+                if long_utt in persona:
+                    # print(f'Returning topic: {topics[0]} for \"{persona}\" after exact match not found.')
+                    return topics[0] + '\n'
+        warn_once(
+            f'Found no WoW topic for persona: \"{persona}\". Returning topics[0]: {topics[0]}'
+        )
+        return topics[0] + '\n'
+
+    def get_modified_text(self, text):
+        # Order should be <Persona> \n <Topic> \n <Utterance>
+        # Should be used for entry_idx == 0 only (for all first
+        # utterances only)
+
+        # Doesn't work b/c Wizard sometimes sends only the topic
+        # has_neither = 'persona:' not in text and '\n' not in text
+        # has_wow_topic_only = 'persona:' not in text and '\n' in text
+        # has_persona_only = 'persona:' in text
+
+        has_neither = not self.should_have_personas and not self.should_have_topics
+        has_wow_topic_only = not self.should_have_personas and self.should_have_topics
+        has_persona_only = not self.should_have_topics and self.should_have_personas
+
+        if (self.should_have_personas and (has_neither or has_wow_topic_only)) or (
+            self.should_have_topics and (has_neither or has_persona_only)
+        ):
+            raise Exception(
+                f'Malformed text: {text}, should_have_personas: {self.should_have_personas}, should_have_topics: {self.should_have_topics}, has_neither: {has_neither}, has_wow_topic_only: {has_wow_topic_only}, has_persona_only: {has_persona_only}'
+            )
+
+        if has_neither:
+            # Will occur with ED
+            persona = self.__choose_persona_from_text(text)
+            topic = self.__choose_topic(persona)
+            utt = text
+        elif has_wow_topic_only:
+            # Will occur with Wizard
+            parts = text.strip().split('\n')
+            if len(parts) > 1:
+                topic = parts[0] + '\n'
+                utt = parts[1]
+                persona = self.__choose_persona_from_topic(topic)
+            else:
+                # Only has a topic, no utterance
+                topic = parts[0] + '\n'
+                utt = ''
+                persona = self.__choose_persona_from_topic(topic)
+        elif has_persona_only:
+            # Will occur with Convai2
+            lines = text.strip().split('\n')
+            utt = lines[-1]
+            persona = ''.join(l + '\n' for l in lines[:-1])
+            topic = self.__choose_topic(persona)
+        else:
+            raise Exception(f'Unknown structure of utterance: {text}')
+
+        modified_utterance = persona + topic + utt
+        # print(f'Text was: \"{text}\", now is: \"{modified_utterance}\"')
+        return modified_utterance
