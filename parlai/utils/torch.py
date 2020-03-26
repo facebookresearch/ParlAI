7c3f896ca285560fbcdfae4b3db0ded461f9d7fc
Margaret Li <hadasah@gmail.com>
2020-03-26 07:55:25 -0700

    black

--- parlai/utils/torch.py
+++ parlai/utils/torch.py
@@ -246,7 +246,7 @@ def compute_grad_norm(parameters, norm_type=2.0):
     for p in parameters:
         param_norm = p.grad.data.norm(norm_type)
         total_norm += param_norm.item() ** norm_type
-    return total_norm ** (1. / norm_type)
+    return total_norm ** (1.0 / norm_type)
 
 
 class IdentityLayer(torch.nn.Module):
