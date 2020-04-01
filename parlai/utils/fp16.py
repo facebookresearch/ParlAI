fcaaf4b2131c18af7b39f52d3caaabfc8df82fa9
EricMichaelSmith <ems@fb.com>
2020-04-01 17:24:05 -0400

    Try registering buffer

--- parlai/utils/fp16.py
+++ parlai/utils/fp16.py
@@ -6,10 +6,12 @@
 """
 Utility methods for mixed precision training.
 """
-from parlai.utils.misc import warn_once
 
-from itertools import chain
 import math
+from itertools import chain
+from typing import Optional
+
+from parlai.utils.misc import warn_once
 
 try:
     import torch
@@ -30,11 +32,16 @@ class FP16SafeCrossEntropy(torch.nn.Module):
     This avoids overflow in the softmax by doing the operation in FP32.
     """
 
-    def __init__(self, weight=None, ignore_index=-100, reduction='none'):
+    def __init__(
+        self,
+        weight: Optional[torch.Tensor] = None,
+        ignore_index: int = -100,
+        reduction: str = 'none',
+    ):
         # default ignore_index=-100 mimics pytorch's default in
         # torch.nn.functional.nll_loss
         super().__init__()
-        self.weight = weight
+        self.register_buffer('weight', weight)
         self.ignore_index = ignore_index
         self.reduction = reduction
 
@@ -266,11 +273,7 @@ class MemoryEfficientFP16Optimizer(torch.optim.Optimizer):
         """
         List of compatible optimizers.
         """
-        return [
-            'adam',
-            'mem_eff_adam',
-            'adafactor',
-        ]
+        return ['adam', 'mem_eff_adam', 'adafactor']
 
     @property
     def params(self):
