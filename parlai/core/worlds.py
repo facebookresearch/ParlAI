c0517ca7297665b731bcbe8b754af53332ff497a
Dexter JU <5313281+dexterju@users.noreply.github.com>
2020-04-03 15:40:04 -0400

    Raise errors when teachers have overlapped ids (#2528)
    * Raise errors when teachers have overlapped ids
    
    * message improvement
    
    * add test for teacher.id overlapping
    
    * apply comments

--- parlai/core/worlds.py
+++ parlai/core/worlds.py
@@ -612,6 +612,20 @@ class MultiWorld(World):
                 weight = 1
             self.cum_task_weights[i] = weight + sum
             sum += weight
+        task_ids = {}
+        # Having overlap in teacher ids will cause issues for metrics aggregation.
+        for each_world in self.worlds:
+            world_id = each_world.getID()
+            if world_id in task_ids:
+                raise AssertionError(
+                    '{} and {} teachers have overlap in id {}.'.format(
+                        task_ids[world_id],
+                        each_world.get_agents()[0].__class__,
+                        world_id,
+                    )
+                )
+            else:
+                task_ids[world_id] = each_world.get_agents()[0]
 
     def num_examples(self):
         """
