390a795bd2952f048b49a1ea0059f020d3380550
domrigoglioso <drrigoglioso@gmail.com>
2020-04-26 19:57:55 -0400

    kept failing tests, trying removing data creation folder to see something

--- parlai/tasks/eli5/data_creation/slurm_scripts/eli_merge_docs.sh
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "${DIR}/.."
-pwd
-echo $NM
-echo $C
-python merge_support_docs.py -n $NM -f $C
--- parlai/tasks/eli5/data_creation/slurm_scripts/eli_merge_docs_launcher.sh
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-
-for nm in 'explainlikeimfive'
-do
-    for c in {0..9}
-    do
-        sbatch --export=ALL,NM=$nm,C=$c eli_merge_docs.sbatch
-    done
-done
