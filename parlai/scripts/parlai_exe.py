7bea52bde9344b6f64d3956796c90dee1d8aeb58
Jason Weston <jase@fb.com>
2020-05-10 15:45:36 -0400

    copyright

--- parlai/scripts/parlai_exe.py
+++ parlai/scripts/parlai_exe.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+"""
+Global parlAI command line launcher
+"""
 from parlai.utils.strings import colorize, name_to_classname
 import importlib
 import os
