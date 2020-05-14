1583bd82a141b105676e975686227f94b5f19453
Stephen Roller <roller@fb.com>
2020-05-14 00:01:18 -0400

    Update the quickstart docs.

--- tests/test_quickstart.sh
+++ tests/test_quickstart.sh
@@ -9,11 +9,11 @@ set -e -x  # error and exit on any failure; print the commands being run
 
 # view a task & train a model
 python examples/display_data.py -t babi:task10k:1
-python examples/train_model.py -t babi:task10k:1 -mf /tmp/babi_memnn -bs 1 -nt 4 -eps 5 -m memnn --no-cuda
+python examples/train_model.py -t babi:task10k:1 -mf /tmp/babi_memnn -bs 8 -eps 5 -m memnn --no-cuda
 python examples/display_model.py -t babi:task10k:1 -mf /tmp/babi_memnn -ecands vocab
 
 # train a transformer on twitter
-pip3 install emoji unidecode
+python -m pip install emoji unidecode
 python examples/display_data.py -t twitter
 python examples/train_model.py -t twitter -mf /tmp/tr_twitter -m transformer/ranker -bs 10 -vtim 3600 -cands batch -ecands batch --data-parallel True --max-train-time 20 -nl 1 --dict-tokenizer split -emb random --ffn-size 128
 python examples/eval_model.py -t twitter -bs 50 -m legacy:seq2seq:0 -mf models:convai2/seq2seq/convai2_self_seq2seq_model --num-examples 1
