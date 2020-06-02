93ef9f3614b03a860c7fb32e2719d9582f69d0d6
Margaret Li <hadasah@gmail.com>
2020-06-01 18:25:36 -0700

    Fromfile jsonl teacher

--- parlai/core/teachers.py
+++ parlai/core/teachers.py
@@ -20,6 +20,10 @@ This module provides a set of teachers that deal with dialog.
      Teacher class that provides access to data in the ParlAI Dialog format.
      See the class description for more details.
 
+     ``ConversationTeacher(DialogTeacher)``
+     Teacher class that provides access to data in the Conversations format.
+     See the class description for more details.
+
     ``FbDialogTeacher(DialogTeacher)``
      Teacher class that provides access to data in the Facebook Dialog format.
      See the class description for more details. **This class is deprecated**.
@@ -38,8 +42,9 @@ from parlai.core.loader import register_teacher  # noqa: F401
 from parlai.core.message import Message
 from parlai.core.metrics import TeacherMetrics, aggregate_named_reports
 from parlai.core.opt import Opt
-from parlai.utils.misc import AttrDict, no_lock, str_to_msg, warn_once
+from parlai.utils.conversations import Conversations
 from parlai.utils.distributed import get_rank, num_workers, is_distributed
+from parlai.utils.misc import AttrDict, no_lock, str_to_msg, warn_once
 
 from abc import ABC, abstractmethod
 
@@ -1397,6 +1402,155 @@ class ParlAIDialogTeacher(FixedDialogTeacher):
                 'is intentional, you may ignore this, but you MAY have a bug in '
                 'your data.'
             )
+
+
+class ConversationTeacher(FixedDialogTeacher):
+    """
+    This module provides access to data in the Conversations format.
+
+    Subclasses ``FixedDialogTeacher`` for functionality and provides an
+    implementation of ``setup_data()`` which iterates over datasets in the
+    "ParlAI text" format. If your data is in the format below, use this class to
+    handle file parsing for you.
+
+    The way the data is set up is as follows:
+
+    ::
+
+        text:Sam went to the kitchen. <NEWL>
+        Pat gave Sam the milk. <NEWL>
+        Where is the milk? <TAB> labels:kitchen <TAB> reward:1
+        <TAB> label_candidates:hallway|kitchen|bathroom
+        text:Sam went to the hallway. <NEWL>
+        Pat went to the bathroom. <NEWL>
+        Where is the milk? <TAB> labels:hallway <TAB> reward:1
+        <TAB> label_candidates:hallway|kitchen|bathroom <TAB> episode_done:True
+
+    Lines 1-2 represent a single episode, with a different example on each line.
+    The lines contain a query and a label for getting the question
+    correct, and three label candidates.
+
+    Since both of these examples are part of the same episode, the information
+    provided in the first example is relevant to the query in the second
+    example and therefore the agent must remember the first example in order to
+    do well.
+
+    In general dialog this format can contain any speech, not just QA pairs:
+
+    ::
+
+        text:Hi how's it going?<TAB>labels:It's going great. What's new?
+        text:Well I'm working on a new project at work.<TAB>labels:Oh me too!
+        text:Oh cool!<TAB>labels:Tell me about yours.
+
+    etc.
+
+    Note that dialogs are interpreted as being one-way. For example, consider
+    this dialog:
+
+    ::
+
+        1 X1    Y1
+        2 X2    Y2
+        3 X3    Y3
+
+    A set of examples X1 => Y1, X2 => Y2, and X3 => Y3 will be generated.
+    However, Y1 => X2 and Y2 => X3 are not created as separate examples by
+    default. This makes sense for some data (we don't need to train on the idea
+    that "kitchen" should be followed by "Sam went to the hallway..." above),
+    but for other datasets it may be helpful to add additional examples in the
+    reverse direction ("Oh cool!" is a response to "Oh me too!" above).
+    """
+
+    def __init__(self, opt, shared=None):
+        super().__init__(opt, shared)
+        if not shared:
+            self.episodes = []
+            self.num_exs = 0
+            self.label_turns = opt.get('label_turns')
+            if opt.get('conversationteacher_datafile') is not None:
+                self._setup_data(opt.get('conversationteacher_datafile'))
+        else:
+            self.episodes = shared['episodes']
+            self.num_exs = sum(len(e) for e in self.episodes)
+
+        self.id = opt['task']
+
+        self.reset()
+
+    def share(self):
+        """
+        Share the episodes.
+        """
+        shared = super().share()
+        shared['episodes'] = self.episodes
+        return shared
+
+    def num_examples(self):
+        """
+        Return the number of examples from the data.
+        """
+        return self.num_exs
+
+    def num_episodes(self):
+        """
+        Return the number of episodes from the data.
+        """
+        return len(self.episodes)
+
+    def get(self, episode_idx, entry_idx=None):
+        """
+        Get a specific example from the dataset.
+        """
+        return self.episodes[episode_idx][entry_idx]
+
+    def _setup_data(self, path):
+        print("[loading parlAI text data:" + path + "]")
+        self.episodes = []
+        self.num_exs = 0
+        eps = []
+        with open(path, newline='\n') as read:
+            for line in read:
+                episode = json.loads(line.rstrip('\n'))
+                if episode:
+                    dialogue = episode.get('dialog', [])
+                    # insert extra silence turn
+                    dialogue.insert(0, {'text': '__SILENCE__'})
+
+                    # train on odd turns as labels (turns w/ first speaker)
+                    if self.label_turns in ['firstspeaker', 'both']:
+                        eps = []
+                        xdialogue = dialogue[::2]
+                        ydialogue = dialogue[1::2]
+                        for xturn, yturn in zip(xdialogue, ydialogue):
+                            turn = {}
+                            turn['text'] = xturn.get('text').strip()
+                            turn['labels'] = [yturn.get('text').strip()]
+                            turn['episode_done'] = False
+                            label_cands = yturn.get('label_cands')
+                            eps.append(turn)
+                        if eps:
+                            eps[-1]['episode_done'] = True
+                            self.episodes.append(eps)
+                            self.num_exs += 1
+
+
+                    # train on even turns as labels (turns w/ second speaker)
+                    if self.label_turns in ['secondspeaker', 'both']:
+                        eps = []
+                        xdialogue = dialogue[1::2]
+                        ydialogue = dialogue[2::2]
+                        for xturn, yturn in zip(xdialogue, ydialogue):
+                            turn = {}
+                            turn['text'] = xturn.get('text').strip()
+                            turn['labels'] = [yturn.get('text').strip()]
+                            turn['episode_done'] = False
+                            label_cands = yturn.get('label_cands')
+                            eps.append(turn)
+                        if eps:
+                            eps[-1]['episode_done'] = True
+                            self.episodes.append(eps)
+                            self.num_exs += 1
 
 
 class AbstractImageTeacher(FixedDialogTeacher):
