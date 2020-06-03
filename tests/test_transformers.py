6b085bd4d86daaf65f54bff4cdc63438682660db
Emily Dinan <edinan@fb.com>
2020-06-03 16:37:12 -0400

    Change whitelist/blacklist terminology  (#2721)
    * remove blacklist + whitelist
    
    * lint
    
    * fix shared bug

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -346,7 +346,7 @@ class TestTransformerGenerator(unittest.TestCase):
                 skip_test=True,
             )
 
-            with open(os.path.join(tmpdir, 'blacklist.txt'), 'w') as f:
+            with open(os.path.join(tmpdir, 'blocklist.txt'), 'w') as f:
                 f.write("38\n62\n")
 
             valid_beam_block3, _ = testing_utils.eval_model(
@@ -357,7 +357,7 @@ class TestTransformerGenerator(unittest.TestCase):
                     batch_size=1,
                     inference='beam',
                     beam_size=5,
-                    beam_blacklist_filename=os.path.join(tmpdir, 'blacklist.txt'),
+                    beam_block_list_filename=os.path.join(tmpdir, 'blocklist.txt'),
                     skip_generation=False,
                 ),
                 skip_test=True,
@@ -375,7 +375,7 @@ class TestTransformerGenerator(unittest.TestCase):
         self.assertLessEqual(valid_beam_block2['f1'], 0.6)
         self.assertLessEqual(valid_beam_block2['bleu-4'], 1e-6)
 
-        # Beam Block blacklist
+        # Beam Block block_list
         self.assertLess(valid_beam_block3['bleu-4'], valid['bleu-4'])
         self.assertLess(valid_beam_block3['f1'], valid['f1'])
 
