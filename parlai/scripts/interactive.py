11da27ba9858442281bd6c3ce93f9b844031eea1
Pedram <salimi.733726@gmail.com>
2020-06-06 08:07:52 +0430

    Added WorldLogger functionality to interactive.py (#2730)
    * Added WorldLogger functionality to interactive.py
    
    * whitespace fix
    
    * more whitespace
    
    * Update interactive.py
    
    Co-authored-by: Stephen Roller <roller@fb.com>

--- parlai/scripts/interactive.py
+++ parlai/scripts/interactive.py
@@ -22,6 +22,7 @@ from parlai.core.params import ParlaiParser
 from parlai.core.agents import create_agent
 from parlai.core.worlds import create_task
 from parlai.scripts.script import ParlaiScript
+from parlai.utils.world_logging import WorldLogger
 from parlai.agents.local_human.local_human import LocalHumanAgent
 
 import random
@@ -51,6 +52,13 @@ def setup_args(parser=None):
         default=True,
         help='Create interactive version of task',
     )
+    parser.add_argument(
+        '--save-world-logs',
+        type='bool',
+        default=False,
+        help='Saves a jsonl file containing all of the task examples and '
+        'model replies. Must also specify --report-filename.',
+    )
     parser.set_defaults(interactive_mode=True, task='interactive')
     LocalHumanAgent.add_cmdline_args(parser)
     return parser
@@ -73,14 +81,24 @@ def interactive(opt, print_parser=None):
         print_parser.opt = agent.opt
         print_parser.print_args()
     human_agent = LocalHumanAgent(opt)
+    # set up world logger
+    world_logger = WorldLogger(opt) if opt['save_world_logs'] else None
     world = create_task(opt, [human_agent, agent])
 
     # Show some example dialogs:
     while not world.epoch_done():
         world.parley()
+        if world_logger is not None:
+            world_logger.log(world)
         if opt.get('display_examples'):
             print("---")
             print(world.display())
+        if world_logger is not None:
+            # dump world acts to file
+            world_logger.reset()  # add final acts to logs
+            base_outfile = opt['report_filename'].split('.')[0]
+            outfile = f'{base_outfile}_{opt["task"]}_replies.jsonl'
+            world_logger.write(outfile, world, file_format=opt['save_format'])
 
 
 class Interactive(ParlaiScript):
