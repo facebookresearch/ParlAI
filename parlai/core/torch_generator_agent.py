Fixed NucleusSampling masking

parlai/core/torch_generator_agent.py
@@ -1534,10 +1534,9 @@ class NucleusSampling(TreeSearch):
         # for the probabilities in order to compute the CDF.
         probs = torch.softmax(logprobs, dim=-1)
         sprobs, sinds = probs.sort(dim=-1, descending=True)
-        # The subtraction here is so that we always include the first word to
-        # go over p. For example, if the most probable token has a prob of 0.5, and
-        # p = 0.3, then we need still need to include that first token.
-        mask = (sprobs.cumsum(dim=-1) - sprobs[:, :1]) >= self.p
+        # The subtraction here is to get the exclusive prefix sum,
+        # to guarantee the first element is not masked
+        mask = (sprobs.cumsum(dim=-1) - sprobs[:, :]) >= self.p
         sprobs[mask] = 0
         sprobs.div_(sprobs.sum(dim=-1).unsqueeze(1))
         choices = torch.multinomial(sprobs, 1)[:, 0]