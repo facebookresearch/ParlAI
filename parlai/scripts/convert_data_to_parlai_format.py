562a18c4dc98634fcb4af9b4a5ae9212a99812d6
Stephen Roller <roller@fb.com>
2020-06-22 00:03:59 -0400

    Undo the deletes jason wants to keep.

---
+++ parlai/scripts/convert_data_to_parlai_format.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+"""
+Convert a dataset into the ParlAI text format.
+
+Examples
+--------
+
+.. code-block:: shell
+
+  python convert_data_to_parlai_format.py -t babi:task1k:1 --outfile /tmp/dump
+"""
+
+from parlai.core.params import ParlaiParser
+from parlai.agents.repeat_label.repeat_label import RepeatLabelAgent
+from parlai.core.worlds import create_task
+from parlai.utils.misc import msg_to_str, TimeLogger
+import parlai.utils.logging as logging
+from parlai.scripts.script import ParlaiScript
+import random
+import tempfile
+
+
+def dump_data(opt):
+    # create repeat label agent and assign it to the specified task
+    agent = RepeatLabelAgent(opt)
+    world = create_task(opt, agent)
+    ignorefields = opt.get('ignore_fields', '')
+    if opt['outfile'] is None:
+        outfile = tempfile.mkstemp(
+            prefix='{}_{}_'.format(opt['task'], opt['datatype']), suffix='.txt'
+        )[1]
+    else:
+        outfile = opt['outfile']
+
+    if opt['num_examples'] == -1:
+        num_examples = world.num_examples()
+    else:
+        num_examples = opt['num_examples']
+    log_timer = TimeLogger()
+
+    logging.debug('starting to convert...')
+    logging.info(f'saving output to {outfile}')
+    fw = open(outfile, 'w')
+    for _ in range(num_examples):
+        world.parley()
+        acts = world.get_acts()
+        value = acts[0].get('labels', acts[0].pop('eval_labels', None))
+        acts[0].force_set('labels', value)
+        txt = msg_to_str(acts[0], ignore_fields=ignorefields)
+        fw.write(txt + '\n')
+        if acts[0].get('episode_done', False):
+            fw.write('\n')
+
+        if log_timer.time() > opt['log_every_n_secs']:
+            text, _log = log_timer.log(world.total_parleys, world.num_examples())
+            logging.info(text)
+
+        if world.epoch_done():
+            logging.info('epoch done')
+            break
+    fw.close()
+
+
+def setup_args():
+    # Get command line arguments
+    parser = ParlaiParser()
+    parser.add_argument(
+        '-n',
+        '--num-examples',
+        default=-1,
+        type=int,
+        help='Total number of exs to convert, -1 to convert all examples',
+    )
+    parser.add_argument(
+        '-of',
+        '--outfile',
+        default=None,
+        type=str,
+        help='Output file where to save, by default will be created in tmp',
+    )
+    parser.add_argument(
+        '-if',
+        '--ignore-fields',
+        default='id',
+        type=str,
+        help='Ignore these fields from the message (returned with .act() )',
+    )
+    parser.add_argument('-ltim', '--log-every-n-secs', type=float, default=2)
+    parser.set_defaults(datatype='train:stream')
+    return parser
+
+
+class ConvertDataToParlaiFormat(ParlaiScript):
+    @classmethod
+    def setup_args(cls):
+        return setup_args()
+
+    def run(self):
+        return dump_data(self.opt)
+
+
+if __name__ == '__main__':
+    random.seed(42)
+    ConvertDataToParlaiFormat.main()
---
+++ parlai/scripts/profile_train.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+"""
+Run the python or pytorch profiler and prints the results.
+
+Examples
+--------
+
+To make sure that bAbI task 1 (1k exs) loads one can run and to
+see a few of them:
+
+.. code-block:: shell
+
+  python examples/profile.py -t babi:task1k:1 -m seq2seq -e 0.1 --dict-file /tmp/dict
+"""
+
+from parlai.core.params import ParlaiParser
+from parlai.scripts.script import ParlaiScript
+from parlai.scripts.train_model import setup_args as train_args
+from parlai.scripts.train_model import TrainLoop
+import parlai.utils.logging as logging
+import cProfile
+import io
+import pdb
+import pstats
+
+try:
+    import torch
+except ImportError:
+    logging.error('Torch not found -- only cProfile allowed with this tool.')
+
+
+def setup_args(parser=None):
+    if parser is None:
+        parser = ParlaiParser(True, True, 'cProfile a training run')
+    parser = train_args(parser)
+    profile = parser.add_argument_group('Profiler Arguments')
+    profile.add_argument(
+        '--torch',
+        type='bool',
+        default=False,
+        help='If true, use the torch profiler. Otherwise use cProfile.',
+    )
+    profile.add_argument(
+        '--torch-cuda',
+        type='bool',
+        default=False,
+        help='If true, use the torch cuda profiler. Otherwise use cProfile.',
+    )
+    profile.add_argument(
+        '--debug',
+        type='bool',
+        default=False,
+        help='If true, enter debugger at end of run.',
+    )
+    return parser
+
+
+def profile(opt):
+    if isinstance(opt, ParlaiParser):
+        logging.error('profile should be passed opt not Parser')
+        opt = opt.parse_args()
+    if opt['torch'] or opt['torch_cuda']:
+        with torch.autograd.profiler.profile(use_cuda=opt['torch_cuda']) as prof:
+            TrainLoop(opt).train()
+        print(prof.total_average())
+
+        sort_cpu = sorted(prof.key_averages(), key=lambda k: k.cpu_time)
+        sort_cuda = sorted(prof.key_averages(), key=lambda k: k.cuda_time)
+
+        def cpu():
+            for e in sort_cpu:
+                print(e)
+
+        def cuda():
+            for e in sort_cuda:
+                print(e)
+
+        cpu()
+
+        if opt['debug']:
+            print(
+                '`cpu()` prints out cpu-sorted list, '
+                '`cuda()` prints cuda-sorted list'
+            )
+
+            pdb.set_trace()
+    else:
+        pr = cProfile.Profile()
+        pr.enable()
+        TrainLoop(opt).train()
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        if opt['debug']:
+            pdb.set_trace()
+
+
+class ProfileTrain(ParlaiScript):
+    @classmethod
+    def setup_args(cls):
+        return setup_args()
+
+    def run(self):
+        return profile(self.opt)
+
+
+if __name__ == '__main__':
+    ProfileTrain.main()
