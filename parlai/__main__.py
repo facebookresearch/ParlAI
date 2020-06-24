f942c628ed4dc69d1ff239e1d89b26e2807099af
Stephen Roller <roller@fb.com>
2020-06-24 16:09:06 -0400

    Fixed ParlAI entry point (#2785)
    * Better entry point
    
    * New entry point, fixed setup.py.
    
    * Fix versioning errors.
    
    * Reviewer comments.

--- parlai/__main__.py
+++ parlai/__main__.py
@@ -3,3 +3,8 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+
+from parlai.scripts.parlai_exe import Parlai as main
+
+if __name__ == '__main__':
+    main()
