1ef4c78f533d07dba27b1a5198cd6ab16f8d28cb
Kurt Shuster <kshuster@fb.com>
2020-07-08 16:16:38 -0400

    [Image] Add ResNeXt101 WSL Spatial Features (#2822)
    * add resnext spatial features to image featurizer
    
    * autoformat
    
    * address eric comments

--- parlai/core/teachers.py
+++ parlai/core/teachers.py
@@ -1881,7 +1881,10 @@ class AbstractImageTeacher(FixedDialogTeacher):
             img_id = ex[self.image_id_key]
             img_path = self.image_id_to_image_path(img_id)
             image = self.image_loader.load(img_path).detach()
-            image = image[0, :, 0, 0]
+            # spatial features are [1, image_dim, spatial_dim, spatial_dim] tensors.
+            # reduce non-spatial features to one-dimensional feature prior to saving.
+            if 'spatial' not in self.image_mode:
+                image = image[0, :, 0, 0]
             image_features_dict[img_id] = image
             num += 1
             pbar.update(1)
