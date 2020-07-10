edebbe0e4c18230c13cd7ce30bc6bb8f591c1380
EricMichaelSmith <ems@fb.com>
2020-07-10 09:20:22 -0400

    Dump build code

---
+++ parlai/zoo/style_gen/prev_curr_classifier.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+"""
+Classifier trained on Image-Chat data with personalities used as labels and the previous
+and current utterances used as context.
+"""
+
+from parlai.core.build_data import download_models
+
+
+def download(datapath):
+    # TODO: revise below
+    model_type = 'bst_single_task'
+    version = 'v1.0'
+    opt = {'datapath': datapath, 'model_type': model_type}
+    fnames = [f'{version}.tar.gz']
+    download_models(
+        opt=opt,
+        fnames=fnames,
+        model_folder='blended_skill_talk',
+        version=version,
+        use_model_type=True,
+    )
