e9ca36ace3bd4db28cbfc5698521d06907106fe4
Diana Rico <dianaglzrico@devfair0263.h2.fair>
2020-07-16 10:27:34 -0700

    abstract wrapper

--- parlai/tasks/wrapper/agents.py
+++ parlai/tasks/wrapper/agents.py
@@ -23,37 +23,20 @@ from parlai.core.opt import Opt
 from parlai.core.teachers import create_task_agent_from_taskname, Teacher
 
 
-class LabelToTextTeacher(Teacher):
+class AbstractWrapperTeacher(Teacher):
     """
-    Teacher that will shift message['labels'][0] into message['text'] for whatever task
-    is specified with --label-to-text-task.
-
-    Because the dialogue history is effectively overwritten by this action, all episodes
-    will be flattened into one example each.
+    Abstract teacher working as base for wrapper teachers.
     """
 
-    @classmethod
-    def add_cmdline_args(cls, argparser):
-        # Teacher has no args
-        parser = argparser.add_argument_group('LabelToText args')
-        parser.add_argument(
-            '-l2tt',
-            '--label-to-text-task',
-            type=str,
-            help='The task whose labels will get shifted into the text field',
-        )
-
+    @abstractmethod
     def __init__(self, opt: Opt, shared=None):
-        if ',' in opt['task']:
-            raise ValueError('LabelToTextTeacher cannot be used with multiple tasks!')
-        self.id = opt['task']
-        self.opt = opt
-        if shared and 'task' in shared:
-            self.task = create_agent_from_shared(shared['task'])
-        else:
-            opt_singletask = copy.deepcopy(opt)
-            opt_singletask['task'] = opt['label_to_text_task']
-            self.task = create_task_agent_from_taskname(opt_singletask)[0]
+        pass
+
+    def act(self):
+        """
+        Act on the previous observation.
+        """
+        raise NotImplementedError('Abstract class: user must implement act() method')
 
     def num_examples(self):
         """
@@ -74,27 +57,6 @@ class LabelToTextTeacher(Teacher):
         Make an observation.
         """
         return self.task.observe(observation)
-
-    def act(self):
-        """
-        Act on the previous observation.
-        """
-        act = self.task.act()
-        new_act = copy.deepcopy(act)
-        if 'labels' in act:
-            labels = act['labels']
-            assert len(labels) == 1
-            new_act.force_set('text', labels[0])
-            new_act.force_set('labels', [''])
-        elif 'eval_labels' in act:
-            labels = act['eval_labels']
-            assert len(labels) == 1
-            new_act.force_set('text', labels[0])
-            new_act.force_set('eval_labels', [''])
-        else:
-            assert 'text' not in act and act['episode_done'] is True
-        new_act.force_set('episode_done', True)  # Clear the dialogue history
-        return new_act
 
     def epoch_done(self):
         """
@@ -144,3 +106,58 @@ class LabelToTextTeacher(Teacher):
 
     def update_counters(self):
         self.task.update_counters()
+
+
+class LabelToTextTeacher(AbstractWrapperTeacher):
+    """
+    Teacher that will shift message['labels'][0] into message['text'] for whatever task
+    is specified with --label-to-text-task.
+
+    Because the dialogue history is effectively overwritten by this action, all episodes
+    will be flattened into one example each.
+    """
+
+    @classmethod
+    def add_cmdline_args(cls, argparser):
+        # Teacher has no args
+        parser = argparser.add_argument_group('LabelToText args')
+        parser.add_argument(
+            '-l2tt',
+            '--label-to-text-task',
+            type=str,
+            help='The task whose labels will get shifted into the text field',
+        )
+
+    def __init__(self, opt: Opt, shared=None):
+        if ',' in opt['task']:
+            raise ValueError('LabelToTextTeacher cannot be used with multiple tasks!')
+        self.id = opt['task']
+        self.opt = opt
+        if shared and 'task' in shared:
+            self.task = create_agent_from_shared(shared['task'])
+        else:
+            opt_singletask = copy.deepcopy(opt)
+            opt_singletask['task'] = opt['label_to_text_task']
+            self.task = create_task_agent_from_taskname(opt_singletask)[0]
+
+
+    def act(self):
+        """
+        Act on the previous observation.
+        """
+        act = self.task.act()
+        new_act = copy.deepcopy(act)
+        if 'labels' in act:
+            labels = act['labels']
+            assert len(labels) == 1
+            new_act.force_set('text', labels[0])
+            new_act.force_set('labels', [''])
+        elif 'eval_labels' in act:
+            labels = act['eval_labels']
+            assert len(labels) == 1
+            new_act.force_set('text', labels[0])
+            new_act.force_set('eval_labels', [''])
+        else:
+            assert 'text' not in act and act['episode_done'] is True
+        new_act.force_set('episode_done', True)  # Clear the dialogue history
+        return new_act
