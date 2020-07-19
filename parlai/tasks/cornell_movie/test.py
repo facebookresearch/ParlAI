5c03fc353d156b329ab30b723ed4fddaf9555db3
Stephen Roller <roller@fb.com>
2020-07-19 15:53:17 -0400

    More. :)

---
+++ parlai/tasks/cornell_movie/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from parlai.utils.testing import AutoTeacherTest  # noqa: F401
+import unittest
+
+
+class TestDefaultTeacher(unittest.TestCase, AutoTeacherTest):
+    task = 'cornell_movie'
+
+
+class TestAll1kTeacher(unittest.TestCase, AutoTeacherTest):
+    task = 'cornell_movie:double'
