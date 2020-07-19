5f5faa5e49fd758b93b335b8019711c5cf8a4ea5
Stephen Roller <roller@fb.com>
2020-07-18 20:43:34 -0400

    Teacher tests.

--- parlai/utils/testing.py
+++ parlai/utils/testing.py
@@ -364,3 +364,79 @@ def display_model(opt) -> Tuple[str, str, str]:
         popt['datatype'] = 'test:stream'
         dm.display_model(popt)
     return (train_output.getvalue(), valid_output.getvalue(), test_output.getvalue())
+
+
+class AutoTeacherTest:
+    @property
+    def task(self):
+        modulename = str(type(self).__module__)
+        assert modulename.startswith("parlai.tasks.")
+        assert modulename.endswith(".test")
+        path = modulename[13:-5]
+        return path
+
+    def _run_display_data(self, datatype, **kwargs):
+        import parlai.scripts.display_data as dd
+
+        with capture_output() as output:
+            dd.DisplayData.main(
+                task=self.task, datatype=datatype, display_verbose=True, **kwargs
+            )
+        value = output.getvalue()
+        assert value
+
+    def test_train(self):
+        """Test --datatype train"""
+        return self._run_display_data('train')
+
+    def test_train_stream(self):
+        """Test --datatype train:stream"""
+        return self._run_display_data('train:stream')
+
+    def test_train_stream_ordered(self):
+        """Test --datatype train:stream:ordered"""
+        return self._run_display_data('train:stream:ordered')
+
+    def test_valid(self):
+        """Test --datatype valid"""
+        return self._run_display_data('valid')
+
+    def test_valid_stream(self):
+        """Test --datatype valid:stream"""
+        return self._run_display_data('valid:stream')
+
+    def test_test(self):
+        """Test --datatype test"""
+        return self._run_display_data('test')
+
+    def test_valid(self):
+        """Test --datatype test:stream"""
+        return self._run_display_data('test:stream')
+
+    def test_bs2_train(self):
+        """Test --datatype train"""
+        return self._run_display_data('train', batchsize=2)
+
+    def test_bs2_train_stream(self):
+        """Test --datatype train:stream"""
+        return self._run_display_data('train:stream', batchsize=2)
+
+    def test_bs2_train_stream_ordered(self):
+        """Test --datatype train:stream:ordered"""
+        return self._run_display_data('train:stream:ordered', batchsize=2)
+
+    def test_bs2_valid(self):
+        """Test --datatype valid"""
+        return self._run_display_data('valid', batchsize=2)
+
+    def test_bs2_valid_stream(self):
+        """Test --datatype valid:stream"""
+        return self._run_display_data('valid:stream', batchsize=2)
+
+    def test_bs2_test(self):
+        """Test --datatype test"""
+        return self._run_display_data('test', batchsize=2)
+
+    def test_bs2_valid(self):
+        """Test --datatype test:stream"""
+        return self._run_display_data('test:stream', batchsize=2)
