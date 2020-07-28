b36c9706f167fb15f23dc7b4556fa054351f8066
EricMichaelSmith <ems@fb.com>
2020-07-28 16:58:06 -0400

    Compare dicts with diff values

--- parlai/scripts/compare_opts.py
+++ parlai/scripts/compare_opts.py
@@ -22,21 +22,36 @@ def compare_opts(opt_path_1: str, opt_path_2: str):
     print('\nArgs only found in opt 1:')
     opt1_only_keys = sorted([k for k in opt1.keys() if k not in opt2.keys()])
     for key in opt1_only_keys:
-        print(f'\t{key}: {opt1[key]}')
+        print(f'{key}: {opt1[key]}')
 
     print('\nArgs only found in opt 2:')
     opt2_only_keys = sorted([k for k in opt2.keys() if k not in opt1.keys()])
     for key in opt2_only_keys:
-        print(f'\t{key}: {opt2[key]}')
+        print(f'{key}: {opt2[key]}')
 
     print('\nArgs that are different in both opts:')
     keys_with_conflicting_values = sorted(
         [k for k, v in opt1.items() if k in opt2.keys() and v != opt2[k]]
     )
     for key in keys_with_conflicting_values:
-        print(f'\t{key}:')
-        print(f'\t\tIn opt 1: {opt1[key]}')
-        print(f'\t\tIn opt 2: {opt2[key]}')
+        if isinstance(opt1[key], dict) and isinstance(opt2[key], dict):
+            print(f'{key} (printing only non-matching values in each dict):')
+            all_inner_keys = sorted(
+                list(set(opt1[key].keys()).union(set(opt2[key].keys())))
+            )
+            for inner_key in all_inner_keys:
+                if (
+                    inner_key not in opt1[key]
+                    or inner_key not in opt2[key]
+                    or opt1[key][inner_key] != opt2[key][inner_key]
+                ):
+                    print(f'\t{key}:')
+                    print(f'\t\tIn opt 1: {opt1[key].get(inner_key, "<MISSING>")}')
+                    print(f'\t\tIn opt 2: {opt2[key].get(inner_key, "<MISSING>")}')
+        else:
+            print(f'{key}:')
+            print(f'\tIn opt 1: {opt1[key]}')
+            print(f'\tIn opt 2: {opt2[key]}')
 
 
 if __name__ == '__main__':
