96d710a85d3569c057400585738e2eff5eddda18
klshuster <kshuster@fb.com>
2020-07-29 11:10:31 -0400

    black

--- parlai/agents/bart/modules.py
+++ parlai/agents/bart/modules.py
@@ -33,13 +33,19 @@ class BartModel(TransformerGeneratorModel):
         """
         Decode with a fixed, true sequence, computing loss.
 
-        Override to bypass assertion that BOS is not present,
-        and additionally insert EOS as first token
+        Override to bypass assertion that BOS is not present, and additionally insert
+        EOS as first token
         """
         bsz = ys.size(0)
         seqlen = ys.size(1)
         inputs = ys.narrow(1, 0, seqlen - 1)
-        inputs = torch.cat([torch.LongTensor([self.END_IDX]).detach().expand(bsz, 1).to(inputs), inputs], 1)
+        inputs = torch.cat(
+            [
+                torch.LongTensor([self.END_IDX]).detach().expand(bsz, 1).to(inputs),
+                inputs,
+            ],
+            1,
+        )
         latent, _ = self.decoder(inputs, encoder_states)
         logits = self.output(latent)
         _, preds = logits.max(dim=2)
