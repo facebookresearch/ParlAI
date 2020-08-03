3510f052f6c745666f98f0e80d28bf0fd75f5cf1
EricMichaelSmith <ems@fb.com>
2020-08-03 16:17:41 -0400

    Change style parsing

--- projects/style_gen/modules.py
+++ projects/style_gen/modules.py
@@ -8,57 +8,12 @@ Models and helper classes for style-controlled generation.
 """
 
 import random
+from typing import Optional
 
-from parlai.core.torch_agent import History
+from parlai.core.message import Message
 
 
 STYLE_SEP_TOKEN = ' STYLE '
-
-
-class StyleHistoryMixin(History):
-    """
-    Methods for adding style to history.
-    """
-
-    def __init__(self, opt, **kwargs):
-        super().__init__(opt, **kwargs)
-        self.use_style_frac = opt['use_style_frac']
-        self.style = None
-
-    def reset(self):
-        super().reset()
-        self.style = None
-
-    def update_history(self, obs, *args, **kwargs):
-        super().update_history(obs, *args, **kwargs)
-        use_style_rand = random.random()
-        if use_style_rand < self.use_style_frac:
-            # Use the style
-            self.style = obs.get('personality')
-            # This key name is dependent on Image-Chat and will change for other tasks.
-            # If obs does not contain 'personality' (i.e. at the end of an epoch during
-            # validation), there will be no style
-            if self.style == '':
-                self.style = None
-        else:
-            self.style = None
-
-    def get_history_str(self):
-        history_str = super().get_history_str()
-        if history_str is not None and self.style is not None:
-            history_str += STYLE_SEP_TOKEN + self.style
-
-        return history_str
-
-    def get_history_vec(self):
-        history = super().get_history_vec()
-
-        if history is not None and self.style is not None:
-            style = STYLE_SEP_TOKEN + self.style
-            style_tok = self.parse(style)
-            history += style_tok
-
-        return history
 
 
 class StyleAgentMixin:
@@ -82,8 +37,22 @@ class StyleAgentMixin:
         )
         return agent
 
+    def __init__(self, opt, **kwargs):
+        super().__init__(opt, **kwargs)
+        self.use_style_frac = opt['use_style_frac']
 
-class StyleHistory(StyleHistoryMixin, History):
-    """
-    Modify history to save the style.
-    """
+    def get_temp_history(self, observation: Message) -> Optional[str]:
+        """
+        Conditionally return a style-token string to temporarily insert into history.
+        """
+        use_style_rand = random.random()
+        if use_style_rand < self.use_style_frac:
+            # Use the style
+            style = observation.get('personality')
+            # This key name is dependent on Image-Chat and will change for other tasks.
+            # If obs does not contain 'personality' (i.e. at the end of an epoch during
+            # validation), there will be no style
+        else:
+            style = ''
+        if style is not None and style != '':
+            return STYLE_SEP_TOKEN + style
