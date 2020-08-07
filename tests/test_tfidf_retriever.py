d54c77d3ccf46af95b6499e71c4f39bf9e0e1437
Stephen Roller <roller@fb.com>
2020-08-07 01:43:18 -0400

    Remove some bad tokenizers; include eval_model run of tfidf. Increase coverage.

--- tests/test_tfidf_retriever.py
+++ tests/test_tfidf_retriever.py
@@ -12,13 +12,7 @@ from parlai.utils.logging import logger, ERROR
 import os
 import unittest
 
-SKIP_TESTS = False
-try:
-    from parlai.agents.tfidf_retriever.tfidf_retriever import (  # noqa: F401
-        TfidfRetrieverAgent,
-    )
-except ImportError:
-    SKIP_TESTS = True
+import parlai.utils.testing as testing_utils
 
 
 class TestTfidfRetriever(unittest.TestCase):
@@ -26,64 +20,45 @@ class TestTfidfRetriever(unittest.TestCase):
     Basic tests on the display_data.py example.
     """
 
-    @unittest.skipIf(SKIP_TESTS, "Missing  Tfidf dependencies.")
     def test_sparse_tfidf_retriever(self):
-        MODEL_FILE = '/tmp/tmp_test_babi'
-        DB_PATH = '/tmp/tmp_test_babi.db'
-        TFIDF_PATH = '/tmp/tmp_test_babi.tfidf'
-        # keep things quiet
-        logger.setLevel(ERROR)
-        try:
-            parser = ParlaiParser(True, True)
-            parser.set_defaults(
+        testing_utils.train_model(
+            dict(
                 model='tfidf_retriever',
                 task='babi:task1k:1',
-                model_file=MODEL_FILE,
                 retriever_numworkers=4,
                 retriever_hashsize=2 ** 8,
                 datatype='train:ordered',
                 num_epochs=1,
             )
-            opt = parser.parse_args([])
-            agent = create_agent(opt)
-            train_world = create_task(opt, agent)
-            # pass examples to dictionary
-            while not train_world.epoch_done():
-                train_world.parley()
+        )
 
-            obs = {
-                'text': (
-                    'Mary moved to the bathroom. John went to the hallway. '
-                    'Where is Mary?'
-                ),
-                'episode_done': True,
-            }
-            agent.observe(obs)
-            reply = agent.act()
-            assert reply['text'] == 'bathroom'
+    def test_ptb_tok(self):
+        """Tests with the PTB tokenizer."""
+        testing_utils.train_model(
+            dict(
+                model='tfidf_retriever',
+                task='babi:task1k:1',
+                retriever_numworkers=4,
+                retriever_hashsize=2 ** 8,
+                datatype='train:ordered',
+                num_epochs=1,
+                retriever_tokenizer='regexp',
+            )
+        )
 
-            ANS = 'The one true label.'
-            new_example = {
-                'text': 'A bunch of new words that are not in the other task, '
-                'which the model should be able to use to identify '
-                'this label.',
-                'labels': [ANS],
-                'episode_done': True,
-            }
-            agent.observe(new_example)
-            reply = agent.act()
-            assert 'text' in reply and reply['text'] == ANS
-
-            new_example.pop('labels')
-            agent.observe(new_example)
-            reply = agent.act()
-            assert reply['text'] == ANS
-        finally:
-            # clean up files
-            if os.path.exists(DB_PATH):
-                os.remove(DB_PATH)
-            if os.path.exists(TFIDF_PATH + '.npz'):
-                os.remove(TFIDF_PATH + '.npz')
+    def test_simple_tok(self):
+        """Tests with the simple tokenizer."""
+        testing_utils.train_model(
+            dict(
+                model='tfidf_retriever',
+                task='babi:task1k:1',
+                retriever_numworkers=4,
+                retriever_hashsize=2 ** 8,
+                datatype='train:ordered',
+                num_epochs=1,
+                retriever_tokenizer='simple',
+            )
+        )
 
 
 if __name__ == '__main__':
