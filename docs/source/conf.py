fdba05e3062e84edd9dc4765567b145a73a538b2
Stephen Roller <roller@fb.com>
2020-08-25 00:38:37 -0400

    After myst 0.13.0 is released, this will be fine.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -125,9 +125,9 @@ html_logo = '_static/img/parlai.png'
 
 # myst options
 myst_admonition_enable = True
-myst_amsmath_enable = True
-myst_html_img_enable = True
-myst_dmath_enable = True
+# myst_amsmath_enable = True
+# myst_html_img_enable = True
+# myst_dmath_enable = True
 
 
 # -- Options for HTMLHelp output ------------------------------------------
