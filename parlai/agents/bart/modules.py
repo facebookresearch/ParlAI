307c1387d8dc751c0055174b0e63d947539a775b
klshuster <kshuster@fb.com>
2020-08-27 16:50:06 -0400

    initial decoder

--- parlai/agents/bart/modules.py
+++ parlai/agents/bart/modules.py
@@ -7,7 +7,7 @@ BART Module.
 """
 import torch
 import torch.nn.functional as F
-from typing import Any, Dict, Union, List, Optional
+from typing import Tuple, Any, Dict, Union, List, Optional
 
 from parlai.agents.transformer.modules import TransformerGeneratorModel
 
@@ -27,14 +27,28 @@ class BartModel(TransformerGeneratorModel):
         output = F.linear(tensor, self.embeddings.weight)
         return output
 
+    def _get_initial_forced_decoder_input(self, bsz: int, inputs: torch.LongTensor):
+        """
+        Return initial input to the decoder.
+
+        :param bsz:
+            batchsize
+        :param inputs:
+            inputs to decode
+
+        :return initial_input:
+            initial input for the decoder.
+        """
+        return torch.cat([torch.LongTensor([self.END_IDX]).to(inputs).detach().expand(bsz, 1), inputs], 1)
+
     def reorder_decoder_incremental_state(
         self,
         incremental_state: Dict[str, Any],
         inds: Union[List[int], torch.LongTensor],
     ) -> Optional[Dict[str, Any]]:
         """
-        We seed decoder with 2 tokens to start, so this is None.
+        Incremental state is too hard with all the docs and what not.
 
-        TODO: can this be handled?
+        We leave as a future exercise.
         """
         return None
