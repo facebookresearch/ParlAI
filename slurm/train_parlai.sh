f58e4db1b23aec3dabf78c27bafec8a84954a017
David Wilmot <david.wilmot@gmail.com>
2020-09-19 00:07:51 +0100

    Fix serial dir config.

--- slurm/train_parlai.sh
+++ slurm/train_parlai.sh
@@ -49,10 +49,11 @@ find ${SCRATCH_HOME} -type d -name "*" -mtime +7 -printf "%T+ %p\n" | sort | cut
 echo "Scratch home: ${SCRATCH_HOME}"
 
 export EXP_ROOT="${CLUSTER_HOME}/git/ParlAI"
-export SERIAL_DIR="${SCRATCH_HOME}/${EXP_ID}"
 
 export EXP_ID="${EXP_NAME}_${SLURM_JOB_ID}_${CURRENT_TIME}"
 echo "Experiment ID: ${EXP_ID}"
+export SERIAL_DIR="${SCRATCH_HOME}/${EXP_ID}"
+echo "Serial dir: ${SERIAL_DIR}"
 
 cd "${EXP_ROOT}"
 
@@ -66,7 +67,7 @@ echo "ParlAI Task========"
 parlai train_model -m hugging_face/gpt2 -mf "${SERIAL_DIR}/${EXP_ID}.mod" \
  --add-special-tokens=True --add-start-token=True --gpt2-size=medium -t writing_prompts \
  --batchsize=4 --text-truncate=128 --label-truncate=128 --datapath="${DATASET_SOURCE}" \
- --save-after-valid=True --num-epochs=NUM_EPOCHS --max_train_time=518400 --validation-patience=5 \
+ --save-after-valid=True --num-epochs=NUM_EPOCHS --max_train_time=144 --validation-patience=5 \
  --validation-every-n-secs=10800 --validation-max-exs=5000
 
 echo "============"
