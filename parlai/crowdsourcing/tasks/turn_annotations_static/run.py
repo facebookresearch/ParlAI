fec83d5d0ae1e4546dc4fb4a71f80fd96c89bccf
EricMichaelSmith <ems@fb.com>
2020-09-28 15:13:45 -0400

    Port over more tags

--- parlai/crowdsourcing/tasks/turn_annotations_static/run.py
+++ parlai/crowdsourcing/tasks/turn_annotations_static/run.py
@@ -60,8 +60,6 @@ def main(cfg: DictConfig) -> None:
     arg_string = (
         f"--task-name turn-ann-s "
         f'--task-source "{TASK_DIRECTORY}/webapp/build/bundle.js" '
-        f'--task-tags chat,conversation,dialog,partner '
-        f"--allowed-concurrent 1 "
         f'--extra-source-dir "{STATIC_FILES_DIR}" '
         f'--port 2222 '
         f'--onboarding-qualification turn-ann-s-onb '
