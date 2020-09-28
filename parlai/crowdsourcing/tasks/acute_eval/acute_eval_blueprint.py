7bcaa0e12886d21110e51212ab266e7ad8a0b1a0
Eric Smith <EricMichaelSmith@users.noreply.github.com>
2020-09-28 10:55:13 -0400

    Porting ACUTE-Eval task to Hydra (#3103)
    * Start changes to example_script
    
    * Starting to revise blueprint
    
    * Port over blueprint script
    
    * Port over runner script
    
    * Work on example script
    
    * Finish porting over example script
    
    * Minor
    
    * Current time
    
    * Fixes
    
    * README
    
    * Start revising README
    
    * Fix README
    
    * Path fix
    
    * Jack's PR comments
    
    * Comments
    
    * Removing unused code
    
    * Assume block_on_onboarding_fail exists
    
    * Minor
    
    * test_init_everywhere fix

--- parlai/crowdsourcing/tasks/acute_eval/acute_eval_blueprint.py
+++ parlai/crowdsourcing/tasks/acute_eval/acute_eval_blueprint.py
@@ -4,25 +4,79 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from mephisto.data_model.blueprint import Blueprint
+import os
+import math
+from dataclasses import dataclass, field
+from typing import ClassVar, List, Type, Any, Dict, Iterable, TYPE_CHECKING
+
+from mephisto.core.registry import register_mephisto_abstraction
+from mephisto.data_model.blueprint import Blueprint, BlueprintArgs, SharedTaskState
 from mephisto.data_model.assignment import InitializationData
+from omegaconf import MISSING, DictConfig
+
 from parlai.crowdsourcing.tasks.acute_eval.acute_eval_agent_state import (
     AcuteEvalAgentState,
 )
-from parlai.crowdsourcing.tasks.acute_eval.acute_eval_runner import AcuteEvalRunner
 from parlai.crowdsourcing.tasks.acute_eval.acute_eval_builder import AcuteEvalBuilder
-from mephisto.core.registry import register_mephisto_abstraction
-
-import os
-import math
-
-from typing import ClassVar, List, Type, Any, Dict, Iterable, TYPE_CHECKING
+from parlai.crowdsourcing.tasks.acute_eval.acute_eval_runner import AcuteEvalRunner
 
 if TYPE_CHECKING:
     from mephisto.data_model.blueprint import AgentState, TaskRunner, TaskBuilder
-    from argparse import _ArgumentGroup as ArgumentGroup
 
 BLUEPRINT_TYPE = "acute_eval"
+
+
+@dataclass
+class AcuteEvalBlueprintArgs(BlueprintArgs):
+    _blueprint_type: str = BLUEPRINT_TYPE
+    _group: str = field(
+        default="AcuteEvalBlueprint",
+        metadata={
+            'help': """\
+Tasks launched from the ACUTE-Eval blueprint require sets of pairings for workers to \
+be able to compare to. These pairings should be provided as a .jsonl with \
+--pairings-filepath."""
+        },
+    )
+    annotations_per_pair: int = field(
+        default=1,
+        metadata={"help": "Number of annotations per conversation comparison pair"},
+    )
+    pairings_filepath: str = field(
+        default=MISSING,
+        metadata={"help": "path to the file containing the task dictionaries"},
+    )
+    s1_choice: str = field(
+        default="I would prefer to talk to <Speaker 1>",
+        metadata={"help": "text next to speaker 1 radio button"},
+    )
+    s2_choice: str = field(
+        default="I would prefer to talk to <Speaker 2>",
+        metadata={"help": "text next to speaker 2 radio button"},
+    )
+    eval_question: str = field(
+        default="Who would you prefer to talk to for a long conversation?",
+        metadata={
+            "help": 'question to present to turker for comparison (e.g. "Which speaker is better?")'
+        },
+    )
+    block_on_onboarding_fail: bool = field(
+        default=True, metadata={"help": "whether to block on onboarding failure"}
+    )
+    num_matchup_pairs: int = field(
+        default=2, metadata={"help": "Number of pairs per model matchup, default 2"}
+    )
+    subtasks_per_unit: int = field(
+        default=5, metadata={"help": "number of subtasks/comparisons to do per unit"}
+    )
+    onboarding_threshold: float = field(
+        default=0.75,
+        metadata={"help": "minimum accuracy on onboarding tasks, as a float 0-1.0"},
+    )
+    random_seed: int = field(default=42, metadata={"help": "seed for random"})
+    additional_task_description: str = field(
+        default='', metadata={"help": "Additional text to show on the left pane"}
+    )
 
 
 # WISH AcuteEval's blueprint can probably be extended to compare more than just convos
@@ -35,122 +89,30 @@ class AcuteEvalBlueprint(Blueprint):
     AgentStateClass: ClassVar[Type["AgentState"]] = AcuteEvalAgentState
     TaskBuilderClass: ClassVar[Type["TaskBuilder"]] = AcuteEvalBuilder
     TaskRunnerClass: ClassVar[Type["TaskRunner"]] = AcuteEvalRunner
+    ArgsClass = AcuteEvalBlueprintArgs
     supported_architects: ClassVar[List[str]] = ["mock"]  # TODO update
     BLUEPRINT_TYPE = BLUEPRINT_TYPE
 
     @classmethod
-    def assert_task_args(cls, opts: Any) -> None:
+    def assert_task_args(cls, args: DictConfig, shared_state: SharedTaskState) -> None:
         """
         Ensure that the data can be properly loaded.
         """
-        if opts.get("pairings_filepath") is not None:
-            pairings_filepath = os.path.expanduser(opts["pairings_filepath"])
+        if args.blueprint.get("pairings_filepath", None) is not None:
+            pairings_filepath = os.path.expanduser(args.blueprint.pairings_filepath)
             assert os.path.exists(
                 pairings_filepath
             ), f"Provided file {pairings_filepath} doesn't exist"
-        elif opts.get("pairings_task_data") is not None:
-            assert (
-                len(opts.get("pairings_task_data")) > 0
-            ), "Length of data dict provided was 0"
         else:
             raise AssertionError(
                 "Must provide one of a data csv, json, or a list of tasks"
             )
 
-        if opts.get("block_on_onboarding_fail") is True:
-            if opts.get("block_qualification") is None:
+        if args.blueprint.block_on_onboarding_fail is True:
+            if args.blueprint.get("block_qualification", None) is None:
                 raise AssertionError(
                     "Must provide `block_qualification` to use `block_on_onboarding_fail`"
                 )
-
-    @classmethod
-    def add_args_to_group(cls, group: "ArgumentGroup") -> None:
-        """
-        Adds required options for AcuteEvalBlueprints.
-
-        task_source points to the file intending to be deployed for this task
-        pairings_filepath has the data to be deployed for this task.
-        """
-        super(AcuteEvalBlueprint, cls).add_args_to_group(group)
-
-        group.description = """
-            AcuteEvalBlueprint: Tasks launched from acute eval blueprints
-            require sets of pairings for workers to be able to compare to.
-
-            These pairings can be provided as a csv or by passing a
-            pairings_task_data dict into extra_args.
-        """
-        group.add_argument(
-            "--annotations-per-pair",
-            dest="annotations_per_pair",
-            type=int,
-            default=1,
-            help="Number of annotations per conversation comparison pair",
-        )
-        group.add_argument(
-            "--pairings-filepath",
-            dest="pairings_filepath",
-            type=str,
-            default=None,
-            help="path to the file containing the task dictionaries",
-        )
-        group.add_argument(
-            "--s1-choice",
-            dest="s1_choice",
-            type=str,
-            default="I would prefer to talk to <Speaker 1>",
-            help="text next to speaker 1 radio button",
-        )
-        group.add_argument(
-            "--s2-choice",
-            dest="s2_choice",
-            type=str,
-            default="I would prefer to talk to <Speaker 2>",
-            help="text next to speaker 2 radio button",
-        )
-        group.add_argument(
-            "--eval-question",
-            dest="eval_question",
-            type=str,
-            default="Who would you prefer to talk to for a long conversation?",
-            help='question to present to turker for comparison (e.g. "Which speaker is better?")',
-        )
-        group.add_argument(
-            "--block-on-onboarding-fail",
-            dest="block_on_onboarding_fail",
-            type=bool,
-            default=True,
-            help="whether to block on onboarding failure",
-        )
-        group.add_argument(
-            "--subtasks-per-unit",
-            dest="subtasks_per_unit",
-            type=int,
-            default=5,
-            help="number of subtasks/comparisons to do per unit",
-        )
-        group.add_argument(
-            "--onboarding-threshold",
-            dest="onboarding_threshold",
-            type=float,
-            default=0.75,
-            help="minimum accuracy on onboarding tasks, as a float 0-1.0",
-        )
-        group.add_argument(
-            "--random-seed",
-            dest="random_seed",
-            type=int,
-            default=42,
-            help="seed for random",
-        )
-        group.add_argument(
-            "--additional-task-description",
-            dest="additional_task_description",
-            type=str,
-            default='',
-            help="Additional text to show on the left pane",
-        )
-        return
 
     def get_frontend_args(self) -> Dict[str, Any]:
         """
@@ -160,11 +122,11 @@ class AcuteEvalBlueprint(Blueprint):
         return {
             "task_description": "Placeholder Task Description - Javascript failed to load",
             "frame_height": 650,
-            "num_subtasks": self.opts["subtasks_per_unit"],
-            "question": self.opts["eval_question"],
+            "num_subtasks": self.args.blueprint.subtasks_per_unit,
+            "question": self.args.blueprint.eval_question,
             "block_mobile": True,
             "get_task_feedback": False,  # TODO(#95) make option
-            "additional_task_description": self.opts['additional_task_description'],
+            "additional_task_description": self.args.blueprint.additional_task_description,
         }
 
     def get_initialization_data(self) -> Iterable["InitializationData"]:
@@ -174,12 +136,11 @@ class AcuteEvalBlueprint(Blueprint):
         # TODO(#99) once we can release HITs over time, configure this to
         # release as many as needed thusfar and top off when
         # onboardings fail
-        print(self.opts)
         num_conversations = math.ceil(
-            self.opts.get("num_matchup_pairs", 8)
-            / max((self.opts["subtasks_per_unit"] - 1), 1)
+            self.args.blueprint.num_matchup_pairs
+            / max((self.args.blueprint.subtasks_per_unit - 1), 1)
         )  # release enough hits to finish all annotations requested
         return [
             InitializationData(shared={}, unit_data=[{}])
-            for d in range(num_conversations)
+            for _ in range(num_conversations)
         ]
