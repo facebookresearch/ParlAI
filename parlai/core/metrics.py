84e80f206816675a39e34cf5948d10a0366f238f
Moya Chen <mpchen@fb.com>
2020-10-01 13:59:26 -0700

    Add notion of metrics collections, which can have other Metrics of multiple metrics be added to it
    See https://github.com/facebookresearch/ParlAI/pull/3138 for context and use

--- parlai/core/metrics.py
+++ parlai/core/metrics.py
@@ -643,7 +643,17 @@ class Metrics(object):
         return {'data': self._data}
 
 
-class TeacherMetrics(Metrics):
+class MetricsCollection(Metrics):
+    """
+    Helper container for Metrics that can have other metrics added to it.
+    """
+
+    def add(self, other: Metrics) -> None:
+        for k, v in other.report():
+            self.add(k, v)
+
+
+class TeacherMetrics(MetricsCollection):
     """
     Helper container which encapsulates standard metrics (F1, BLEU, ...).
     """
