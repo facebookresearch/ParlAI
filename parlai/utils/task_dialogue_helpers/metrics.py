d39e7462d2f6190dd5bedc3adf376910cc6f8552
Moya Chen <mpchen@fb.com>
2020-09-30 23:05:28 -0700

    Made linters happy; added a test case, cleaned up some defs
    Not quiet sure if returning "nans" for F1 metrics is the best way of going about things, but probably okay as a start. Also assuming the Metrics base class will handle addition correctly.
    
    Added the test case to make sure there weren't any glaring syntax errors with how I implemented SlotMetrics
    
    Nominally should probably add more test cases to validate slot-based domain metrics are working correctly, but that seems lower risk/lower pri and will cover it anyway when I update existing tasks.

--- parlai/utils/task_dialogue_helpers/metrics.py
+++ parlai/utils/task_dialogue_helpers/metrics.py
@@ -5,14 +5,51 @@
 # LICENSE file in the root directory of this source tree.
 
 # Helper functions for collecting metrics for dialogue oriented tasks
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple, Dict, Any
 
-from parlai.core.teachers import Teacher
-from parlai.core.metrics import AverageMetric, BleuMetric, F1Metric, Metric
-from parlai.utils.typing import TScalar
+from parlai.core.metrics import AverageMetric, BleuMetric, F1Metric, Metric, Metrics
 
 
-def __add_domain_metrics(
+class SlotMetrics(Metrics):
+    """Helper container which encapsulates standard slot metrics in task-
+    oriented learning (jga, slot_p, slot_r, etc).
+
+    Due to differences in dialogue representations between tasks, the
+    input is pre-parsed ground truth and predicted slot dictionaries.
+    This class will add domain-specific versions of these metrics as
+    well, when appropriate.
+    """
+
+    def __init__(
+        self,
+        teacher_slots: Dict[str, str],
+        predicted_slots: Dict[str, str],
+        domain: Optional[str] = None,
+        valid_domains: Optional[Tuple[str]] = None,
+        shared: Dict[str, Any] = None,
+    ) -> None:
+        super().__init__(shared=shared)
+        self.add("jga", AverageMetric(teacher_slots == predicted_slots))
+        for pred_slot_name, pred_value in predicted_slots.items():
+            slot_p = AverageMetric(teacher_slots.get(pred_slot_name) == pred_value)
+            self.add("slot_p", slot_p)
+            self.add("slot_f1", SlotF1Metric(slot_p=slot_p))
+            if _use_domain_metrics(domain, valid_domains):
+                self.add(f"{domain}/slot_p", slot_p)
+                self.add(f"{domain}/slot_f1", SlotF1Metric(slot_p=slot_p))
+
+        for teacher_slot_name, teacher_value in teacher_slots.items():
+            slot_r = AverageMetric(
+                predicted_slots.get(teacher_slot_name) == teacher_value
+            )
+            self.add("slot_r", slot_r)
+            self.add("slot_f1", SlotF1Metric(slot_r=slot_r))
+            if _use_domain_metrics(domain, valid_domains):
+                self.add(f"{domain}/slot_r", slot_r)
+                self.add(f"{domain}/slot_f1", SlotF1Metric(slot_r=slot_r))
+
+
+def _use_domain_metrics(
     domain: Optional[str] = None, valid_domains: Optional[Tuple[str]] = None
 ) -> bool:
     if domain is not None:
@@ -21,44 +58,16 @@ def __add_domain_metrics(
     return False
 
 
-class SlotMetrics(Metric):
-    """
-    Helper container which encapsulates standard slot metrics in task-oriented learning (jga, slot_p, slot_r, etc). Due to differences in dialogue representations between tasks, the input is pre-parsed ground truth and predicted slot dictionaries. This class will add domain-specific versions of these metrics as well, when appropriate.
-    """
+class NlgMetrics(Metrics):
+    """Helper container for generation version of standard metrics (F1, BLEU,
+    ...).
 
-    def __init__(
-        teacher_slots={},
-        predicted_slots={},
-        domain: Optional[str] = None,
-        valid_domains: Optional[Tuple[str]] = None,
-    ):
-        self.add("jga", AverageMetric(predictions == slots))
-        for pred_slot_name, pred_value in predicted_slots.items():
-            slot_p = AverageMetric(teacher_slots.get(pred_slot_name) == pred_value)
-            self.add("slot_p", slot_p)
-            self.add("slot_f1", SlotF1Metric(slot_p=slot_p))
-            if __add_domain_metrics(domain, valid_domains):
-                self.add(f"{domain}/slot_p", slot_p)
-                self.add(f"{domain}/slot_f1", SlotF1Metric(slot_p=slot_p))
-
-        for teacher_slot_name, teacher_value in slots.items():
-            slot_r = AverageMetric(
-                predicted_slots.get(teacher_slot_name) == teacher_value
-            )
-            self.add("slot_r", slot_r)
-            self.add("slot_f1", SlotF1Metric(slot_r=slot_r))
-            if __add_domain_metrics(domain, valid_domains):
-                self.add(f"{domain}/slot_r", slot_r)
-                self.add(f"{domain}/slot_f1", SlotF1Metric(slot_r=slot_r))
-
-
-class NlgMetrics(Metric):
-    """
-    Helper container for generation version of standard metrics (F1, BLEU, ...). This class will add domain-specific versions of these classes as well, when appropriate
+    This class will add domain-specific versions of these classes as
+    well, when appropriate
     """
 
     def __init__(
-        teacher: Teacher,
+        self,
         guess: str,
         labels: Optional[Tuple[str]],
         teacher_domains: Optional[Tuple[str]],
@@ -72,9 +81,19 @@ class NlgMetrics(Metric):
             self.add(f"{domain}/nlg_f1", f1)
 
 
+AverageType = Optional[AverageMetric]
+
+
+def _average_type_sum_helper(first: AverageType, second: AverageType) -> AverageType:
+    if first is None:
+        return second
+    if second is None:
+        return first
+    return first + second
+
+
 class SlotF1Metric(Metric):
-    """
-    Metric to keep track of slot F1.
+    """Metric to keep track of slot F1.
 
     Keeps track of slot precision and slot recall as running metrics.
     """
@@ -83,16 +102,11 @@ class SlotF1Metric(Metric):
 
     @property
     def macro_average(self) -> bool:
-        """
-        Indicates whether this metric should be macro-averaged when globally reported.
-        """
+        """Indicates whether this metric should be macro-averaged when globally
+        reported."""
         return True
 
-    def __init__(
-        self,
-        slot_p: Optional[Union[AverageMetric, TScalar]] = None,
-        slot_r: Optional[Union[AverageMetric, TScalar]] = None,
-    ):
+    def __init__(self, slot_p: AverageType = None, slot_r: AverageType = None):
         if not isinstance(slot_p, AverageMetric) and slot_p is not None:
             slot_p = AverageMetric(slot_p)
         if not isinstance(slot_r, AverageMetric) and slot_r is not None:
@@ -105,22 +119,13 @@ class SlotF1Metric(Metric):
         # we drop Python 3.6
         if other is None:
             return self
-        full_slot_p: AverageMetric = (
-            None
-            if self._slot_p is None and other._slot_p is None
-            else self._slot_p + other._slot_p
-        )
-        full_slot_r: AverageMetric = (
-            None
-            if self._slot_r is None and other._slot_r is None
-            else self._slot_r + other._slot_r
-        )
-        # always keep the same return type
-        return type(self)(slot_p=full_slot_p, slot_r=full_slot_r)
+        slot_p = _average_type_sum_helper(self._slot_p, other._slot_p)
+        slot_r = _average_type_sum_helper(self._slot_r, other._slot_r)
+        return type(self)(slot_p=slot_p, slot_r=slot_r)
 
-    def value(self) -> Optional[float]:
+    def value(self) -> float:
         if self._slot_p is None or self._slot_r is None:
-            return None
+            return float("nan")
         else:
             slot_p = self._slot_p.value()
             slot_r = self._slot_r.value()
