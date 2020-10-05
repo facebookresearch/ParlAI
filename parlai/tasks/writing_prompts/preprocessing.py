2274ceb362f401df0d6b22d1c1ff425e77c9ee5a
David Wilmot <david.wilmot@ed.ac.uk>
2020-10-05 16:40:19 +0100

    Simple script to collate the WritingPrompts corpus into a simple jsonl format.

--- parlai/tasks/writing_prompts/preprocessing.py
+++ parlai/tasks/writing_prompts/preprocessing.py
@@ -1,9 +1,11 @@
 import os
+from collections import OrderedDict
 from random import random, shuffle, sample
 
 import fire
 import more_itertools
 from blingfire import text_to_sentences
+from jsonlines import jsonlines
 
 
 def replace_characters(prompt):
@@ -90,6 +92,34 @@ class WritingPrompts(object):
                                             f"\tepisode_done:True\n"
 
                     out_file.write(line_to_write)
+
+    def json(self, prompts_file: str, stories_file: str, output_file: str):
+
+        print(f"Write output to: {output_file}")
+        ensure_dir(output_file)
+
+        print(f"Read prompts from: {prompts_file}")
+        print(f"Read stories from: {stories_file}")
+
+        stories_list = []
+
+        with open(prompts_file, encoding="utf8") as prompts, open(stories_file, encoding="utf8") as stories:
+            for i, (prompt, story) in enumerate(zip(prompts, stories)):
+
+                prompt = prompt.replace("<newline>","\n")
+                story = story.replace("<newline>", "\n")
+
+                story_dict = OrderedDict()
+                story_dict["id"] = i
+                story_dict["title"] = prompt
+                story_dict["text"] = story
+
+                stories_list.append(story_dict)
+
+        with jsonlines.open(output_file, mode='w') as writer:
+
+            for story in stories_list:
+                writer.write(story)
 
 
 if __name__ == '__main__':
