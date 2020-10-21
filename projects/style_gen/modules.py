78a4618b27e870e1ea25619b57c2455ef57520c0
EricMichaelSmith <ems@fb.com>
2020-10-21 16:00:16 -0400

    More cleanup of classifier code

--- projects/style_gen/modules.py
+++ projects/style_gen/modules.py
@@ -7,8 +7,7 @@
 Models and helper classes for style-controlled generation.
 """
 
-import random
-from typing import Optional, List
+from typing import List, Optional
 
 import numpy as np
 import torch
@@ -23,58 +22,12 @@ from parlai.core.agents import Agent
 from parlai.core.message import Message
 from parlai.core.metrics import GlobalAverageMetric
 from parlai.core.opt import Opt
-from parlai.core.torch_agent import History
 from parlai.core.torch_classifier_agent import ConfusionMatrixMetric, WeightedF1Metric
+from parlai.utils.distributed import is_primary_worker
 from parlai.utils.misc import AttrDict, warn_once
 
 
 STYLE_SEP_TOKEN = ' STYLE '
-
-
-class StyleHistoryMixin(History):
-    """
-    Methods for adding style to history.
-    """
-
-    def __init__(self, opt, **kwargs):
-        super().__init__(opt, **kwargs)
-        self.use_style_frac = opt['use_style_frac']
-        self.style = None
-
-    def reset(self):
-        super().reset()
-        self.style = None
-
-    def update_history(self, obs, *args, **kwargs):
-        super().update_history(obs, *args, **kwargs)
-        use_style_rand = random.random()
-        if use_style_rand < self.use_style_frac:
-            # Use the style
-            self.style = obs.get('personality')
-            # This key name is dependent on Image-Chat and will change for other tasks.
-            # If obs does not contain 'personality' (i.e. at the end of an epoch during
-            # validation), there will be no style
-            if self.style == '':
-                self.style = None
-        else:
-            self.style = None
-
-    def get_history_str(self):
-        history_str = super().get_history_str()
-        if history_str is not None and self.style is not None:
-            history_str += STYLE_SEP_TOKEN + self.style
-
-        return history_str
-
-    def get_history_vec(self):
-        history = super().get_history_vec()
-
-        if history is not None and self.style is not None:
-            style = STYLE_SEP_TOKEN + self.style
-            style_tok = self.parse(style)
-            history += style_tok
-
-        return history
 
 
 class StyleAgentMixin:
@@ -97,12 +50,6 @@ class StyleAgentMixin:
             help='What fraction of the time to use the style label',
         )
         return agent
-
-
-class StyleHistory(StyleHistoryMixin, History):
-    """
-    Modify history to save the style.
-    """
 
 
 class ClassifierOnGeneratorModel(TransformerGeneratorModel):
