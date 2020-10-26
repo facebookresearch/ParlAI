46845cc8948ae3a258e83fede87ca6b5eef0a37f
EricMichaelSmith <ems@fb.com>
2020-10-26 10:16:55 -0400

    Stub for new test

---
+++ tests/crowdsourcing/tasks/test_acute_eval.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+"""
+Test the ACUTE-Eval crowdsourcing task.
+"""
+
+import unittest
+
+
+class TestAcuteEval(unittest.TestCase):
+    """
+    Test the ACUTE-Eval crowdsourcing task.
+    """
+
+    def test_base_task(self):
+
+        pass
+        # {{{TODO}}}
+
+
+if __name__ == "__main__":
+    unittest.main()
