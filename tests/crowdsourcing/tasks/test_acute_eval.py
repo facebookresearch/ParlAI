b2b0d63bd35f7d50c6ce4a1d1854c9a2223e4971
EricMichaelSmith <ems@fb.com>
2020-10-26 10:56:11 -0400

    Initial test

--- tests/crowdsourcing/tasks/test_acute_eval.py
+++ tests/crowdsourcing/tasks/test_acute_eval.py
@@ -8,6 +8,23 @@ Test the ACUTE-Eval crowdsourcing task.
 """
 
 import unittest
+from dataclasses import field
+from typing import Any, List
+
+import hydra
+from mephisto.core.hydra_config import register_script_config
+from mephisto.core.operator import Operator
+from mephisto.utils.scripts import load_db_and_process_config
+from omegaconf import DictConfig, OmegaConf
+
+from parlai.crowdsourcing.tasks.acute_eval.run import ScriptConfig, defaults
+
+
+test_defaults = {**defaults, 'mephisto/architect': 'mock', 'mephisto/provider': 'mock'}
+
+
+class TestScriptConfig(ScriptConfig):
+    defaults: List[Any] = field(default_factory=lambda: test_defaults)
 
 
 class TestAcuteEval(unittest.TestCase):
@@ -15,10 +32,19 @@ class TestAcuteEval(unittest.TestCase):
     Test the ACUTE-Eval crowdsourcing task.
     """
 
-    def test_base_task(self):
+    @hydra.main(config_name="test_script_config")
+    def main(self, cfg: DictConfig) -> None:
+        db, cfg = load_db_and_process_config(cfg)
+        operator = Operator(db)
+        operator.validate_and_run_config(run_config=cfg.mephisto, shared_state=None)
+        print(OmegaConf.to_yaml(cfg))
+        print(operator.supervisor.channels[-1].job.architect.server)
+        # TODO: remove 2
+        # {{{TODO: do all per-turn testing and end-state testing}}}
 
-        pass
-        # {{{TODO}}}
+    def test_base_task(self):
+        register_script_config(name='test_script_config', module=TestScriptConfig)
+        self.main()
 
 
 if __name__ == "__main__":
