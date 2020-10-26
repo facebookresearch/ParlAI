a2caa8b74cc5684e41aedbf5cc4b31dff1af90ee
EricMichaelSmith <ems@fb.com>
2020-10-26 17:21:22 -0400

    Work on unit test

--- tests/crowdsourcing/tasks/test_acute_eval.py
+++ tests/crowdsourcing/tasks/test_acute_eval.py
@@ -40,16 +40,55 @@ class TestScriptConfig(ScriptConfig):
 
 @hydra.main(config_path=relative_task_directory, config_name="test_script_config")
 def main(cfg: DictConfig) -> None:
+
+    # Set up the mock server
     db, cfg = load_db_and_process_config(cfg)
     cfg.mephisto.architect.should_run_server = True
     operator = Operator(db)
     operator.validate_and_run_config(run_config=cfg.mephisto, shared_state=None)
-    assert len(operator.supervisor.channels) == 1
-    channel = list(operator.supervisor.channels.keys())[0]
+    sup = operator.supervisor
+    assert len(sup.channels) == 1
+    channel = list(sup.channels.keys())[0]
+    server = sup.channels[channel].job.architect.server
     print(OmegaConf.to_yaml(cfg))
-    print(operator.supervisor.channels[channel].job.architect.server)
-    # TODO: remove print statements
+    # TODO: remove print statement
+
+    # # Handle baseline setup
+    # task_runner = TaskRunnerClass(self.task_run, config, EMPTY_STATE)
+    # sup.register_job(self.architect, task_runner, self.provider)
+    # self.assertEqual(len(sup.channels), 1)
+    # channel_info = list(sup.channels.values())[0]
+    # self.assertIsNotNone(channel_info)
+    # self.assertTrue(channel_info.channel.is_alive())
+    # channel_id = channel_info.channel_id
+    # task_runner = channel_info.job.task_runner
+    # self.assertIsNotNone(channel_id)
+    # self.assertEqual(
+    #     len(self.architect.server.subs),
+    #     1,
+    #     "MockServer doesn't see registered channel",
+    # )
+    # self.assertIsNotNone(
+    #     self.architect.server.last_alive_packet,
+    #     "No alive packet received by server",
+    # )
+    # sup.launch_sending_thread()
+    # self.assertIsNotNone(sup.sending_thread)
+
+    # Create a mock worker agent
+    mock_worker_name = "MOCK_WORKER"
+    server.register_mock_worker(mock_worker_name)
+    workers = db.find_workers(worker_name=mock_worker_name)
+    worker_id = workers[0].db_id
+    mock_agent_details = "FAKE_ASSIGNMENT"
+    server.register_mock_agent(worker_id, mock_agent_details)
+    agent_id = db.find_agents()[0].db_id
+
+    print("SENDING_THREAD: ", sup.sending_thread)
+
     # {{{TODO: do all per-turn testing and end-state testing}}}
+
+    sup.shutdown()
 
 
 class TestAcuteEval(unittest.TestCase):
