e1e7feb89f0e57e1cbbcf6f05aa92a17ee5cf913
klshuster <kshuster@fb.com>
2020-10-27 11:50:48 -0400

    add line back

--- parlai/agents/transformer/modules.py
+++ parlai/agents/transformer/modules.py
@@ -278,6 +278,7 @@ def create_position_codes(n_pos, dim, out):
             for pos in range(n_pos)
         ]
     )
+
     out.detach_()
     out.requires_grad = False
     out[:, 0::2] = torch.FloatTensor(np.sin(position_enc)).type_as(out).detach()
