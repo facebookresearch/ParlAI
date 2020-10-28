5e0ec4401d20b9bce7344f0650a92d8d3a292d03
Stephen Roller <roller@fb.com>
2020-10-28 16:50:44 -0400

    Implement BART incremental decoding (#3231)
    * Implement BART incremental decoding
    
    * Fix lint in random other spot.
    
    * Reviewer comments.

--- parlai/agents/bart/modules.py
+++ parlai/agents/bart/modules.py
@@ -56,7 +56,18 @@ class BartModel(TransformerGeneratorModel):
         """
         Incremental state is weird to handle when we seed decoder with two inputs
         initially.
-
-        We leave as a future exercise.
         """
-        return None
+        # we only have this method called when it's actually being used
+        assert incremental_state is not None
+        assert len(incremental_state) > 0
+
+        for incr_state_l in incremental_state.values():
+            assert 'self_attn' in incr_state_l
+            assert 'prev_mask' in incr_state_l['self_attn']
+            self_attn_mask = incr_state_l['self_attn']['prev_mask']
+            # check this is on the very first run with incremental state
+            if self_attn_mask.ndim == 3 and tuple(self_attn_mask.shape[1:]) == (2, 2):
+                # cut off the inappropriate incremental state
+                incr_state_l['self_attn']['prev_mask'] = self_attn_mask[:, -1:, :]
+
+        return super().reorder_decoder_incremental_state(incremental_state, inds)
