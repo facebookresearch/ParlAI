d494fe5c4084425da510e2e3bb05c2fc61e02495
Stephen Roller <roller@fb.com>
2020-10-29 22:28:42 -0400

    DREAM.

---
+++ parlai/tasks/dream/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from parlai.utils.testing import AutoTeacherTest
+
+
+class TestDefaultTeacher(AutoTeacherTest):
+    task = "dream"
+
+
+class TestDREAMTeacher(AutoTeacherTest):
+    task = "dream:d_r_e_a_m"
