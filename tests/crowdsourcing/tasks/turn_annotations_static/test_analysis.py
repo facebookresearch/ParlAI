85deb35b41a1cdec5ba1358a37688afb71febb73
EricMichaelSmith <ems@fb.com>
2020-11-11 17:48:58 -0500

    Hack together unit test

--- tests/crowdsourcing/tasks/turn_annotations_static/test_analysis.py
+++ tests/crowdsourcing/tasks/turn_annotations_static/test_analysis.py
@@ -7,8 +7,17 @@
 Test components of specific crowdsourcing tasks.
 """
 
+import json
+import os
 import unittest
-from parlai.crowdsourcing.utils.acceptability import AcceptabilityChecker
+
+import pandas as pd
+
+from parlai.crowdsourcing.tasks.turn_annotations_static.run import TASK_DIRECTORY
+from parlai.crowdsourcing.tasks.turn_annotations_static.analysis.compile_results import (
+    TurnAnnotationsStaticResultsCompiler,
+)
+import parlai.utils.testing as testing_utils
 
 
 class TestAnalysis(unittest.TestCase):
@@ -21,106 +30,89 @@ class TestAnalysis(unittest.TestCase):
         Test compiling results on a dummy set of data.
         """
 
-        # TODO: revise below
+        with testing_utils.tempdir() as tmpdir:
 
-        # Define test cases
-        test_cases = [
-            {  # Should pass
-                'messages': [
-                    'Hi - how are you?',
-                    'What? Whatever for?',
-                    'Wow, that sounds like a lot of work.',
-                    "No, I don't expect he would be too happy about that either.",
-                    "I don't even know where you would find that many squirrels.",
-                    'Well, let me know if you need an extra hand.',
-                ],
-                'is_worker_0': False,
-                'expected_violations': '',
-            },
-            {
-                'messages': ['Hi', 'What?', 'Wow', "No", "I don't even know", 'Well,'],
-                'is_worker_0': False,
-                'expected_violations': 'under_min_length',
-            },
-            {  # Should fail, because the first worker shouldn't start with a greeting
-                'messages': [
-                    'Hi - how are you?',
-                    'What? Whatever for?',
-                    'Wow, that sounds like a lot of work.',
-                    "No, I don't expect he would be too happy about that either.",
-                    "I don't even know where you would find that many squirrels.",
-                    'Well, let me know if you need an extra hand.',
-                ],
-                'is_worker_0': True,
-                'expected_violations': 'starts_with_greeting',
-            },
-            {
-                'messages': [
-                    'HEYYYYYYY',
-                    'What? Whatever for?',
-                    'Wow, that sounds like a lot of work.',
-                    "No, I don't expect he would be too happy about that either.",
-                    "I don't even know where you would find that many squirrels.",
-                    'WELLLLL LEMME KNOOOOOO',
-                ],
-                'is_worker_0': False,
-                'expected_violations': 'too_much_all_caps',
-            },
-            {
-                'messages': [
-                    'Hi - how are you?',
-                    'What? Whatever for?',
-                    'Wow, that sounds like a lot of work.',
-                    "No, I don't expect he would be too happy about that either.",
-                    "I don't even know where you would find that many squirrels.",
-                    'Hi - how are you?',
-                ],
-                'is_worker_0': False,
-                'expected_violations': 'exact_match',
-            },
-            {
-                'messages': [
-                    'Hi - how are you?',
-                    'What? Whatever for?',
-                    'Wow, that sounds like a lot of work.',
-                    "No, I don't expect he would be too happy about that either.",
-                    "I don't even know where you would find that many squirrels.",
-                    'Well, let me know if you need an extra hand.',
-                    "I'm gonna say something that's totally XXX!",
-                ],
-                'is_worker_0': False,
-                'expected_violations': 'unsafe:7',
-            },
-        ]
-        test_cases_with_errors = [
-            {
-                'messages': ['Message 1', 'Message 2'],
-                'is_worker_0': True,
-                'violation_types': ['non_existent_violation_type'],
-                'expected_exception': ValueError,
+            # Define desired stdout
+
+            # Paths
+            temp_gold_annotations_path = os.path.join(tmpdir, 'gold_annotations.json')
+
+            # Save a file of gold annotations
+            gold_annotations = {
+                "1_0_5": {
+                    "bucket_0": False,
+                    "bucket_1": False,
+                    "bucket_2": False,
+                    "bucket_3": False,
+                    "bucket_4": False,
+                    "none_all_good": True,
+                },
+                "1_1_5": {
+                    "bucket_0": False,
+                    "bucket_1": False,
+                    "bucket_2": False,
+                    "bucket_3": False,
+                    "bucket_4": True,
+                    "none_all_good": False,
+                },
+                "2_0_5": {
+                    "bucket_0": False,
+                    "bucket_1": True,
+                    "bucket_2": False,
+                    "bucket_3": False,
+                    "bucket_4": False,
+                    "none_all_good": False,
+                },
+                "2_1_5": {
+                    "bucket_0": False,
+                    "bucket_1": False,
+                    "bucket_2": False,
+                    "bucket_3": False,
+                    "bucket_4": True,
+                    "none_all_good": False,
+                },
             }
-        ]
+            with open(temp_gold_annotations_path, 'w') as f:
+                json.dump(gold_annotations, f)
 
-        # Create checker
-        acceptability_checker = AcceptabilityChecker()
+            # Run compilation of results
+            opt = {
+                'results_folders': [
+                    os.path.join(
+                        os.path.dirname(os.path.abspath(__file__)), 'analysis_samples'
+                    )
+                ],
+                'output_folder': tmpdir,
+                'onboarding_in_flight_data_file': os.path.join(
+                    TASK_DIRECTORY, 'task_config/onboarding_in_flight.jsonl'
+                ),
+                'gold_annotations_file': temp_gold_annotations_path,
+            }
+            with testing_utils.capture_output() as output:
+                TurnAnnotationsStaticResultsCompiler(opt).compile_results()
+                actual_stdout = output.getvalue()
 
-        # Run through violation test cases
-        for test_case in test_cases:
-            actual_violations = acceptability_checker.check_messages(
-                messages=test_case['messages'],
-                is_worker_0=test_case['is_worker_0'],
-                violation_types=acceptability_checker.possible_violation_types,
+                # Check the output against what it should be
+                desired_stdout = f"""\
+I AM OBVIOUSLY WRONG\
+"""
+            import pdb
+
+            pdb.set_trace()
+            self.assertEqual(actual_stdout, desired_stdout)
+
+            # Check that the saved results file is what it should be
+            desired_results_path = os.path.join(
+                os.path.dirname(os.path.abspath(__file__)),
+                'analysis_outputs/desired_results.csv',
             )
-            self.assertEqual(actual_violations, test_case['expected_violations'])
+            desired_results = pd.read_csv(desired_results_path)
+            actual_results_path = [obj for obj in os.listdir(tmpdir)][0]
+            actual_results = pd.read_csv(actual_results_path)
+            import pdb
 
-        # Run through test cases that should raise an error
-        for test_case in test_cases_with_errors:
-            with self.assertRaises(test_case['expected_exception']):
-                acceptability_checker.check_messages(
-                    messages=test_case['messages'],
-                    is_worker_0=test_case['is_worker_0'],
-                    violation_types=test_case['violation_types'],
-                )
+            pdb.set_trace()
+            self.assertTrue(actual_results.equals(desired_results))
 
 
 if __name__ == "__main__":
