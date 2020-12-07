1f3bf59d4b5671f491d3c24f62ab6777474aac86
EricMichaelSmith <ems@fb.com>
2020-12-07 10:08:46 -0500

    Revisions

--- tests/crowdsourcing/tasks/test_fast_acute.py
+++ tests/crowdsourcing/tasks/test_fast_acute.py
@@ -13,19 +13,37 @@ import unittest
 
 try:
 
-    # TODO: revise below
-    from parlai.crowdsourcing.tasks.acute_eval.acute_eval_blueprint import (
-        BLUEPRINT_TYPE,
+    from parlai.crowdsourcing.tasks.fast_acute.run import (
+        BLUEPRINT_TYPE as BASE_BLUEPRINT_TYPE,
+    )
+    from parlai.crowdsourcing.tasks.fast_acute.run_q_function import (
+        BLUEPRINT_TYPE as Q_FUNCTION_BLUEPRINT_TYPE,
     )
     from parlai.crowdsourcing.tasks.acute_eval.run import TASK_DIRECTORY
     from parlai.crowdsourcing.utils.tests import AbstractOneTurnCrowdsourcingTest
 
-    class TestAcuteEval(AbstractOneTurnCrowdsourcingTest):
+    class TestFastAcute(AbstractOneTurnCrowdsourcingTest):
         """
-        Test the ACUTE-Eval crowdsourcing task.
+        Test the Fast ACUTE crowdsourcing task.
         """
 
         def test_base_task(self):
+
+            # Set up the config, database, operator, and server
+            # TODO: revise below
+            overrides = [f'mephisto.blueprint.block_on_onboarding_fail={False}']
+            self._set_up_config(
+                blueprint_type=BLUEPRINT_TYPE,
+                task_directory=TASK_DIRECTORY,
+                overrides=overrides,
+            )
+            self._set_up_server()
+
+            # Check that the agent state is as it should be
+            expected_state = {'inputs': DESIRED_INPUTS, 'outputs': DESIRED_OUTPUTS}
+            self._test_agent_state(expected_state=expected_state)
+
+        def test_q_function_task(self):
 
             # Set up the config, database, operator, and server
             overrides = [f'mephisto.blueprint.block_on_onboarding_fail={False}']
