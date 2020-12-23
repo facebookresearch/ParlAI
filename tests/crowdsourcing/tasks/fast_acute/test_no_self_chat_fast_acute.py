baabffce76c28172fb6a9ab19610cce78216dc3f
EricMichaelSmith <ems@fb.com>
2020-12-23 09:30:38 -0500

    Minor

--- tests/crowdsourcing/tasks/fast_acute/test_no_self_chat_fast_acute.py
+++ tests/crowdsourcing/tasks/fast_acute/test_no_self_chat_fast_acute.py
@@ -91,6 +91,7 @@ try:
 
             # Run Fast ACUTEs
             runner = FastAcuteExecutor(self.config)
+            runner.compile_chat_logs()
             runner.set_up_acute_eval()
             self.config.mephisto.blueprint = runner.fast_acute_args
             self._set_up_server()
