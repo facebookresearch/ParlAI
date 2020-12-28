99dcf39ab4e1a323a66190cf5cc682f0bb6ce101
EricMichaelSmith <ems@fb.com>
2020-12-28 15:42:22 -0500

    Revert

--- tests/crowdsourcing/tasks/qa_data_collection/test_qa_data_collection.py
+++ tests/crowdsourcing/tasks/qa_data_collection/test_qa_data_collection.py
@@ -76,23 +76,23 @@ try:
 
             # Set up the operator and server
             teacher = get_teacher(self.config)
-            world_opt = {"turn_timeout": self.config.turn_timeout}
+            world_opt = {"turn_timeout": self.config.turn_timeout, "teacher": teacher}
             shared_state = SharedParlAITaskState(
                 world_opt=world_opt, onboarding_world_opt=world_opt
             )
             self._set_up_server(shared_state=shared_state)
 
-            # # Check that the agent states are as they should be
-            # with open(expected_state_path) as f:
-            #     expected_state = json.load(f)
-            # self._test_agent_states(
-            #     num_agents=1,
-            #     agent_display_ids=AGENT_DISPLAY_IDS,
-            #     agent_messages=AGENT_MESSAGES,
-            #     form_messages=FORM_MESSAGES,
-            #     form_task_data=FORM_TASK_DATA,
-            #     expected_states=(expected_state,),
-            # )
+            # Check that the agent states are as they should be
+            with open(expected_state_path) as f:
+                expected_state = json.load(f)
+            self._test_agent_states(
+                num_agents=1,
+                agent_display_ids=AGENT_DISPLAY_IDS,
+                agent_messages=AGENT_MESSAGES,
+                form_messages=FORM_MESSAGES,
+                form_task_data=FORM_TASK_DATA,
+                expected_states=(expected_state,),
+            )
 
 
 except ImportError:
