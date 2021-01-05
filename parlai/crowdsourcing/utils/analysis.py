48cd4c12a1c7ed4d8d606af779115d8e9b173210
EricMichaelSmith <ems@fb.com>
2021-01-05 17:24:39 -0500

    Note

--- parlai/crowdsourcing/utils/analysis.py
+++ parlai/crowdsourcing/utils/analysis.py
@@ -49,6 +49,8 @@ class AbstractResultsCompiler(ABC):
 
         # Validate problem buckets
         if 'none_all_good' not in self.problem_buckets:
+            # The code relies on a catchall "none" category if the user selects no other
+            # annotation bucket
             raise ValueError(
                 'There must be a "none_all_good" category in self.problem_buckets!'
             )
