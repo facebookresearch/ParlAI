6557515e7fc26caa54e8344175f681947ac37b1b
EricMichaelSmith <ems@fb.com>
2021-01-15 16:17:04 -0500

    Remove else

--- parlai/crowdsourcing/tasks/model_chat/model_chat_blueprint.py
+++ parlai/crowdsourcing/tasks/model_chat/model_chat_blueprint.py
@@ -190,12 +190,11 @@ class BaseModelChatBlueprint(ParlAIChatBlueprint, ABC):
 
         if args.blueprint.get("chat_data_folder") == '':
             raise ValueError('Must provide a valid chat data folder')
-        else:
-            assert '~' not in args.blueprint.chat_data_folder, (
-                f'"~" can\'t currently be parsed in the chat data folder path '
-                f'{args.blueprint.chat_data_folder}'
-            )
-            # TODO: allow ~ to be parsed correctly
+        assert '~' not in args.blueprint.chat_data_folder, (
+            f'"~" can\'t currently be parsed in the chat data folder path '
+            f'{args.blueprint.chat_data_folder}'
+        )
+        # TODO: allow ~ to be parsed correctly
 
         if args.blueprint.get("annotations_config_path", "") != "":
             full_path = os.path.expanduser(args.blueprint.annotations_config_path)
