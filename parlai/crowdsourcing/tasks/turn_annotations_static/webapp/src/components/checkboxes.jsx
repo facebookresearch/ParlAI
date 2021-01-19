9a58ad28aea9818df09e2b3e7963b867e0d6bc3f
Spencer Poff <spencerpoff@gmail.com>
2021-01-19 11:33:24 -0800

    update task_components and checkboxes to use react state

--- parlai/crowdsourcing/tasks/turn_annotations_static/webapp/src/components/checkboxes.jsx
+++ parlai/crowdsourcing/tasks/turn_annotations_static/webapp/src/components/checkboxes.jsx
@@ -8,50 +8,122 @@
 
 import React from "react";
 
-var handleCheckboxChange = function (evt, annotationBuckets, onUserInputUpdate) {
-  var checkboxId = evt.target.id;
-  var whichCheckbox = checkboxId.substring(0, checkboxId.lastIndexOf('_'));
-  var turnIdx = checkboxId.substring(checkboxId.lastIndexOf('_') + 1);
-  var reasonHtml = '';
-
-  if (evt.target.checked) {
-    let checkboxDict = annotationBuckets.config[whichCheckbox];
-    var checkboxPrettyName = ('prettyName' in checkboxDict) ? checkboxDict['prettyName'] : checkboxDict['name'];
-    var checkboxDescription = checkboxDict['description'];
-    reasonHtml = 'You labeled this as <b>' + checkboxPrettyName + '</b>, meaning that: ' + checkboxDescription;
+function CheckboxDescription({
+  checkboxConfig,
+  turnIdx,
+}) {
+  const id = 'checkbox_description_' + turnIdx;
+  const style = {height: '24px'};
+  if (checkboxConfig) {
+    const checkboxPrettyName =
+      checkboxConfig.prettyName ?? checkboxConfig.name;
+    var checkboxDescription = checkboxConfig.description;
+    return (
+      <div id={id} style={style}>
+        You labeled this as <b>{checkboxPrettyName}</b>, meaning that:{' '}
+        {checkboxDescription}
+      </div>
+    );
   }
-  document.getElementById('checkbox_description_' + turnIdx).innerHTML = reasonHtml;
-  if (onUserInputUpdate) {
-    onUserInputUpdate();
-  }
+  return <div id={id} style={style}></div>;
 }
 
-function Checkboxes({ annotationBuckets, turnIdx, onUserInputUpdate, askReason }) {
-  var reasonComponent = (
-    <div>
-      <br></br>
-      <div>
-        <div>Why did you select the checkboxes you did?</div>
-        <input type="text" id={'input_reason_' + turnIdx} style={{ minWidth: '50%' }} />
-      </div>
-    </div>
-  )
-  if (!askReason) {
-    reasonComponent = '';
+// type CheckboxesProps = {
+//   annotationBuckets: AnnotationBuckets,
+//   turnIdx: number,
+//   turnAnswers: TurnAnswers,
+//   onUserInputUpdate?: () => void,
+//   askReason: boolean,
+//   lastSelected: ?string,
+//   setLastSelected: (?string) => void,
+//   setChecked: (number, string, boolean) => void,
+//   setReason: (number, string) => void,
+// };
+
+class Checkboxes extends React.Component {
+  constructor(props) {
+    super(props);
   }
-  let input_type = annotationBuckets.type !== undefined ? annotationBuckets.type : "checkbox";
-  return (
-    <div key={'checkboxes_' + turnIdx}>
-      {
-        Object.keys(annotationBuckets.config).map(c => (
-          <span key={'span_' + c + '_' + turnIdx}><input type={input_type} id={c + '_' + turnIdx} name={'checkbox_group_' + turnIdx} onChange={(evt) => handleCheckboxChange(evt, annotationBuckets, onUserInputUpdate)} /><span style={{ marginRight: '15px' }}>{annotationBuckets.config[c].name}</span>
-          </span>
-        ))
-      }
-      <div id={'checkbox_description_' + turnIdx} style={{ height: '24px' }}></div>
-      {reasonComponent}
-    </div>
-  )
+
+  handleCheckboxChange(evt, onUserInputUpdate) {
+    var checkboxId = evt.target.id;
+    var whichCheckbox = checkboxId.substring(0, checkboxId.lastIndexOf('_'));
+    this.props.setChecked(
+      this.props.turnIdx,
+      whichCheckbox,
+      evt.target.checked,
+    );
+    this.props.setLastSelected(evt.target.checked ? whichCheckbox : null);
+
+    if (onUserInputUpdate) {
+      onUserInputUpdate();
+    }
+  }
+
+  handleReasonChange = evt => {
+    this.props.setReason(this.props.turnIdx, evt.target.value);
+  };
+
+  render() {
+    const {
+      annotationBuckets,
+      turnAnswers,
+      turnIdx,
+      onUserInputUpdate,
+      askReason,
+      lastSelected,
+    } = this.props;
+    var reasonComponent = (
+      <div>
+        <br></br>
+        <div>
+          <div>Why did you select the checkboxes you did?</div>
+          <input
+            type="text"
+            id={'input_reason_' + turnIdx}
+            style={{minWidth: '50%'}}
+            value={turnAnswers.input_reason}
+            onChange={this.handleReasonChange}
+          />
+        </div>
+      </div>
+    );
+    if (!askReason) {
+      reasonComponent = '';
+    }
+    const input_type =
+      annotationBuckets.type !== undefined
+        ? annotationBuckets.type
+        : 'checkbox';
+    return (
+      <div key={'checkboxes_' + turnIdx}>
+        {Object.keys(annotationBuckets.config).map(bucket => {
+          const c = annotationBuckets.config[bucket];
+          return (
+            <span key={'span_' + bucket + '_' + turnIdx}>
+              <input
+                type={input_type}
+                id={bucket + '_' + turnIdx}
+                name={'checkbox_group_' + turnIdx}
+                checked={turnAnswers.buckets[bucket]}
+                onChange={evt =>
+                  this.handleCheckboxChange(evt, onUserInputUpdate)
+                }
+              />
+              <span style={{marginRight: '15px'}}>{c.name}</span>
+            </span>
+          );
+        })}
+        <CheckboxDescription
+          checkboxConfig={
+            lastSelected ? annotationBuckets.config[lastSelected] : null
+          }
+          turnIdx={turnIdx}
+        />
+        {reasonComponent}
+      </div>
+    );
+  }
 }
 
 export { Checkboxes };
--- parlai/crowdsourcing/tasks/turn_annotations_static/webapp/src/components/task_components.jsx
+++ parlai/crowdsourcing/tasks/turn_annotations_static/webapp/src/components/task_components.jsx
@@ -6,28 +6,10 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
- // NOTE: this frontend uses document accessors rather than React to control state,
- // and may not be compatible with some future Mephisto features
-
 import React from "react";
 import { ErrorBoundary } from './error_boundary.jsx';
 import { Checkboxes } from './checkboxes.jsx';
 import { FormControl } from 'react-bootstrap';
-
-// HACK global variable (fix at some point)
-// Array of arrays (by subtask and then by turn)
-window.workerAnswers = [];
-
-var showDisableCssNextButton = function () {
-  // Have to disable the Next button in CSS b/c if React doesn't allow you to do
-  // it in JS Note: person can actually click button even though it doesn't
-  // look like they can! HACK: This is a hack; should tie it to state really
-  document.getElementById('submit-button').style = 'cursor:not-allowed;background-color: #c8c8c8 !important;';
-}
-
-var showEnabledCssNextButton = function () {
-  document.getElementById('submit-button').style = '';
-}
 
 var validateFreetextResponse = function (response, charMin, wordMin, vowelMin) {
   // Requires the response to contain at least charMin characters, wordMin words, and vowelMin vowels.
@@ -39,123 +21,6 @@ var validateFreetextResponse = function (response, charMin, wordMin, vowelMin) {
   } else {
     return false;
   }
-}
-
-var validateUserInput = function (subtaskData) {
-  for (var i = 0; i < subtaskData.length; i++) {
-    if (subtaskData[i].agent_idx == 1) {
-      // only "bot" utterances (could be self-chat) have checkboxes
-      var checkboxes = document.getElementsByName('checkbox_group_' + i);
-      if (checkboxes.length > 0) {
-        var atLeastOneAnswerChecked = false
-        for (var j = 0; j < checkboxes.length; j++) {
-          if (checkboxes[j].checked) {
-            atLeastOneAnswerChecked = true;
-            break;
-          }
-        }
-        if (!atLeastOneAnswerChecked) {
-          return false;
-        }
-      }
-    }
-  }
-  // check response fields
-  var responses = document.getElementsByName('input_response');
-  if (responses.length > 0) {
-    for (var j = 0; j < responses.length; j++) {
-      if (!validateFreetextResponse(responses[j].value, 10, 2, 2)) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
-
-var handleUserInputUpdate = function (subtaskData) {
-  // subtaskData: task data with utterances (both human and bot) 
-  // This checks that at least one checkbox is checked in every turn
-  // before enabling the Next button
-  // Needed b/c conversations scroll off the screen
-  // HACK: need a more elegant way to check if all turns have an answer 
-  if (validateUserInput(subtaskData)) {
-    showEnabledCssNextButton();
-  } else {
-    showDisableCssNextButton();
-  }
-}
-
-var handleSubtaskSubmit = function (subtaskIndex, setIndex, numSubtasks, initialTaskData, annotationBuckets, mephistoSubmit) {
-  // initialTaskData is the initial task data for this index
-  console.log('In handleSubtaskSubmit for subtask: ' + subtaskIndex);
-  if (!validateUserInput(initialTaskData)) {
-    alert('Data does not meet basic quality standards. Cannot be submitted.');
-    return;
-  }
-  var answersForSubtaskIndex = {
-    'subtask_index': subtaskIndex,
-    data: []
-  };
-  for (var i = 0; i < initialTaskData.length; i++) {
-    var answersForTurn = {
-      'turn_idx': i,
-      'text': initialTaskData[i].text,
-      'agent_idx': initialTaskData[i].agent_idx,
-      'other_metadata': initialTaskData[i].other_metadata
-    };
-    if (annotationBuckets !== null) {
-      var buckets = Object.keys(annotationBuckets.config);
-      for (var j = 0; j < buckets.length; j++) {
-        answersForTurn[buckets[j]] = null;
-        var checkbox = document.getElementById(buckets[j] + '_' + i);
-        if (checkbox) {
-          answersForTurn[buckets[j]] = false;
-          if (checkbox.checked) {
-            // Won't have checkboxes for agent_idx != 1
-            answersForTurn[buckets[j]] = true;
-            // uncheck any checked boxes
-            checkbox.checked = false;
-          }
-        }
-      }
-    }
-    var input = document.getElementById('input_reason_' + i);
-    if (input) {
-      answersForTurn['input_reason'] = input.value;
-    }
-    var response = document.getElementById('input_response_' + i);
-    if (response) {
-      answersForTurn['input_response'] = response.value;
-    }
-    answersForSubtaskIndex.data.push(answersForTurn);
-    // Need to also manually clear the reason DIV below checkboxes
-    if (answersForTurn.agent_idx == 1) {
-      var checkbox_i = document.getElementById('checkbox_description_' + i);
-      if (checkbox_i) {
-        checkbox_i.innerHTML = '';
-      }
-      var input_i = document.getElementById('input_reason_' + i);
-      if (input_i) {
-        input_i.value = '';
-      }
-      var response_i = document.getElementById('input_response_' + i);
-      if (response_i) {
-        response_i.value = '';
-      }
-    }
-  }
-  window.workerAnswers.push(answersForSubtaskIndex);
-  document.getElementById('right-pane').scrollTo(0, 0);
-
-  if (subtaskIndex == (numSubtasks - 1)) {
-    if (window.workerAnswers.length != numSubtasks) {
-      alert('Unable to submit task due to malformed data. Please contact requester.');
-      return;
-    }
-    mephistoSubmit(window.workerAnswers);
-  }
-  showDisableCssNextButton();
-  setIndex(subtaskIndex + 1); 
 }
 
 function LeftPane({ stretch = false, children }) {
@@ -184,125 +49,352 @@ function SubtaskCounter({ subtaskIndex, numSubtasks }) {
   }
 }
 
-function SubtaskSubmitButton({ subtaskIndex, numSubtasks, onSubtaskSubmit }) {
+function SubtaskSubmitButton({
+  subtaskIndex,
+  numSubtasks,
+  onSubtaskSubmit,
+  disabled,
+}) {
   return (
     <div style={{ textAlign: 'center' }}>
       <button id="submit-button"
-        style={{ cursor: 'not-allowed', backgroundColor: '#c8c8c8' }}
+        style={
+          disabled ? {cursor: 'not-allowed', backgroundColor: '#c8c8c8'} : {}
+        }
         className="button is-link"
         onClick={onSubtaskSubmit}
+        disabled={disabled}
       >
         {subtaskIndex === (numSubtasks - 1) ? "Submit" : "Next"}
       </button>
     </div>
-  )
+  );
 }
 
-function ChatMessage({ text, agentIdx, annotationQuestion, annotationBuckets, turnIdx, doAnnotateMessage, askReason, responseField, onUserInputUpdate }) {
-  var extraElements = '';
-  var responseInputElement = '';
-  if (doAnnotateMessage) {
-    if (annotationBuckets !== null) { 
-      extraElements = (<span key={'extra_' + turnIdx}><br /><br />
-        <span style={{ fontStyle: 'italic' }} >
-          <span dangerouslySetInnerHTML={{ __html: annotationQuestion }}></span>
-          <br />
-          <Checkboxes turnIdx={turnIdx} annotationBuckets={annotationBuckets} askReason={askReason} onUserInputUpdate={onUserInputUpdate} />
-        </span>
-      </span>)
-    }
-    if (responseField !== null) {
-      responseInputElement = (
-        <FormControl
-        type="text"
-        name="input_response"
-        id={"input_response_" + turnIdx}
-        style={{
-            fontSize: "14px",
-            resize: "none",
-            marginBottom: "40px"
-        }}
-        onChange={(e) => {onUserInputUpdate();}}
-        placeholder={"Please enter your response here"}
-        onPaste={(e) => {e.preventDefault(); alert("Please do not copy and paste. You must manually respond to each message.")}}
-        autoComplete="off"
-      />
-      )
-    }
+class ChatMessage extends React.Component {
+  // description below checkboxes is updated based on last selection
+  state = {
+    lastSelected: null,
+    lastSubtaskIdx: 0,
+  };
+  constructor(props) {
+    super(props);
   }
-  return (
-    <div>
-      <div className={`alert ${agentIdx == 0 ? "alert-info" : "alert-warning"}`} style={{ float: `${agentIdx == 0 ? "right" : "left"}`, display: 'table', minWidth: `${agentIdx == 0 ? "30%" : "80%"}`, marginTop: "auto" }}>
-        <span><b>{turnIdx % 2 == 0 ? 'YOU' : 'THEM'}:</b> {text}
-          <ErrorBoundary>
-            {extraElements}
-          </ErrorBoundary>
+
+  static getDerivedStateFromProps(props, current_state) {
+    // clear description when navigating to next task
+    if (props.subtaskIdx != current_state.lastSubtaskIdx) {
+      return {
+        lastSelected: null,
+        lastSubtaskIdx: props.subtaskIdx,
+      }
+    }
+    return null
+  }
+
+  setLastSelected = (checkbox) => {
+    this.setState({lastSelected: checkbox});
+  }
+
+  setLastSubtaskIdx = (subtaskIdx) => {
+    this.setState({lastSubtaskIdx: subtaskIdx});
+  }
+
+  handleResponseFieldChange = evt => {
+    this.props.setResponse(this.props.turnIdx, evt.target.value);
+  };
+
+  render() {
+    const {
+      annotationQuestion,
+      annotationBuckets,
+      turnIdx,
+      turnAnswers,
+      doAnnotateMessage,
+      askReason,
+      askResponse,
+      setChecked,
+      setReason,
+    } = this.props;
+    const {
+      lastSelected,
+    } = this.state;
+    var extraElements = '';
+    var responseInputElement = '';
+    if (doAnnotateMessage) {
+      if (annotationBuckets !== null) { 
+        extraElements = (
+          <span key={'extra_' + turnIdx}>
+            <br />
+            <br />
+            <span style={{fontStyle: 'italic'}}>
+              {annotationQuestion}
+              <br />
+              <Checkboxes
+                turnIdx={turnIdx}
+                turnAnswers={turnAnswers}
+                annotationBuckets={annotationBuckets}
+                askReason={askReason}
+                lastSelected={lastSelected}
+                setLastSelected={this.setLastSelected}
+                setChecked={setChecked}
+                setReason={setReason}
+              />
+            </span>
+          </span>
+        );
+      }
+      if (askResponse !== null) {
+        responseInputElement = (
+          <FormControl
+          type="text"
+          name="input_response"
+          id={"input_response_" + turnIdx}
+          style={{
+              fontSize: "14px",
+              resize: "none",
+              marginBottom: "40px"
+          }}
+          onChange={this.handleResponseFieldChange}
+          value={turnAnswers.response}
+          placeholder={"Please enter your response here"}
+          onPaste={(e) => {e.preventDefault(); alert("Please do not copy and paste. You must manually respond to each message.")}}
+          autoComplete="off"
+        />
+        )
+      }
+    }
+    const agentIdx = turnAnswers.agent_idx;
+    return (
+      <div
+        className={`alert ${agentIdx == 0 ? 'alert-info' : 'alert-warning'}`}
+        style={{
+          float: `${agentIdx == 0 ? 'right' : 'left'}`,
+          display: 'table',
+          minWidth: `${agentIdx == 0 ? '30%' : '80%'}`,
+          marginTop: `${turnIdx == 1 ? '40px' : 'auto'}`,
+        }}>
+        <span>
+          <b>{turnIdx % 2 == 0 ? 'YOU' : 'THEM'}:</b> {turnAnswers.text}
+          <ErrorBoundary>{extraElements}</ErrorBoundary>
         </span>
       </div>
-      {responseInputElement}
-    </div>
-  )
+    );
+  }
 }
 
-function ContentPane({ subtaskData, taskConfig, subtaskIndex, numSubtasks }) {
+function ContentPane({
+  subtaskData,
+  taskConfig,
+  subtaskIndex,
+  numSubtasks,
+  setChecked,
+  setReason,
+  setResponse,
+}) {
   var annotationQuestion = taskConfig.annotation_question;
   var annotationBuckets = taskConfig.annotation_buckets;
   var askReason = taskConfig.ask_reason;
-  var responseField = taskConfig.response_field;
   if (subtaskData == undefined && subtaskIndex >= numSubtasks) {
     // This happens when index gets set to num subtasks + 1 after submitting
-    return (<div>
-      Thank you for submitting this HIT!
-    </div>);
+    return <div>Thank you for submitting this HIT!</div>;
   }
-
   return (
     <div>
-      {subtaskData.map(
-        (m, idx) =>
-          <div key={idx}>
-            <div>
-              <ChatMessage
-                text={m.text}
-                agentIdx={m.agent_idx}
-                turnIdx={idx}
-                annotationQuestion={annotationQuestion}
-                annotationBuckets={annotationBuckets}
-                doAnnotateMessage={m.do_annotate}
-                askReason={askReason}
-                responseField={responseField}
-                onUserInputUpdate={() => handleUserInputUpdate(subtaskData)}
-              />
-            </div>
+      {subtaskData.map((m, idx) => (
+        <div key={idx}>
+          <div>
+            <ChatMessage
+              subtaskIdx={subtaskIndex}
+              turnIdx={idx}
+              turnAnswers={m}
+              annotationQuestion={annotationQuestion}
+              annotationBuckets={annotationBuckets}
+              doAnnotateMessage={m.do_annotate}
+              askReason={askReason}
+              askResponse={taskConfig.response_field}
+              setChecked={setChecked}
+              setReason={setReason}
+              setResponse={setResponse}
+            />
           </div>
-      )}
-    </div>
-  )
-}
-
-function MainTaskComponent({ taskData, taskTitle, taskDescription, taskConfig, onSubmit }) {
-  if (taskData == undefined) {
-    return <div><p> Loading chats...</p></div>;
-  }
-  const [index, setIndex] = React.useState(0);
-
-  return (
-    <div style={{ margin: 0, padding: 0, height: '100%' }}>
-      <LeftPane>
-        <h4><span dangerouslySetInnerHTML={{ __html: taskTitle || 'Task Title Loading' }}></span></h4>
-        <SubtaskCounter subtaskIndex={index} numSubtasks={taskData.length}></SubtaskCounter>
-
-        <br />
-        <span dangerouslySetInnerHTML={{ __html: taskDescription || 'Task Description Loading' }}></span>
-        <SubtaskSubmitButton subtaskIndex={index} numSubtasks={taskData.length} onSubtaskSubmit={() => { handleSubtaskSubmit(index, setIndex, taskData.length, taskData[index], taskConfig.annotation_buckets, onSubmit); }} initialTaskData={taskData[index]}></SubtaskSubmitButton>
-      </LeftPane>
-      <RightPane>
-        <ContentPane subtaskData={taskData[index]} taskConfig={taskConfig} subtaskIndex={index} numSubtasks={taskData.length} ></ContentPane>
-      </RightPane>
-      <div style={{ clear: 'both' }}>
-      </div>
+        </div>
+      ))}
     </div>
   );
+}
+
+// type Buckets = {
+//   [bucket: string]: ?boolean,
+// };
+
+// type TurnAnswers = {
+//   text: string,
+//   agent_idx: number,
+//   other_metadata: any,
+//   buckets: Buckets,
+//   do_annotate: boolean,
+//   input_reason: ?string,
+//   response: ?string,
+// };
+
+// type SubtaskAnswers = Array<TurnAnswers>;
+// type WorkerAnswers = Array<SubtaskAnswers>;
+
+// type MainTaskComponentProps = {
+//   taskData: TaskData,
+//   taskTitle: string,
+//   taskDescription: string,
+//   taskConfig: TaskConfig,
+//   onSubmit: WorkerAnswers => void,
+// };
+// type MainTaskComponentState = {
+//   index: number,
+//   answers: WorkerAnswers,
+// };
+
+class MainTaskComponent extends React.Component {
+  state = {
+    index: 0,
+    answers: this._initAnswersFromTaskData(),
+  };
+  constructor(props) {
+    super(props);
+  }
+
+  _initAnswersFromTaskData() {
+    var answers = [];
+    for (var i = 0; i < this.props.taskData.length; i++) {
+      var answersForSubtaskIndex = [];
+      const subTaskData = this.props.taskData[i];
+      for (var j = 0; j < subTaskData.length; j++) {
+        var answersForTurn = {
+          text: subTaskData[j].text,
+          agent_idx: subTaskData[j].agent_idx,
+          other_metadata: subTaskData[j].other_metadata,
+          buckets: {},
+          do_annotate: subTaskData[j].do_annotate,
+          input_reason: null,
+          response: null,
+        };
+        Object.keys(this.props.taskConfig.annotation_buckets.config).forEach(
+          b => {
+            answersForTurn.buckets[b] = null;
+          },
+        );
+        answersForSubtaskIndex[j] = answersForTurn;
+      }
+      answers[i] = answersForSubtaskIndex;
+    }
+    return answers;
+  }
+
+  validateUserInput() {
+    console.log('Validating user input');
+    // if task is over, user should not be able to submit anything else
+    if (this.state.index >= this.state.answers.length) {
+      console.log('Task is over: ' + this.state.answers.length + ' vs ' + this.state.index);
+      return false;
+    }
+    // check that all turns have answers
+    const subtaskData = this.state.answers[this.state.index];
+    for (var i = 0; i < subtaskData.length; i++) {
+      const uttAnswer = subtaskData[i];
+      if (uttAnswer.agent_idx == 1 && uttAnswer.do_annotate) {
+        // only "bot" utterances (could be self-chat) have checkboxes
+        var atLeastOneAnswerChecked = false;
+        Object.keys(uttAnswer.buckets).forEach(b => {
+          atLeastOneAnswerChecked =
+            atLeastOneAnswerChecked || uttAnswer.buckets[b];
+        });
+        if (!atLeastOneAnswerChecked) {
+          console.log('No answers checked');
+          return false;
+        }
+      }
+    }
+    // check response fields
+    var responses = document.getElementsByName('input_response');
+    if (responses.length > 0) {
+      for (var j = 0; j < responses.length; j++) {
+        if (!validateFreetextResponse(responses[j].value, 10, 2, 2)) {
+          return false;
+        }
+      }
+    }
+    console.log('Input valid!');
+    return true;
+  }
+
+  handleSubtaskSubmit = () => {
+    const subtaskIndex = this.state.index;
+    console.log('In handleSubtaskSubmit for subtask: ' + subtaskIndex);
+    if (!this.validateUserInput()) {
+      alert('Data does not meet basic quality standards. Cannot be submitted.');
+      return;
+    }
+
+    if (subtaskIndex == this.props.taskData.length - 1) {
+      this.props.onSubmit(this.state.answers);
+    }
+    this.setState(prevState => ({index: prevState.index + 1}));
+  };
+
+  setChecked = (turnIdx, bucket, value) => {
+    var answers = this.state.answers;
+    answers[this.state.index][turnIdx].buckets[bucket] = value;
+    this.setState({answers});
+  };
+
+  setReason = (turnIdx, reason) => {
+    var answers = this.state.answers;
+    answers[this.state.index][turnIdx].input_reason = reason;
+    this.setState({answers});
+  };
+
+  setResponse = (turnIdx, response) => {
+    var answers = this.state.answers;
+    answers[this.state.index][turnIdx].response = response;
+    this.setState({answers});
+  }
+
+  render() {
+    const {taskData, taskTitle, taskDescription, taskConfig} = this.props;
+    const {index, answers} = this.state;
+    if (taskData == undefined) {
+      return <div><p> Loading chats...</p></div>;
+    }
+    const task_title = taskTitle || 'Task Title Loading';
+    const task_description = taskDescription || 'Task Description Loading';
+    return (
+      <div style={{ margin: 0, padding: 0, height: '100%' }}>
+        <LeftPane>
+          <h4>{task_title}</h4>
+          <SubtaskCounter subtaskIndex={index} numSubtasks={taskData.length}></SubtaskCounter>
+
+          <br />
+          <span dangerouslySetInnerHTML={{ __html: task_description}}></span>
+          <SubtaskSubmitButton 
+            subtaskIndex={index} 
+            numSubtasks={taskData.length} 
+            onSubtaskSubmit={this.handleSubtaskSubmit}
+            disabled={!this.validateUserInput()} />
+        </LeftPane>
+        <RightPane>
+          <ContentPane 
+            subtaskData={answers[index]}
+            taskConfig={taskConfig}
+            subtaskIndex={index}
+            numSubtasks={taskData.length}
+            setChecked={this.setChecked}
+            setReason={this.setReason}
+            setResponse={this.setResponse} />
+        </RightPane>
+        <div style={{ clear: 'both' }}>
+        </div>
+      </div>
+    );
+  }
 }
 
 export { MainTaskComponent };
