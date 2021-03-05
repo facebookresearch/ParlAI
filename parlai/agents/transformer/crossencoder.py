a7b0ffb1a047f855762d4e76acd7b8bffc7202f0
spencerp <spencerpoff@gmail.com>
2021-03-04 21:29:32 -0500

    Pass Opt to TransformerEncoder/Decoder (#3486)
    * Pass opt to transformer encoder and decoder instead of each arg individually
    
    * apply to all TransfomerEncoder/Decoder call sites
    
    * fix some more call sites
    
    * more call sites
    
    * update opt descriptions
    
    * fixing some things that were lost in a merge
    
    * being more cautious of default values
    
    * kurt and jason's comments

--- parlai/agents/transformer/crossencoder.py
+++ parlai/agents/transformer/crossencoder.py
@@ -8,7 +8,6 @@ from typing import Optional
 from parlai.core.params import ParlaiParser
 from parlai.core.opt import Opt
 from .modules import TransformerEncoder
-from .modules import get_n_positions_from_options
 from parlai.core.torch_ranker_agent import TorchRankerAgent
 from .transformer import TransformerRankerAgent
 from parlai.utils.torch import concat_without_padding
@@ -85,30 +84,17 @@ class CrossEncoderModule(torch.nn.Module):
 
     def __init__(self, opt, dict, null_idx):
         super(CrossEncoderModule, self).__init__()
-        n_positions = get_n_positions_from_options(opt)
         embeddings = torch.nn.Embedding(
             len(dict), opt['embedding_size'], padding_idx=null_idx
         )
         torch.nn.init.normal_(embeddings.weight, 0, opt['embedding_size'] ** -0.5)
         self.encoder = TransformerEncoder(
-            n_heads=opt['n_heads'],
-            n_layers=opt['n_layers'],
-            embedding_size=opt['embedding_size'],
-            ffn_size=opt['ffn_size'],
-            vocabulary_size=len(dict),
+            opt=opt,
             embedding=embeddings,
-            dropout=opt['dropout'],
-            attention_dropout=opt['attention_dropout'],
-            relu_dropout=opt['relu_dropout'],
+            vocabulary_size=len(dict),
             padding_idx=null_idx,
-            learn_positional_embeddings=opt['learn_positional_embeddings'],
-            embeddings_scale=opt['embeddings_scale'],
             reduction_type=opt.get('reduction_type', 'first'),
-            n_positions=n_positions,
             n_segments=2,
-            activation=opt['activation'],
-            variant=opt['variant'],
-            output_scaling=opt['output_scaling'],
         )
         self.linear_layer = torch.nn.Linear(opt['embedding_size'], 1)
 
