7171d7cda65f0ddb851035754a2fbad3efb30d96
spencerp <spencerpoff@gmail.com>
2021-03-23 14:00:48 -0400

    Breaking transformer modules.py into smaller files (#3466)
    * Separating out components of transformer into their own files
    
    * move the rest of transformer modules out of modules.py
    
    * remove transformer modules.py
    
    * resolving imports
    
    * move functions into modules, fix imports
    
    * make a constants.py
    
    * noqa comment placement 🙄

---
+++ parlai/agents/transformer/modules/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .constants import LAYER_NORM_EPS  # noqa: F401
+from .functions import (  # noqa: F401
+    create_embeddings,
+    create_position_codes,
+    get_n_positions_from_options,
+)
+from .attention import BasicAttention, MultiHeadAttention  # noqa: F401
+from .ffn import TransformerFFN  # noqa: F401
+from .encoder import TransformerEncoder, TransformerEncoderLayer  # noqa: F401
+from .decoder import TransformerDecoder, TransformerDecoderLayer  # noqa: F401
+from .generator import TransformerGeneratorModel  # noqa: F401
+from .wrappers import TransformerLinearWrapper, TransformerResponseWrapper  # noqa: F401
+from .mem_net import TransformerMemNetModel  # noqa: F401
