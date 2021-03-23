82ae161dcba7882dfa4c1fdb7092f413432a882e
meganung <meganu@fb.com>
2021-03-23 21:28:28 +0000

    huggingface task for glue dataset support

---
+++ parlai/tasks/huggingface/agents.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from parlai.core.teachers import DialogTeacher
+from parlai.core.params import ParlaiParser
+from parlai.core.opt import Opt
+from typing import Optional
+import os
+
+# huggingface imports
+from datasets import load_dataset, concatenate_datasets
+
+
+class HuggingFaceTeacher(DialogTeacher):
+    def __init__(self, opt, shared=None):
+        self.datatype = opt['datatype']
+        self.data_path = HuggingFaceTeacher._path(opt)
+        opt['datafile'] = self.data_path
+
+        # load dataset from HuggingFace
+        self.dataset = load_dataset(
+            path=opt['hf_path'],
+            name=opt['hf_name'],
+            cache_dir=self.data_path,
+            split=opt['hf_split'],
+        )
+
+        self.id = "huggingface"
+        super().__init__(opt, shared)
+
+    @classmethod
+    def add_cmdline_args(
+        cls, parser: ParlaiParser, partial_opt: Optional[Opt] = None
+    ) -> ParlaiParser:
+        super().add_cmdline_args(parser, partial_opt)
+        parser = parser.add_argument_group('Hugging Face Teacher Arguments')
+        parser.add_argument(
+            '-hfp',
+            '--hf_path',
+            type=str,
+            default='',
+            required=True,
+            help="HuggingFace dataset identifier name",
+        )
+        parser.add_argument(
+            '-hfn',
+            '--hf_name',
+            type=str,
+            default=None,
+            help="defining the name of the HuggingFace dataset configuration",
+        )
+        parser.add_argument(
+            '-hfs',
+            '--hf_split',
+            type=str,
+            default=None,
+            help="which split of the HuggingFace dataset to load",
+        )
+        return parser
+
+    def _path(opt):
+        path = os.path.join(opt['datapath'], 'huggingface')
+        return path
+
+    def setup_data(self, path):
+        for row in self.dataset:
+            premise = row.get('premise', '')
+            hypothesis = row.get('hypothesis', '')
+            sentence = row.get('sentence', '')
+            text = sentence + premise + hypothesis
+            label = str(row['label'])
+
+            # specific to glue
+            candidates = ['0', '1', '2']
+
+            yield (text, [label], None, candidates), True
+
+
+class DefaultTeacher(HuggingFaceTeacher):
+    pass
