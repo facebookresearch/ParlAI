5f3665a27ac2c598b22285cb01fec0a54ae7d43f
Stephen Roller <roller@fb.com>
2021-03-30 17:15:12 -0400

    Exception for test

--- tests/test_code.py
+++ tests/test_code.py
@@ -22,7 +22,14 @@ class TestInit(unittest.TestCase):
 
     def test_init_everywhere(self):
         for folder_path in testing_utils.git_ls_dirs('parlai'):
-            excluded_folders = ['conf', 'frontend', 'mturk', 'task_config', 'webapp']
+            excluded_folders = [
+                'conf',
+                'frontend',
+                'mturk',
+                'task_config',
+                'webapp',
+                'opt_presets',
+            ]
             # conf: contains YAML files for Hydra
             # frontend, mturk, webapp: contains frontend code for crowdsourcing tasks
             # task_config: contains JSONs, HTML files, etc. for MTurk/Mephisto tasks
