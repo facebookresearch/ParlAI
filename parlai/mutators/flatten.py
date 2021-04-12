14f3ea689b1e5bcdc5834f8fa2a57bc6d38c7acc
Mojtaba <komeili@fb.com>
2021-04-12 08:50:19 -0700

    PR review comments

--- parlai/mutators/flatten.py
+++ parlai/mutators/flatten.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import random
 from typing import List
 from parlai.core.message import Message
 from parlai.core.mutators import ManyEpisodeMutator, register_mutator
@@ -23,5 +24,5 @@ class FlattenMutator(ManyEpisodeMutator):
         for message in episode:
             history.append(message.pop('text'))
             message['text'] = '\n'.join(history)
-            history.extend(message['labels'])
             yield [message]
+            history.append(random.choice(message['labels']))
