2af972881189c0b1f9d8cab107f363715e2183d9
Spencer Poff <spencerpoff@gmail.com>
2021-04-12 20:17:22 -0700

    tests

--- tests/test_self_chat.py
+++ tests/test_self_chat.py
@@ -4,8 +4,11 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from tempfile import NamedTemporaryFile
 import unittest
+
 from parlai.scripts.self_chat import SelfChat
+import parlai.utils.testing as testing_utils
 
 
 class TestSelfChat(unittest.TestCase):
@@ -30,3 +33,19 @@ class TestSelfChat(unittest.TestCase):
 
         with self.assertRaises(RuntimeError):
             DisplayData.main(task='self_chat')
+
+    def test_seed_messages_from_file(self):
+        with testing_utils.capture_output() as output:
+            with NamedTemporaryFile() as tmpfile:
+                tmpfile.write(b'howdy\nunique message')
+                tmpfile.seek(0)
+                SelfChat.main(
+                    model='fixed_response',
+                    fixed_response='hi',
+                    seed_messages_from_file=tmpfile.name,
+                    num_self_chats=10,
+                    selfchat_max_turns=2,
+                )
+            output = output.getvalue()
+        assert 'howdy' in output
+        assert 'unique message' in output
