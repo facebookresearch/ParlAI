25596383b469b35ed887bebeee8a027c7a3fb689
Mojtaba <komeili@fb.com>
2021-04-15 19:26:58 -0700

    removed splitting version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open('requirements.txt') as f:
     reqs = []
     for line in f:
         line = line.strip()
-        reqs.append(line.split('==')[0])
+        reqs.append(line)
 
 
 if __name__ == '__main__':
