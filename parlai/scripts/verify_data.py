1dc3c6c3cf93cdaa7c54bbb54c5126e4421efe88
Spencer Poff <spencerpoff@gmail.com>
2021-04-19 22:00:22 -0700

    add duplicate checking to verify_data

--- parlai/scripts/verify_data.py
+++ parlai/scripts/verify_data.py
@@ -13,11 +13,16 @@ candidates.
 parlai verify_data --task convai2 --datatype valid
 ```
 """
+from collections import Counter
+import copy
+from typing import Iterable, List, Tuple
+
 from parlai.agents.repeat_label.repeat_label import RepeatLabelAgent
-from parlai.core.message import Message
+from parlai.core.message import History, Message
+from parlai.core.opt import Opt
 from parlai.core.params import ParlaiParser
 from parlai.utils.misc import TimeLogger, warn_once
-from parlai.core.worlds import create_task
+from parlai.core.worlds import World, create_task
 from parlai.core.script import ParlaiScript, register_script
 import parlai.utils.logging as logging
 
@@ -28,6 +33,24 @@ def setup_args(parser=None):
     # Get command line arguments
     parser.add_argument('-ltim', '--log-every-n-secs', type=float, default=2)
     parser.add_argument('-d', '--display-examples', type='bool', default=False)
+    parser.add_argument(
+        '--verify-task',
+        type=bool,
+        default=False,
+        help=(
+            'In addition to verifying the specified --datatype, also verifies the '
+            'task holistically (looks for issues across train, valid, and test).'
+        ),
+    )
+    parser.add_argument(
+        '--datatypes',
+        type=str,
+        default='train,valid,test',
+        help=(
+            'Comma separated list of datatype splits to consider in holistic verification. '
+            'Only applicable if --verify-task True.'
+        ),
+    )
     parser.set_defaults(datatype='train:stream:ordered')
     return parser
 
@@ -55,14 +78,12 @@ def warn(txt, act, opt):
         warn_once(txt)
 
 
-def verify(opt):
+def verify_split(opt):
     if opt['datatype'] == 'train':
         logging.warning("changing datatype from train to train:ordered")
         opt['datatype'] = 'train:ordered'
     opt.log()
-    # create repeat label agent and assign it to the specified task
-    agent = RepeatLabelAgent(opt)
-    world = create_task(opt, agent)
+    world = task_world(opt)
 
     log_every_n_secs = opt.get('log_every_n_secs', -1)
     if log_every_n_secs <= 0:
@@ -77,12 +98,7 @@ def verify(opt):
     counts['label_candidates_with_missing_label'] = 0
     counts['did_not_return_message'] = 0
 
-    # Show some example dialogs.
-    while not world.epoch_done():
-        world.parley()
-
-        act = world.acts[0]
-
+    for act, *_ in task_world_iterator(world):
         if not isinstance(act, Message):
             counts['did_not_return_message'] += 1
 
@@ -122,22 +138,81 @@ def verify(opt):
             text, log = report(world, counts, log_time)
             print(text)
 
-    try:
-        # print dataset size if available
-        logging.info(
-            f'Loaded {world.num_episodes()} episodes with a '
-            f'total of {world.num_examples()} examples'
-        )
-    except AttributeError:
-        pass
-
     counts['exs'] = int(world.report()['exs'])
     return counts
 
 
+def task_world(opt: Opt) -> World:
+    agent = RepeatLabelAgent(opt)
+    world = create_task(opt, agent)
+    try:
+        # print dataset size if available
+        logging.info(
+            f'Loaded {world.num_episodes()} episodes with a '
+            f'total of {world.num_examples()} examples.'
+        )
+    except AttributeError:
+        pass
+    return world
+
+
+def task_world_iterator(world: World) -> Iterable[Tuple[Message, Message, bool]]:
+    while not world.epoch_done():
+        world.parley()
+        acts = world.get_acts()
+        assert len(acts) == 2
+        yield acts[0], acts[1], world.episode_done()
+
+
+def _count_conversations(opt: Opt) -> Counter:
+    convo_counter = Counter()
+    history = History(opt)
+    world = task_world(opt)
+    for *acts, episode_done in task_world_iterator(world):
+        for act in acts:
+            history.update_history(act)
+        if episode_done:
+            convo_counter[str(history)] += 1
+            history.reset()
+    return convo_counter
+
+
+def verify_task(opt: Opt) -> None:
+    opt = copy.deepcopy(opt)
+
+    def _dupes(counter: Counter) -> List[str]:
+        dupes = []
+        for convo, cnt in counter.most_common():
+            if cnt > 1:
+                dupes.append(convo)
+            else:
+                break
+        return dupes
+
+    convo_counts = Counter()
+    for dt in opt['datatypes'].split(','):
+        if dt == 'train':
+            logging.info('changing datatype from train to train:ordered')
+            dt = 'train:ordered'
+        opt['datatype'] = dt
+        split_convo_counts = _count_conversations(opt)
+        split_convo_dupes = _dupes(split_convo_counts)
+        log_func = logging.warn if len(split_convo_dupes) > 0 else logging.info
+        log_func(f"{len(split_convo_dupes)} duplicate conversations found in {dt}.")
+        convo_counts += split_convo_counts
+
+    convo_dupes = _dupes(convo_counts)
+    log_func = logging.warn if len(convo_dupes) > 0 else logging.info
+    log_func(
+        f"Found {len(convo_dupes)} duplicate conversations across train/valid/test splits."
+    )
+
+
 def verify_data(opt):
-    counts = verify(opt)
+    counts = verify_split(opt)
     print(counts)
+    if opt['verify_task']:
+        verify_task(opt)
     return counts
 
 
