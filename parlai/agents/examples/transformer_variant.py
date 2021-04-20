c4b6924b9c54d42295bd0b59032b4b002e54cfdb
Spencer Poff <spencerpoff@gmail.com>
2021-04-20 09:38:53 -0700

    add another example

--- parlai/agents/examples/transformer_variant.py
+++ parlai/agents/examples/transformer_variant.py
@@ -13,9 +13,13 @@ TransformerVariantAgent:
 VerboseTransformerAgent:
 - Doesn't swap out anything
 - Fully-specifies all components, for illustration
+
+ConfigurableTransformerAgent:
+- Swaps out implementations based on command line args
 """
 from __future__ import annotations
 import torch
+from enum import Enum
 from typing import Optional, Tuple, Union
 
 from parlai.agents.transformer.modules import (
@@ -26,9 +30,18 @@ from parlai.agents.transformer.modules import (
     TransformerEncoder,
     TransformerEncoderLayer,
     TransformerGeneratorModel,
+    decoder,
 )
 from parlai.agents.transformer.modules.interfaces import ModularComponentBuilder
 from parlai.agents.transformer.transformer import TransformerGeneratorAgent
+from parlai.core.opt import Opt
+from parlai.core.params import ParlaiParser
+import parlai.utils.logging as logging
+
+
+###########################################
+# Transformer With Two Components Swapped #
+###########################################
 
 
 class TransformerVariantAgent(TransformerGeneratorAgent):
@@ -83,9 +96,19 @@ class MyCustomAttention(MultiHeadAttention):
     pass
 
 
+#######################################
+# Fully-specified Default Transformer #
+#######################################
+
+
 class VerboseTransformerAgent(TransformerGeneratorAgent):
+    """
+    Doesn't make any changes to TransformerGeneratorModel, just
+    specifies all subcomponents explicitly as a refernce of what's
+    swappable.
+    """
+
     def build_model(self, states=None):
-        # Using a custom encoder and a custom decoder self attention
         transformer_template = TransformerGeneratorModel.Template(
             encoder=ModularComponentBuilder(
                 TransformerEncoder,
@@ -112,6 +135,72 @@ class VerboseTransformerAgent(TransformerGeneratorAgent):
                     )
                 ),
             ),
+        )
+        return TransformerGeneratorModel(
+            opt=self.opt, dictionary=self.dict, template=transformer_template
+        )
+
+
+################################################
+# Command-line Configurable Custom Transformer #
+################################################
+
+
+class DecoderFeedForwardVariant(Enum):
+    ONE = 'one'
+    TWO = 'two'
+
+
+class DecoderFFNOne(TransformerFFN):
+    logging.info("Using Decoder FFN Variant One")
+
+
+class DecoderFFNTwo(TransformerFFN):
+    logging.info("Using Decoder FFN Variant Two")
+
+
+class ConfigurableTransformerAgent(TransformerGeneratorAgent):
+    """
+    Illustrates swapping out components based on command line args.
+    Specifically, swaps out the decoder ffn between two options.
+    """
+
+    @classmethod
+    def add_cmdline_args(
+        cls, parser: ParlaiParser, partial_opt: Optional[Opt] = None
+    ) -> ParlaiParser:
+        super().add_cmdline_args(parser, partial_opt=partial_opt)
+        agent = parser.add_argument_group('MyCustom Transformer Arguments')
+        parser.add_argument(
+            '--decoder-ffn-variants',
+            type=DecoderFeedForwardVariant,
+            default=DecoderFeedForwardVariant.ONE,
+            help='Some variants in the decoder FFN implementation',
+        )
+        return agent
+
+    def build_model(self, states=None):
+        decoder_variant: DecoderFeedForwardVariant = self.opt['decoder_ffn_variants']
+        if decoder_variant == DecoderFeedForwardVariant.ONE:
+            decoder_ffn_class = DecoderFFNOne
+        elif decoder_variant == DecoderFeedForwardVariant.TWO:
+            decoder_ffn_class = DecoderFFNTwo
+        else:
+            logging.error(
+                'Invalid --decoder-ffn-variants option, defaulting to original ffn implementation.'
+            )
+            decoder_ffn_class = TransformerFFN
+
+        transformer_template = TransformerGeneratorModel.Template(
+            decoder=ModularComponentBuilder(
+                TransformerDecoder,
+                TransformerDecoder.Template(
+                    layer=ModularComponentBuilder(
+                        TransformerDecoderLayer,
+                        TransformerDecoderLayer.Template(feedforward=decoder_ffn_class),
+                    )
+                ),
+            )
         )
         return TransformerGeneratorModel(
             opt=self.opt, dictionary=self.dict, template=transformer_template
