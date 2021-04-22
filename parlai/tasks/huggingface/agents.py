c3cc2c4755030727922aa8425c074e00681f0ede
meganung <meganu@fb.com>
2021-04-22 17:19:40 +0000

    add properties to abstract class and methods

--- parlai/tasks/huggingface/agents.py
+++ parlai/tasks/huggingface/agents.py
@@ -6,11 +6,19 @@
 
 from parlai.core.teachers import DialogTeacher
 from parlai.utils.data import DatatypeHelper
-from typing import List
+from typing import Any, Dict, Iterable, List, Optional, Tuple
+from typing_extensions import TypedDict
 from abc import ABC
+import os
 
 # huggingface imports
 from datasets import load_dataset
+
+
+class SplitsMappingDict(TypedDict):
+    train: str
+    valid: str
+    test: str
 
 
 class AbstractHuggingFaceTeacher(DialogTeacher, ABC):
@@ -27,53 +35,107 @@ class AbstractHuggingFaceTeacher(DialogTeacher, ABC):
     render_text_field = bool where if True, will include the text field name in the query (e.g. "sentence: <sentence>")
     """
 
-    hf_path: str = 'glue'
-    hf_name: str = 'cola'
-    hf_text_fields: List[str] = ['sentence']
-    hf_label_field: str = 'label'
-    hf_splits_mapping: dict = {'train': 'train', 'valid': 'validation', 'test': 'test'}
-    render_text_field: bool = False
+    hf_path: str
+    hf_name: Optional[str]
+    hf_text_fields: List[str]
+    hf_label_field: str
+    hf_splits_mapping: SplitsMappingDict
 
     def __init__(self, opt, shared=None):
         self.fold = DatatypeHelper.fold(opt['datatype'])
         self.hf_split = self.hf_splits_mapping[self.fold]
-        opt['datafile'] = self.hf_split
+        if self.hf_name:
+            opt['datafile'] = os.path.join(
+                opt['datapath'], 'huggingface', self.hf_path, self.hf_name, self.fold
+            )
+        else:
+            opt['datafile'] = os.path.join(
+                opt['datapath'], 'huggingface', self.hf_path, self.fold
+            )
 
         self.id = "huggingface"
         super().__init__(opt, shared)
 
-    def setup_data(self, split):
+    @property
+    def hf_path(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def hf_name(self) -> Optional[str]:
+        return None
+
+    @property
+    def hf_text_fields(self) -> List[str]:
+        raise NotImplementedError
+
+    @property
+    def hf_label_field(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def hf_splits_mapping(self) -> SplitsMappingDict:
+        raise NotImplementedError
+
+    def setup_data(self, path: str) -> Iterable[tuple]:
         """
         Default implementation of setup_data.
 
         Manually override if needed.
         """
-        # load dataset from HuggingFace
-        dataset = load_dataset(path=self.hf_path, name=self.hf_name, split=split)
 
-        pre_candidates = dataset.features[self.hf_label_field].names
-        for row in dataset:
+        def _get_text_value(row) -> Tuple[str, Dict[str, str]]:
+            """
+            return the constructed text query and dict mapping text field names to
+            values.
+            """
             # construct text query from the hf_text_fields specified
-            text_arr = []
+            text_dict = {}
             for col in self.hf_text_fields:
                 text_part = row.get(col)
                 if text_part is None:
                     raise KeyError(f'Feature "{col}" not found in data.')
-                if self.render_text_field:
-                    text_part = col + ': ' + text_part
-                text_arr.append(text_part)
-            query = '\n'.join(text_arr)
+                text_dict[col] = text_part
+            return '\n'.join(text_dict.values()), text_dict
 
+        def _get_label_value(row):
+            return row[self.hf_label_field]
+
+        def _get_label_candidates(row, label) -> str:
+            pre_candidates = dataset.features[self.hf_label_field].names
             # construct label and candidates
-            label = row[self.hf_label_field]
             if type(label) is int:
-                label = pre_candidates[label]
-                candidates = pre_candidates
+                return pre_candidates[label], pre_candidates
             if label in row:
-                label = row[label]
-                candidates = [row[l] for l in pre_candidates]
-            yield (query, [label], None, candidates), True
+                return row[label], [row[l] for l in pre_candidates]
+            return label, pre_candidates
+
+        # load dataset from HuggingFace
+        dataset = load_dataset(
+            path=self.hf_path, name=self.hf_name, split=self.hf_split
+        )
+
+        for row in dataset:
+            query, text_dict = _get_text_value(row)
+            label = _get_label_value(row)
+            label, candidates = _get_label_candidates(row, label)
+
+            episode_dict = text_dict
+            episode_dict['text'] = query
+            episode_dict['label'] = label
+            episode_dict['label_candidates'] = candidates
+            yield episode_dict, True
+
+
+class GlueColaTeacher(AbstractHuggingFaceTeacher):
+    hf_path = 'glue'
+    hf_name = 'cola'
+    hf_text_fields = ['sentence']
+    hf_label_field = 'label'
+    hf_splits_mapping = {'train': 'train', 'valid': 'validation', 'test': 'test'}
 
 
 class DefaultTeacher(AbstractHuggingFaceTeacher):
-    pass
+    def __init__():
+        raise NotImplementedError(
+            "There is no default teacher for HuggingFace datasets. Please use a specific one."
+        )
