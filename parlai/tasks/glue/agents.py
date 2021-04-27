1a30f8621bb7d681deaea24deda693f5aed882c1
meganung <meganu@fb.com>
2021-04-27 16:32:05 +0000

    add glue tasks and their hf configs

--- parlai/tasks/glue/agents.py
+++ parlai/tasks/glue/agents.py
@@ -7,10 +7,86 @@
 from parlai.tasks.huggingface.agents import AbstractHuggingFaceTeacher
 
 
+class AxTeacher(AbstractHuggingFaceTeacher):
+    hf_path = 'glue'
+    hf_name = 'ax'
+    hf_text_fields = ['premise', 'hypothesis']
+    hf_label_field = 'label'
+    hf_splits_mapping = {'train': 'test', 'valid': 'test', 'test': 'test'}
+
+
 class ColaTeacher(AbstractHuggingFaceTeacher):
     hf_path = 'glue'
     hf_name = 'cola'
     hf_text_fields = ['sentence']
+    hf_label_field = 'label'
+    hf_splits_mapping = {'train': 'train', 'valid': 'validation', 'test': 'test'}
+
+
+class MnliTeacher(AbstractHuggingFaceTeacher):
+    hf_path = 'glue'
+    hf_name = 'mnli'
+    hf_text_fields = ['premise', 'hypothesis']
+    hf_label_field = 'label'
+    hf_splits_mapping = {
+        'train': 'train',
+        'valid': 'validation_matched',
+        'test': 'test_matched',
+    }
+
+
+class MrpcTeacher(AbstractHuggingFaceTeacher):
+    hf_path = 'glue'
+    hf_name = 'mrpc'
+    hf_text_fields = ['sentence1', 'sentence2']
+    hf_label_field = 'label'
+    hf_splits_mapping = {'train': 'train', 'valid': 'validation', 'test': 'test'}
+
+
+class QnliTeacher(AbstractHuggingFaceTeacher):
+    hf_path = 'glue'
+    hf_name = 'qnli'
+    hf_text_fields = ['sentence', 'question']
+    hf_label_field = 'label'
+    hf_splits_mapping = {'train': 'train', 'valid': 'validation', 'test': 'test'}
+
+
+class QqpTeacher(AbstractHuggingFaceTeacher):
+    hf_path = 'glue'
+    hf_name = 'qqp'
+    hf_text_fields = ['question1', 'question2']
+    hf_label_field = 'label'
+    hf_splits_mapping = {'train': 'train', 'valid': 'validation', 'test': 'test'}
+
+
+class RteTeacher(AbstractHuggingFaceTeacher):
+    hf_path = 'glue'
+    hf_name = 'rte'
+    hf_text_fields = ['sentence1', 'sentence2']
+    hf_label_field = 'label'
+    hf_splits_mapping = {'train': 'train', 'valid': 'validation', 'test': 'test'}
+
+
+class Sst2Teacher(AbstractHuggingFaceTeacher):
+    hf_path = 'glue'
+    hf_name = 'sst2'
+    hf_text_fields = ['sentence']
+    hf_label_field = 'label'
+    hf_splits_mapping = {'train': 'train', 'valid': 'validation', 'test': 'test'}
+
+
+class StsbTeacher(AbstractHuggingFaceTeacher):
+    hf_path = 'glue'
+    hf_name = 'stsb'
+    hf_text_fields = ['sentence1', 'sentence2']
+    hf_label_field = 'label'
+    hf_splits_mapping = {'train': 'train', 'valid': 'validation', 'test': 'test'}
+
+
+class WnliTeacher(AbstractHuggingFaceTeacher):
+    hf_path = 'glue'
+    hf_name = 'wnli'
+    hf_text_fields = ['sentence1', 'sentence2']
     hf_label_field = 'label'
     hf_splits_mapping = {'train': 'train', 'valid': 'validation', 'test': 'test'}
 
