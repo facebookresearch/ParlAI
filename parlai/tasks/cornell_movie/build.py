11be647127b50f73a1e4c65c05d43d115183e07c
Stephen Roller <roller@fb.com>
2021-04-28 08:45:51 -0400

    Fix cornell movies

--- parlai/tasks/cornell_movie/build.py
+++ parlai/tasks/cornell_movie/build.py
@@ -20,55 +20,11 @@ RESOURCES = [
 ]
 
 
-def create_fb_format(lines_file, convo_file, outpath):
-    print('[building fbformat]')
-    with PathManager.open(
-        os.path.join(outpath, 'train.txt'), 'w'
-    ) as ftrain, PathManager.open(
-        os.path.join(outpath, 'valid.txt'), 'w'
-    ) as fvalid, PathManager.open(
-        os.path.join(outpath, 'test.txt'), 'w'
-    ) as ftest:
-        lines = {}
-
-        codecs.register_error('strict', codecs.ignore_errors)
-        with codecs.open(lines_file, 'r') as f:
-            for line in f:
-                l = line.split(' +++$+++ ')
-                lines[l[0]] = ' '.join(l[4:]).strip('\n').replace('\t', ' ')
-
-        cnt = 0
-        with codecs.open(convo_file, 'r') as f:
-            for line in f:
-                l = line.split(' ')
-                convo = ' '.join(l[6:]).strip('\n').strip('[').strip(']')
-                c = convo.replace("'", '').replace(' ', '').split(',')
-
-                # forward conversation
-                s = ''
-                index = 0
-                for i in range(0, len(c), 2):
-                    index += 1
-                    s += str(index) + ' ' + lines[c[i]]
-                    if len(c) > i + 1:
-                        s += '\t' + lines[c[i + 1]]
-                    s += '\n'
-
-                cnt = cnt + 1
-                handle = ftrain
-                if (cnt % 10) == 0:
-                    handle = ftest
-                if (cnt % 10) == 1:
-                    handle = fvalid
-                handle.write(s + '\n')
-
-
 def build(opt):
     dpath = os.path.join(opt['datapath'], 'CornellMovie')
-    version = None
+    version = 'v1.01'
 
     if not build_data.built(dpath, version_string=version):
-        print('[building data: ' + dpath + ']')
         if build_data.built(dpath):
             # An older version exists, so remove these outdated files.
             build_data.remove_dir(dpath)
@@ -77,13 +33,6 @@ def build(opt):
         # Download the data.
         for downloadable_file in RESOURCES:
             downloadable_file.download_file(dpath)
-
-        dpext = os.path.join(dpath, 'cornell movie-dialogs corpus')
-        create_fb_format(
-            os.path.join(dpext, 'movie_lines.txt'),
-            os.path.join(dpext, 'movie_conversations.txt'),
-            dpath,
-        )
 
         # Mark the data as built.
         build_data.mark_done(dpath, version_string=version)
