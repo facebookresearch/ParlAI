36004c9c61750f49a6f4ba5b48e22588b82301f1
Wenxuan (Wendy) Zhang <wzhang4343@fb.com>
2021-07-07 17:16:12 -0700

    AUC metrics (#3751)
    * added rates
    
    * added AUC and updated rates slightly (not sure if we'll be using them at the end, but need to add tests
    
    * linting
    
    * fixed a bug
    
    * Still needs to be updated, but will deal with this later....
    
    * only dependency on sklearn.metrics should be auc, but still need to be tested for accuracy
    
    * cleaned up/deleted unnecessary statements
    
    * might have messed sth up
    
    * last commit before adding tests & changing the structure so that it is recorded at the very end
    
    * found a bug
    
    * added 3 tests + a couple small changes to torch classifier agent (have not updated to only calculate at the end yet
    
    * fixed a  small thing that would affect testing metrics
    
    * fixed things so the tests pass... but also don't know why it likes the fpr and tpr reversed???
    
    * passed all current auc tests.... but I will add more later for checking
    
    * added a hopefully faster merge way
    
    * finished testing inside test_metrics... now gonna work on the actual side
    
    * changed some stuff so it only is run on eval_model, but there are some weirdness going on with auc
    
    * testing snapshot
    
    * removed extra prints
    
    * fixed something
    
    * removed an extra commit
    
    * fixed another thing....
    
    * another bug
    
    * removed slots and added better class comment
    
    * linting
    
    * why was linting not added before...
    
    * final commit hopefully
    
    * oops forgot to update the tests
    
    * fixed some max_bucket_dec_places problems
    
    * updated sort and counter typing
    
    * fixed another typing
    
    * fixed an import thing
    
    * fixed a small typing thing
    
    * added comments + changed -auc in eval_model
    
    * added comment about classifier agent
    
    * linted
    
    * fixed optional
    
    * added to documentation
    
    * fixed comments + counter import
    
    * fixed wording :)

--- parlai/scripts/eval_model.py
+++ parlai/scripts/eval_model.py
@@ -42,6 +42,9 @@ from parlai.utils.distributed import (
     get_rank,
 )
 
+# the index to access classifier agent's output in the world
+CLASSIFIER_AGENT = 1
+
 
 def setup_args(parser=None):
     if parser is None:
@@ -68,6 +71,21 @@ def setup_args(parser=None):
         type=str,
         default='conversations',
         choices=['conversations', 'parlai'],
+    )
+    parser.add_argument(
+        '--area-under-curve-digits',
+        '-auc',
+        type=int,
+        default=-1,
+        help='a positive number indicates to calculate the area under the roc curve and it also determines how many decimal digits of the predictions to keep (higher numbers->more precise); also used to determine whether or not to calculate the AUC metric',
+    )
+    parser.add_argument(
+        '--area-under-curve-class',
+        '-auclass',
+        type=str,
+        default=None,
+        nargs='*',
+        help='the name(s) of the class to calculate the auc for',
     )
     parser.add_argument('-ne', '--num-examples', type=int, default=-1)
     parser.add_argument('-d', '--display-examples', type='bool', default=False)
@@ -182,8 +200,18 @@ def _eval_single_world(opt, agent, task):
         world_logger.write(outfile, world, file_format=opt['save_format'])
 
     report = aggregate_unnamed_reports(all_gather_list(world.report()))
-    world.reset()
 
+    if isinstance(world.agents, list) and len(world.agents) > 1:
+        classifier_agent = world.agents[CLASSIFIER_AGENT]
+        if hasattr(classifier_agent, 'calc_auc') and classifier_agent.calc_auc:
+            for class_indices, curr_auc in zip(
+                classifier_agent.auc_class_indices, classifier_agent.aucs
+            ):
+                report[f'AUC_{classifier_agent.class_list[class_indices]}'] = curr_auc
+            classifier_agent.reset_auc()
+            # for safety measures
+            agent.reset_auc()
+    world.reset()
     return report
 
 
