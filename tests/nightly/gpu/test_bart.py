f45487e5b0a13e58bf68580952de2f9220e72abc
klshuster <kshuster@fb.com>
2021-07-22 10:33:24 -0400

    less memory for tests?

--- tests/nightly/gpu/test_bart.py
+++ tests/nightly/gpu/test_bart.py
@@ -24,7 +24,7 @@ class TestBartModel(unittest.TestCase):
         Test out-of-the-box BART on repeat task.
         """
         valid, _ = testing_utils.eval_model(
-            dict(task='integration_tests', model='bart')
+            dict(task='integration_tests', model='bart', num_examples=10)
         )
         self.assertAlmostEqual(valid['ppl'].value(), 1.0, places=1)
 
@@ -77,12 +77,13 @@ class TestBartModel(unittest.TestCase):
                     dict_file='zoo:bart/bart_large/model.dict',
                     optimizer='adam',
                     learningrate=3e-5,
-                    batchsize=4,
+                    batchsize=2,
                     num_epochs=1,
                     short_final_eval=True,
                     validation_max_exs=12,
                     model_file=mf,
                     model_parallel=True,
+                    fp16=True,
                 )
             )
             self.assertAlmostEqual(valid['ppl'].value(), 1.0, places=1)
