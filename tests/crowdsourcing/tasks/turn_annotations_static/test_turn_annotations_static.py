541c793377d9a35f70d4ec6b15fb1f88fffefbd8
meganung <meganu@fb.com>
2021-10-12 11:18:08 -0700

    onboarding qualification arg

--- tests/crowdsourcing/tasks/turn_annotations_static/test_turn_annotations_static.py
+++ tests/crowdsourcing/tasks/turn_annotations_static/test_turn_annotations_static.py
@@ -139,11 +139,19 @@ try:
 
             # Set up the config and database
             overrides += [
+<<<<<<< HEAD
                 '+mephisto.blueprint.annotation_last_only=False',
                 '+mephisto.blueprint.conversation_count=null',
                 'mephisto.blueprint.onboarding_qualification=test_turn_annotations',
                 '+mephisto.blueprint.random_seed=42',
                 'mephisto.task.assignment_duration_in_seconds=1800',
+=======
+                '++mephisto.blueprint.annotation_last_only=False',
+                '++mephisto.blueprint.conversation_count=null',
+                '++mephisto.blueprint.onboarding_qualification=test_turn_annoataions',
+                '++mephisto.blueprint.random_seed=42',
+                '++mephisto.task.assignment_duration_in_seconds=1800',
+>>>>>>> f15a1640 (onboarding qualification arg)
             ]
             # TODO: remove all of these params once Hydra 1.1 is released with support
             #  for recursive defaults
