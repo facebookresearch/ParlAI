e686a02ab745388265ca653983321a90a58d68d4
Eric Smith <EricMichaelSmith@users.noreply.github.com>
2022-01-04 09:45:46 -0500

    Pull out settings for model chat onboarding (#4274)
    * Layer changes
    
    * Documentation

--- parlai/crowdsourcing/tasks/model_chat/worlds.py
+++ parlai/crowdsourcing/tasks/model_chat/worlds.py
@@ -19,7 +19,6 @@ from parlai.crowdsourcing.utils.acceptability import AcceptabilityChecker
 from parlai.crowdsourcing.utils.worlds import CrowdOnboardWorld, CrowdTaskWorld
 from parlai.crowdsourcing.tasks.model_chat.bot_agent import TurkLikeAgent
 from parlai.crowdsourcing.tasks.model_chat.constants import (
-    ONBOARD_CONFIG,
     ONBOARD_FAIL,
     ONBOARD_SUCCESS,
 )
@@ -50,8 +49,6 @@ class ModelChatOnboardWorld(CrowdOnboardWorld):
 
         self.skip_onboarding = opt['skip_onboarding']
 
-        self.min_correct = ONBOARD_CONFIG['min_correct']
-        self.max_incorrect = ONBOARD_CONFIG['max_incorrect']
         self.onboard_task_data = opt['onboard_task_data']
         self.status = 'DISCONNECT'
         self.onboard_statistics = opt['onboard_statistics']
@@ -59,49 +56,7 @@ class ModelChatOnboardWorld(CrowdOnboardWorld):
         self.max_onboard_time = opt['max_onboard_time']
         self.onboarding_qualification = opt['onboarding_qualification']
         self.worker_id = get_mturk_id_from_mephisto_wrapper(self.agent)
-
-    def has_same_answer(self, ans1, ans2):
-        if len(ans1) != len(ans2):
-            return False
-
-        ans1_sort = sorted(ans1)
-        ans2_sort = sorted(ans2)
-
-        for x in range(len(ans1_sort)):
-            if ans1_sort[x] != ans2_sort[x]:
-                return False
-        return True
-
-    def check_onboarding_answers(self, worker_answers) -> bool:
-        """
-        Calculate how many correct answers the user gave.
-
-        `worker_answers` is a list of dicts containing mappings between an annotation
-        value and whether it was selected for each bucket. We return a boolean as to
-        whether the worker passed or failed the task.
-        """
-        given_turns = self.onboard_task_data['dialog']
-        correct_answers = [t[1]['answers'] for t in given_turns]
-        number_correct = 0
-        number_incorrect = 0
-        for worker_answer, correct_answer in zip(worker_answers, correct_answers):
-            worker_only_selected = [
-                key for key, selected in worker_answer.items() if selected
-            ]
-            if self.has_same_answer(worker_only_selected, correct_answer):
-                number_correct += 1
-            else:
-                number_incorrect += 1
-
-        print(
-            f'Worker {self.worker_id} got {number_correct} annotations correct and {number_incorrect} incorrect in onboarding.'
-        )
-        if (
-            number_correct >= self.min_correct
-            and number_incorrect <= self.max_incorrect
-        ):
-            return True
-        return False
+        self.annotations = None
 
     def parley(self):
 
@@ -140,10 +95,11 @@ class ModelChatOnboardWorld(CrowdOnboardWorld):
             print(f'{self.__class__.__name__}: {self.worker_id} had no data submitted')
             return ONBOARD_FAIL
 
-        worker_answers = act['task_data']['annotations']
+        self.annotations = act['task_data'].get('annotations')
+        print('Onboarding annotation results: ', self.annotations)
 
-        if self.check_onboarding_answers(worker_answers):
-            print(f'Worker {self.worker_id} successfully passed the onboard task.')
+        if act['task_data']['success']:
+            print(f'Worker {self.worker_id} successfully passed the onboarding task.')
 
             # This will end the onboarding and send them directly to the HIT
             self.episodeDone = True
@@ -163,6 +119,9 @@ class ModelChatOnboardWorld(CrowdOnboardWorld):
                 if self.status not in self.onboard_statistics:
                     self.onboard_statistics[self.status] = 0
                 self.onboard_statistics[self.status] += 1
+
+    def get_custom_task_data(self):
+        return self.annotations
 
 
 class BaseModelChatWorld(CrowdTaskWorld, ABC):
