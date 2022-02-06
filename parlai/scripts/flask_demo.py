0fd475d9e1cdb0c38874353bd80b5b5ada5e1976
Khushal Jethava <khushaljethwa14@gmail.com>
2022-02-06 14:13:16 +0530

    updated flask_demo.py

--- parlai/scripts/flask_demo.py
+++ parlai/scripts/flask_demo.py
@@ -4,21 +4,16 @@ from parlai.core.agents import create_agent_from_model_file
 
 app = Flask(__name__)
 
-def Model_init():
-  #import model from the model file can be pretrained or fine tuned
-  blender_agent = create_agent_from_model_file("zoo:blender/blender_90M/model")
-  return blender_agent
 
-#blender_agent = index()
 @app.route("/response", methods=["GET","POST"])
 def chatbot_response():
     data = request.json
     blender_agent.observe({'text': data["UserText"], 'episode_done': False})
     response = blender_agent.act()
-    return {"response" : response['text']} 
+    return {"response" : response['text']}
 
 
 # main driver function
 if __name__ == "__main__":
-  blender_agent = Model_init()
-  app.run()
+    blender_agent = create_agent_from_model_file("zoo:blender/blender_90M/model")
+    app.run()
