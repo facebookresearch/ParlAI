7be772e7a3a75df0586e4729987d9343d46f16e2
Spencer Poff <spencerpoff@gmail.com>
2022-02-08 11:34:09 -0800

    resize token embeddings and unit test

--- parlai/agents/transformer/decoder_only.py
+++ parlai/agents/transformer/decoder_only.py
@@ -10,6 +10,8 @@ from parlai.agents.transformer.transformer import add_common_cmdline_args
 from parlai.core.opt import Opt
 from parlai.core.params import ParlaiParser
 from parlai.core.torch_generator_agent import TorchGeneratorAgent
+from parlai.utils.logging import logging
+from parlai.utils.misc import recursive_getattr
 from parlai.utils.torch import padded_tensor
 
 from .modules import (
@@ -64,3 +66,27 @@ class DecoderOnlyAgent(TorchGeneratorAgent):
             return padded_tensor(
                 items, pad_idx=self.NULL_IDX, left_padded=True, fp16friendly=False
             )
+
+    def _resize_token_embeddings(self, state_dict, msg=None):
+        """
+        Resize the token embeddings when adding extra special tokens.
+        """
+        # map extra special tokens carefully
+        new_size = self.model.embeddings.weight.size()[0]
+        orig_size = state_dict['embeddings.weight'].size()[0]
+        logging.info(f'Resizing token embeddings from {orig_size} to {new_size}')
+        if new_size <= orig_size:
+            # new size should be greater than original size,
+            # as we are adding special tokens
+            raise RuntimeError(msg)
+
+        for emb_weights in ['embeddings.weight', 'decoder.embeddings.weight']:
+            # get new_embs
+            old_embs = state_dict[emb_weights]
+            new_embs = recursive_getattr(self.model, emb_weights).to(old_embs.device)
+            # copy over old weights
+            new_embs.data[:orig_size, :] = old_embs.data[:orig_size, :]
+            # reset in state dict
+            state_dict[emb_weights] = new_embs
+
+        return state_dict
