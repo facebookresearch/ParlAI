75e64c504afafad409454dcaaa6be5e995536faa
Stephen Roller <roller@fb.com>
2022-03-23 12:28:33 -0400

    Break up some tests.

--- tests/nightly/gpu/test_dialogue_bias.py
+++ tests/nightly/gpu/test_dialogue_bias.py
@@ -15,37 +15,41 @@ from parlai.core.opt import Opt
 
 
 class TestDialogueBias(unittest.TestCase):
-    def test_perplexities(self):
-        """
-        Test perplexities of reduced-bias models in the zoo.
-        """
-        test_cases = [
-            ('gender__name_scrambling', 'transformer/generator', 22.91),
-            (
-                'gender__ctrl_gen_tokens',
-                'projects.dialogue_bias.agents:NoBiasStyleGenAgent',
-                22.61,
-            ),
-            ('gender__unlikelihood_sequence_level', 'transformer/generator', 11.44),
-            ('gender_ethnicity__name_scrambling', 'transformer/generator', 19.57),
-        ]
-        # Perplexities are high because models were tuned on conversations starting with
-        # "Hi! My name is ___."
-        for model_name, model, desired_ppl in test_cases:
-            _, test = testing_utils.eval_model(
-                opt={
-                    'batchsize': 4,
-                    'beam_block_full_context': True,
-                    'fp16': True,
-                    'num_examples': 16,
-                    'model': model,
-                    'model_file': f'zoo:dialogue_bias/{model_name}/model',
-                    'skip_generation': True,
-                    'task': 'blended_skill_talk',
-                },
-                skip_valid=True,
-            )
-            self.assertAlmostEqual(test['ppl'], desired_ppl, delta=0.05)
+    def _test_perplexity(self, model_name, model, desired_ppl):
+        _, test = testing_utils.eval_model(
+            opt={
+                'batchsize': 4,
+                'beam_block_full_context': True,
+                'fp16': True,
+                'num_examples': 16,
+                'model': model,
+                'model_file': f'zoo:dialogue_bias/{model_name}/model',
+                'skip_generation': True,
+                'task': 'blended_skill_talk',
+            },
+            skip_valid=True,
+        )
+        self.assertAlmostEqual(test['ppl'], desired_ppl, delta=0.05)
+
+    def test_gender__name_scrambling(self):
+        self._test_perplexity('gender__name_scrambling', 'transformer/generator', 22.91)
+
+    def test_gender__ctrl_gen_tokens(self):
+        self._test_perplexity(
+            'gender__ctrl_gen_tokens',
+            'projects.dialogue_bias.agents:NoBiasStyleGenAgent',
+            22.61,
+        )
+
+    def test_gender__unlikelihood_sequence_leve(self):
+        self._test_perplexity(
+            'gender__unlikelihood_sequence_level', 'transformer/generator', 11.44
+        )
+
+    def test_gender__ethnicity__name_scrambling(self):
+        self._test_perplexity(
+            'gender_ethnicity__name_scrambling', 'transformer/generator', 19.57
+        )
 
 
 if __name__ == '__main__':
