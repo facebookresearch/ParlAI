618f44d04846caab5f1747c8b07aebc8477234aa
klshuster <kshuster@fb.com>
2022-03-29 13:58:00 -0400

    sample test

--- parlai/tasks/natural_questions/test.py
+++ parlai/tasks/natural_questions/test.py
@@ -7,8 +7,22 @@
 from parlai.utils.testing import AutoTeacherTest  # noqa: F401
 
 
-class TestNaturalQuestionsTeacher(AutoTeacherTest):
+class NQAutoTeacherTest(AutoTeacherTest):
+    def test_train_stream_ordered(self, data_regression):
+        """
+        Test --datatype train:stream:ordered.
+        """
+        pass
+
+
+class TestNaturalQuestionsTeacher(NQAutoTeacherTest):
     task = 'natural_questions'  # replace with your teacher name
+
+
+class TestNaturalQuestionsSampleTeacher(AutoTeacherTest):
+    task = (
+        'natural_questions:NaturalQuestionsSampleTeacher'
+    )  # replace with your teacher name
 
 
 class TestNaturalQuestionsOpenTeacher(AutoTeacherTest):
