911468be4b592eba3c83d5b6f4be00bfb25621b9
klshuster <kshuster@fb.com>
2022-03-29 15:17:08 -0400

    remove normal teachers

--- parlai/tasks/natural_questions/test.py
+++ parlai/tasks/natural_questions/test.py
@@ -4,19 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from parlai.utils.testing import AutoTeacherTest  # noqa: F401
-
-
-class NQAutoTeacherTest(AutoTeacherTest):
-    def test_train_stream_ordered(self, data_regression):
-        """
-        Ignore train set because it's so large.
-        """
-        pass
-
-
-class TestNaturalQuestionsTeacher(NQAutoTeacherTest):
-    task = 'natural_questions'
+from parlai.utils.testing import AutoTeacherTest
 
 
 class TestNaturalQuestionsSampleTeacher(AutoTeacherTest):
