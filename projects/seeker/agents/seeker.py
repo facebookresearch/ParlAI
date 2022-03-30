42e577339e86d80182af5c6db55ae7e731aad8aa
Kurt Shuster <kshuster@fb.com>
2022-03-30 10:18:48 -0400

    [SeeKeR] Opt construction for Python 3.8 (#4458)
    * opt construction for py38
    
    * max

--- projects/seeker/agents/seeker.py
+++ projects/seeker/agents/seeker.py
@@ -201,7 +201,7 @@ class SeekerAgent(Agent):
         """
         additional_agent_parser = cls.get_additional_agent_args()
         for action in additional_agent_parser._actions:
-            key = action.option_strings[-1]
+            key = max(action.option_strings, key=lambda x: len(x))
             type = action.type
 
             for prefix in ['krm', 'drm', 'sqm', 'sdm']:
