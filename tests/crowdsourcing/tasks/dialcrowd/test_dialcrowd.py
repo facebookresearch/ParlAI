f1e83125f4c47787bbb5dd00827d207855938014
Eric Smith <EricMichaelSmith@users.noreply.github.com>
2022-04-28 19:20:40 -0400

    Revert

--- tests/crowdsourcing/tasks/dialcrowd/test_dialcrowd.py
+++ tests/crowdsourcing/tasks/dialcrowd/test_dialcrowd.py
@@ -23,56 +23,58 @@ try:
     from parlai.crowdsourcing.tasks.dialcrowd.run import TASK_DIRECTORY
     from parlai.crowdsourcing.utils.frontend import build_task
     from parlai.crowdsourcing.utils.tests import AbstractOneTurnCrowdsourcingTest
+
+    class TestDialCrowd(AbstractOneTurnCrowdsourcingTest):
+        """
+        Test the DialCrowd tasks.
+        """
+
+        @pytest.fixture(scope="function")
+        def setup_teardown(self):
+            """
+            Call code to set up and tear down tests.
+            """
+            self._setup()
+            yield self.operator
+            # All code after this will be run upon teardown
+            self._teardown()
+
+        def test_dialcrowd(
+            self, setup_teardown, data_regression: DataRegressionFixture
+        ):
+            """
+            Test DialCrowd.
+            """
+
+            self.operator = setup_teardown
+
+            overrides = [
+                f'mephisto.blueprint.data_jsonl={TASK_CONFIG_FOLDER}/sample_data.jsonl'
+            ]
+
+            task_data_path = os.path.join(TASK_DATA_FOLDER, 'data.json')
+
+            # # Load the .json of the task data
+            with open(task_data_path) as f:
+                task_data = json.load(f)
+
+            # # Setup
+
+            build_task(task_directory=TASK_DIRECTORY)
+
+            self._set_up_config(
+                task_directory=TASK_DIRECTORY,
+                overrides=overrides,
+                config_name='example',
+            )
+
+            # Set up the operator and server
+            self._set_up_server()
+
+            self._test_agent_state(task_data=task_data, data_regression=data_regression)
+
 except ImportError:
     pass
-
-
-class TestDialCrowd(AbstractOneTurnCrowdsourcingTest):
-    """
-    Test the DialCrowd tasks.
-    """
-
-    @pytest.fixture(scope="function")
-    def setup_teardown(self):
-        """
-        Call code to set up and tear down tests.
-        """
-        self._setup()
-        yield self.operator
-        # All code after this will be run upon teardown
-        self._teardown()
-
-    def test_dialcrowd(self, setup_teardown, data_regression: DataRegressionFixture):
-        """
-        Test DialCrowd.
-        """
-
-        self.operator = setup_teardown
-
-        overrides = [
-            f'mephisto.blueprint.data_jsonl={TASK_CONFIG_FOLDER}/sample_data.jsonl'
-        ]
-
-        task_data_path = os.path.join(TASK_DATA_FOLDER, 'data.json')
-
-        # # Load the .json of the task data
-        with open(task_data_path) as f:
-            task_data = json.load(f)
-
-        # # Setup
-
-        build_task(task_directory=TASK_DIRECTORY)
-
-        self._set_up_config(
-            task_directory=TASK_DIRECTORY,
-            overrides=overrides,
-            config_name='example',
-        )
-
-        # Set up the operator and server
-        self._set_up_server()
-
-        self._test_agent_state(task_data=task_data, data_regression=data_regression)
 
 
 if __name__ == "__main__":
