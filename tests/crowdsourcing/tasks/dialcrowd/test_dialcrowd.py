1b3458199aaced75b102b8c2e86bcfbd08e5494b
Eric Smith <EricMichaelSmith@users.noreply.github.com>
2022-04-28 19:00:42 -0400

    Remove space entirely

--- tests/crowdsourcing/tasks/dialcrowd/test_dialcrowd.py
+++ tests/crowdsourcing/tasks/dialcrowd/test_dialcrowd.py
@@ -73,7 +73,7 @@ try:
 
             self._test_agent_state(task_data=task_data, data_regression=data_regression)
 
-except (ImportError) as _:
+except ImportError:
     pass
 
 if __name__ == "__main__":
