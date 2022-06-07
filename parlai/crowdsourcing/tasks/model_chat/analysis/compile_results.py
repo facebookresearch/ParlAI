952b6c2b5bd055d58d98bac3c053413e6ab321f5
Pearl Li <pearlli@devfair0420.h2.fair>
2022-06-07 08:04:54 -0700

    get rid of re and json imports

--- parlai/crowdsourcing/tasks/model_chat/analysis/compile_results.py
+++ parlai/crowdsourcing/tasks/model_chat/analysis/compile_results.py
@@ -4,9 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import json
 import os
-import re
 from datetime import datetime
 from typing import Any, Dict
 
