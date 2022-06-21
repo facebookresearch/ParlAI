ac88e0191881737793534992475722c32e8b64d3
meganung <meganu@fb.com>
2022-06-21 11:43:58 -0700

    lint

--- parlai/tasks/saferdialogues/test.py
+++ parlai/tasks/saferdialogues/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) Meta Platforms, Inc. and affiliates.
+# Copyright (c) Facebook Platforms, Inc. and affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
