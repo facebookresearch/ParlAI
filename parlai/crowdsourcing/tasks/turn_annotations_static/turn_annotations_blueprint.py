be3e87af19a60bee4d29f456058a13025e4700ed
Eric Smith <EricMichaelSmith@users.noreply.github.com>
2022-06-21 16:16:15 -0400

    Update turn_annotations_blueprint.py

--- parlai/crowdsourcing/tasks/turn_annotations_static/turn_annotations_blueprint.py
+++ parlai/crowdsourcing/tasks/turn_annotations_static/turn_annotations_blueprint.py
@@ -217,7 +217,7 @@ class TurnAnnotationsStaticBlueprint(StaticReactBlueprint):
             "annotation_buckets": annotation_buckets,
             "ask_reason": self.args.blueprint.ask_reason,
             "response_field": self.args.blueprint.response_field,
-            "frame_height": 650,
+            "frame_height": 0,
             "num_subtasks": self.args.blueprint.subtasks_per_unit,
             "block_mobile": True,
         }
