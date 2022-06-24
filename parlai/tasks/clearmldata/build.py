aecbe2b9b0264ba0b2b994b49c1738da44df4e0f
Muhammad Sakib Khan Inan <sakib.khaninan@gmail.com>
2022-06-24 21:47:21 +0600

    Update build.py

--- parlai/tasks/clearmldata/build.py
+++ parlai/tasks/clearmldata/build.py
@@ -1,6 +1,23 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Download and build the data if it does not exist.
+
+from parlai.core.build_data import DownloadableFile
 import parlai.core.build_data as build_data
 import os
-from clearml import Dataset
+
+RESOURCES = [
+    DownloadableFile(
+        "fd1c36de494c4fabb790d03894b4b4c7",
+        'empatheticdialogues.tar.gz',
+        'daf91fd2f8c2c9bc1713c74257b0a8643a0c78a87a26f946cceb5278f3445b1d',
+        from_clearml=True,
+    )
+]
 
 
 def build(opt):
@@ -15,11 +32,9 @@ def build(opt):
             build_data.remove_dir(dpath)
         build_data.make_dir(dpath)
 
-        # Download the data from ClearML
-        clearml_dataset = Dataset.get(
-            dataset_name='clearmldata', dataset_project="ParlAI"
-        )
-        clearml_dataset.get_mutable_local_copy(target_folder=dpath, overwrite=True)
+        # Download the data.
+        for downloadable_file in RESOURCES:
+            downloadable_file.download_file(dpath)
 
         # Mark the data as built.
         build_data.mark_done(dpath, version_string=version)
