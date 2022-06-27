1576b3115bffb621fb72c27263ae5c6ab667fb9a
meganung <meganu@fb.com>
2022-06-27 08:36:34 -0700

    lint

--- parlai/tasks/saferdialogues/agents.py
+++ parlai/tasks/saferdialogues/agents.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-# Copyright (c) Facebook Platforms, Inc. and affiliates.
+# Copyright (c) Facebook, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+
 
 from typing import Optional
 from parlai.core.params import ParlaiParser
