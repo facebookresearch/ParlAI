82f2bffc136b4278aa07896e466c03df8c9fed6c
Michael Saxon <mssaxon@fb.com>
2022-07-07 17:20:05 -0700

    substituted in older multiwoz_v22 (might fix jga issues)

---
+++ parlai/tasks/multiwoz_v22_backup/README.md
@@ -0,0 +1,12 @@
+Task: Multiwoz v2.2
+===============
+Description: Version of Multiwoz 2.0 dataset as cleaned by Google and structured to the Schema-Guided Dataset format.
+
+From https://github.com/budzianowski/multiwoz/tree/master/data/MultiWOZ\_2.2 
+
+License: MIT
+
+Link: https://aclanthology.org/2020.nlp4convai-1.13.pdf 
+
+Tags: #TOD #Multiwoz
+
---
+++ parlai/tasks/multiwoz_v22_backup/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from parlai.utils.testing import AutoTeacherTest
+
+
+class TestSystemTeacher(AutoTeacherTest):
+    task = "multiwoz_v22"
+
+
+class TestUserSimulatorTeacher(AutoTeacherTest):
+    task = "multiwoz_v22:UserSimulatorTeacher"
