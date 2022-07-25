561073c0a053cd3559870e950f93fa56a71043fd
Jack Urbanek <jju@fb.com>
2022-07-25 11:55:55 -0400

    Dialcrowd fix, drop unreachable 'times'

--- parlai/crowdsourcing/tasks/acute_eval/acute_eval_agent_state.py
+++ parlai/crowdsourcing/tasks/acute_eval/acute_eval_agent_state.py
@@ -38,14 +38,3 @@ class AcuteEvalAgentState(StaticAgentState):
             entry.update(outputs["final_data"][idx])
             response_list.append(entry)
         return response_list
-
-    def update_data(self, packet: "Packet") -> None:
-        """
-        Process the incoming data packet, and handle updating the state.
-        """
-        assert (
-            packet.data.get("MEPHISTO_is_submit") is True
-        ), "Static tasks should only have final act"
-        self.state["times"]["task_end"] = time.time()
-        self.state["outputs"] = packet.data["task_data"]
-        self.save_data()
