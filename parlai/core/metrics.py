e0f59aa107a55db5c080cf5847e6539b05509453
prajjwal1 <prajjwalin@protonmail.com>
2022-07-27 21:54:50 +0000

    changed how F1Metric returns

--- parlai/core/metrics.py
+++ parlai/core/metrics.py
@@ -526,7 +526,7 @@ class F1Metric(AverageMetric):
         return precision, recall, f1
 
     @staticmethod
-    def compute(guess: str, answers: List[str], output) -> F1Metric:
+    def compute(guess: str, answers: List[str]) -> F1Metric:
         if guess is None or answers is None:
             return AverageMetric(0, 0)
         g_tokens = normalize_answer(guess).split()
@@ -534,12 +534,10 @@ class F1Metric(AverageMetric):
             F1Metric._prec_recall_f1_score(g_tokens, normalize_answer(a).split())
             for a in answers
         ]
-        if output == "f1":
-            return F1Metric(max(f1 for p, r, f1 in scores), 1)
-        elif output == "precision":
-            return F1Metric(max(p for p, r, f1 in scores), 1)
-        elif output == "recall":
-            return F1Metric(max(r for p, r, f1 in scores), 1)
+        max_p, max_r, max_f1 = 0, 0, 0
+        for p, r, f1 in scores:
+            max_p, max_r, max_f1 = max(max_p, p), max(max_r, r), max(f1, max_f1)
+        return (F1Metric(max_p, 1), F1Metric(max_r, 1), F1Metric(max_f1, 1))
 
 
 class ExactMatchMetric(AverageMetric):
@@ -1021,9 +1019,10 @@ class TeacherMetrics(Metrics):
 
         if prediction is not None:
             self.add('accuracy', ExactMatchMetric.compute(prediction, labels))
-            self.add('precision', F1Metric.compute(prediction, labels, "precision"))
-            self.add('recall', F1Metric.compute(prediction, labels, output="recall"))
-            self.add('f1', F1Metric.compute(prediction, labels, output="f1"))
+            precision, recall, f1 = F1Metric.compute(prediction, labels)
+            self.add('precision', precision)
+            self.add('recall', recall)
+            self.add('f1', f1)
 
             for k in range(1, 5):  # 1..4
                 if f'bleu-{k}' in self._metrics_list:
