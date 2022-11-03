fb136fc4c15ff2e90a92b6ccf09aca67f9d44a35
Olga Golovneva <olggol@fb.com>
2022-11-03 06:46:02 -0700

    update links for public release

--- projects/roscoe/roscoe_data/download_annotated.sh
+++ projects/roscoe/roscoe_data/download_annotated.sh
@@ -7,9 +7,12 @@ mkdir -p ${PATH_TO_DATA}/raw
 mkdir -p ${PATH_TO_DATA}/generated
 mkdir -p ${PATH_TO_DATA}/annotated
 
-#Step 1. Download [dataset]_reasoning.txt in projects/roscoe/roscoe_data/generated/
-echo "Sorry, Pending data release approval"
-exit
+#Step 1. Download [dataset]_reasoning.txt in projects/roscoe/roscoe_data/generated/,
+# and annotated sets in projects/roscoe/roscoe_data/annotated/
+wget https://dl.fbaipublicfiles.com/parlai/projects/roscoe/annotations.zip
+unzip annotations.zip
+mv annotation_release/annotated/* ${PATH_TO_DATA}/annotated/.
+mv annotation_release/generated/* ${PATH_TO_DATA}/generated/.
 
 #Step 2. Download original datasets in projects/roscoe/roscoe_data/raw/
 # DROP
@@ -24,16 +27,12 @@ mv valid.csv ${PATH_TO_DATA}/raw/cosmos.txt
 # e-SNLI
 wget https://github.com/OanaMariaCamburu/e-SNLI/raw/master/dataset/esnli_dev.csv
 mv esnli_dev.csv ${PATH_TO_DATA}/raw/esnli.txt
-# SemEval
-wget https://raw.githubusercontent.com/DungLe13/commonsense/master/data/clean_data/clean-train-data.xml
-mv clean-train-data.xml ${PATH_TO_DATA}/raw/semevalcommonsense.txt
+# SemEval - pending release approval
+# wget https://raw.githubusercontent.com/DungLe13/commonsense/master/data/clean_data/clean-train-data.xml
+# mv clean-train-data.xml ${PATH_TO_DATA}/raw/semevalcommonsense.txt
 # GSM8K
 wget https://github.com/openai/grade-school-math/raw/master/grade_school_math/data/example_model_solutions.jsonl
 mv example_model_solutions.jsonl ${PATH_TO_DATA}/raw/gsm8k.txt
 
-#Step 3. Restore sets
-python ${RESTORE_SCRIPT}
-
-#Step 4. Download annotated sets in projects/roscoe/roscoe_data/annotated/
-echo "Sorry, Pending data release approval"
-exit
+#Step 3. Restore sets; SemEval - pending release approval
+python ${RESTORE_SCRIPT} --datasets drop esnli cosmos gsm8k
