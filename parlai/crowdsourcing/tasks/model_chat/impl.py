bb551e32e17e27ab86fa3bd5ab047045dae78747
Mojtaba <11262163+mojtaba-komeili@users.noreply.github.com>
2023-03-24 10:04:04 -0700

    Fixing some of the simple tests (#4991)
    * removed
    
    * the clean install docformatter
    
    * transformers upgrade to 4.20
    
    * clearml skip
    
    * ClearML test headers.

--- parlai/crowdsourcing/tasks/model_chat/impl.py
+++ parlai/crowdsourcing/tasks/model_chat/impl.py
@@ -48,7 +48,7 @@ def run_task(cfg: DictConfig, task_directory: str, world_module=None):
     # Default to a task-specific name to avoid soft-block collisions
     soft_block_mturk_workers(cfg=cfg, db=db, soft_block_qual_name=soft_block_qual_name)
 
-    # TODO: this maybe moved to the parent class: SharedModelChatTaskState 
+    # TODO: this maybe moved to the parent class: SharedModelChatTaskState
     if cfg.mephisto.blueprint.allowed_worker_qualification is not None:
         use_qualifications = [
             make_qualification_dict(
