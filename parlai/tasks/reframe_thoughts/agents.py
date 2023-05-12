922388125750d3a6ec0dcf491901a10f19ee8f49
meganung <meganu@fb.com>
2023-05-12 00:19:47 -0700

    reframe thoughts task and dataset

---
+++ parlai/tasks/reframe_thoughts/agents.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from typing import Optional
+from parlai.core.params import ParlaiParser
+from parlai.core.opt import Opt
+import os
+import copy
+import json
+from parlai.core.teachers import register_teacher, DialogTeacher
+from .build import build
+
+
+def _path(opt):
+    # Build the data if it doesn't exist.
+    build(opt)
+    dt = opt['datatype'].split(':')[0]
+    return os.path.join(
+        opt['datapath'], 'reframe_thoughts', 'reframe_thoughts_dataset', dt + '.txt'
+    )
+
+
+class ReframeThoughtsTeacher(DialogTeacher):
+    def __init__(self, opt, shared=None):
+        opt = copy.deepcopy(opt)
+        opt['task'] = "reframe_thoughts"
+        opt['datafile'] = _path(opt)
+        super().__init__(opt, shared)
+
+    def setup_data(self, datafile):
+        all_data = [json.loads(line.strip()) for line in open(datafile)]
+        for data in all_data:
+            persona = data["persona"]
+            pattern = data["pattern"] + " (" + data["pattern_def"] + ")"
+            thought = data["thought"]
+            persona_token = ""
+            pattern_token = "[PAT]"
+            thought_token = "[THT]"
+            for reframe in data["reframes"]:
+                input = " ".join(
+                    [
+                        persona_token,
+                        persona,
+                        pattern_token,
+                        pattern,
+                        thought_token,
+                        thought,
+                    ]
+                ).strip()
+                yield (input, reframe['reframe']), True
+
+
+class DefaultTeacher(ReframeThoughtsTeacher):
+    pass
